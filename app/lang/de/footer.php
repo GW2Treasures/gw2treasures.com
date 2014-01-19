72e442b8fd780843b9bf2656c8373b516a4ab9e1
darthmaim <darth.maim@arcor.de>
2014-01-19 15:04:39 +0100

    added german localization for the footer

---
+++ app/lang/de/footer.php
@@ -0,0 +1,26 @@
+<?php 
+
+return array(
+	'itemlists' => 'Gegenstands Listen',
+	'recentlyAddedItems' =>'Kürzlich hinzugefügte Gegenstände',
+	'recentlyChangedItems' =>'Kürzlich geänderte Gegenstände',
+	'weaponSets' =>'Waffen Sets',
+	'armorSets' =>'Rüstungs Sets',
+	'RSSFeeds' =>'RSS Feeds',
+	'newItems' =>'Neue Gegenstände',
+	'changedItems' =>'Geänderte Gegenstände',
+	'developer' =>'Entwickler',
+	'APIDocumentation' =>'API Dokumentation',
+	'about' =>'Über',
+	'statistics' =>'Statistiken',
+	'changelog' =>'Changelog',
+	'terms' =>'Datenschutzerklärung & Nutzungsbedingungen',
+	'contact' =>'Kontakt',
+	'language' =>'Language',
+	'german' =>'Deutsch',
+	'english' =>'English',
+	'spanish' =>'Español',
+	'french' =>'Français',
+	'legalNotice1' => 'This site is not affiliated with ArenaNet, Guild Wars 2, or any of their partners. All copyrights reserved to their respective owners.',
+	'legalNotice2' => '© 2013 ArenaNet, Inc. All rights reserved. NCsoft, the interlocking NC logo, ArenaNet, Guild Wars, Guild Wars Factions, Guild Wars Nightfall, Guild Wars: Eye of the North, Guild Wars 2, and all associated logos and designs are trademarks or registered trademarks of NCsoft Corporation. All other trademarks are the property of their respective owners.',
+);

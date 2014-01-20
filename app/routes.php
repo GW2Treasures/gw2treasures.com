d392ac21ec35cef63730f83a6e0548b4094a5dea
darthmaim <darth.maim@arcor.de>
2014-01-20 10:04:19 +0100

    improved language handling

--- app/routes.php
+++ app/routes.php
@@ -11,34 +11,69 @@
 |
 */
 
-// filter to set locale depending on subdomain
-// redirect to best fitting subdomain if subdomain doesnt exist / is invalid
+// handle localizations 
 Route::filter( 'setLocale', function( $route, $request ) {
 	$parameters = $route->parameters();
 	$lang = array_key_exists( 'language', $parameters ) ? $parameters[ 'language' ] : '';
+
+	// handle invalid languages
 	if( !in_array( $lang, array( 'de', 'en', 'es', 'fr' )) ) {
-		$lang = Helper::prefered_language( array('de', 'en', 'es', 'fr') );
+
+		// check if we have a valid language in the session
+		if (Session::has( 'language' ) && in_array( Session::get( 'language'), array( 'de', 'en', 'es', 'fr' )) ) {
+			$lang = Session::get( 'language');
+		} else {
+			// get the prefered language for the visitor
+			$lang = Helper::prefered_language( array('en', 'de', 'es', 'fr') );
+
+			// show a notification that we changed the language
+			Notification::Add( 'language', Lang::get( 'notifications.autoLanguage', array('language' => Lang::get( 'notifications.language.' . $lang )) ) );
+		}
+
+		// redirect to the right subdomain
 		return Redirect::to( '//' . $lang . '.' . Config::get('app.domain') .  $request->getRequestUri() );
 	}
-	App::setLocale( $parameters['language'] );
+
+	// set locale
+	App::setLocale( $lang );
+
+	// handle changed language
+	if( Session::has( 'language' ) && Session::get( 'language') != $lang ) {
+		// hide the autodetected language notification if its there
+		Notification::Remove( 'language' );
+	}
+
+	// save the language in the session
+	Session::put( 'language', $lang );
 });
 
-//redirect all requests without language sub domain
+// route to hide notifications
+Route::get('notification/hide/{key}', array('as' => 'hideNotification', function( $key ) {
+	Notification::Remove( $key );
+	return Redirect::back();
+}));
+
+// redirect all requests without language sub domain
 Route::group( array('domain' => Config::get('app.domain'), 'before' => 'setLocale'), function() {
 	Route::any('{x}', function() {})->where('x', '.*');
 });
 
+// all main routes
 Route::group( array('domain' => '{language}.' . Config::get('app.domain'), 'before' => 'setLocale'), function() {
+	// bind the {item} parameter in routes to the Item model
 	Route::model('item', 'Item', function( ) {
 		throw new Exception( 'Item not found' );
 	});
 
+	// mainpage
 	Route::get('/', function() {
 		return View::make('start');
 	});
 	
+	// item details
 	Route::get('item/{item}', array( 'as' => 'itemdetails', 'uses' => 'ItemController@showDetails') );
 
+	// search
 	Route::get('search', array('as' => 'search', function() {
 		return 'search';
 	}));

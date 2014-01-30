0b6f37ce2607ed9fd57b70981e38762a2da63e48
darthmaim <darth.maim@arcor.de>
2014-01-30 02:10:18 +0100

    show search input on /search

--- app/views/item/searchresults.blade.php
+++ app/views/item/searchresults.blade.php
@@ -1,10 +1,17 @@
-<h2>Searchresults for "{{ $searchterm }}"</h2>
-{{ count($items) }} results.
-@foreach( $items as $item )
-	<div style="margin:2px">
-		<a href="{{ $item->getUrl() }}">
-			<img src="{{ $item->getIconUrl( 32 ) }}" width="32" height="32" alt="">
-			{{ $item->getName() }}
-		</a>
-	</div>
-@endforeach
+{{ Form::open( array( 'method' => 'GET', 'route' => array('search', App::getLocale() )) ) }}
+	{{ Form::text( 'q', $searchterm, array('placeholder' => 'Search', 'class' => 'textbox', 'id' => 'searchInput' )) }}
+	{{ Form::submit( null, array('class' => 'button') ) }}
+{{ Form::close() }}
+
+@if( strlen( $searchterm ) > 0 )
+	<h2>Searchresults for "{{ $searchterm }}"</h2>
+	{{ count($items) }} results.
+	@foreach( $items as $item )
+		<div style="margin:2px">
+			<a href="{{ $item->getUrl() }}">
+				<img src="{{ $item->getIconUrl( 32 ) }}" width="32" height="32" alt="">
+				{{ $item->getName() }}
+			</a>
+		</div>
+	@endforeach
+@endif

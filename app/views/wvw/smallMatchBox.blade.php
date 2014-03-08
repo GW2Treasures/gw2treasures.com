2879f9c107fa9265f6b882194f65ba05871aab81
darthmaim <darth.maim@arcor.de>
2014-03-08 18:14:38 +0100

    use normal space as separator

--- app/views/wvw/smallMatchBox.blade.php
+++ app/views/wvw/smallMatchBox.blade.php
@@ -4,7 +4,7 @@
 		<?php $maxIncome = max( $match->income[ Match::WORLD_RED ], $match->income[ Match::WORLD_BLUE ], $match->income[ Match::WORLD_GREEN ] ); ?>
 		<dt>{{ Match::getWorldName( $match->red_world_id ) }}</dt>
 		<dd class="score"><div class="scoreBar red" data-score="{{$match->red_score}}" style="width:{{ $match->red_score / $maxScore * 100 }}%">
-				{{ number_format( $match->red_score, 0, '.', ' ' ) }}
+				{{ number_format( $match->red_score, 0, '.', ' ' ) }}
 		</div></dd>
 		<dd class="income"><div class="scoreBar red" data-score="{{$match->red_score}}" style="width:{{ $match->income[ Match::WORLD_RED ] / $maxIncome * 100 }}%">
 				+{{ $match->income[ Match::WORLD_RED ] }}
@@ -18,7 +18,7 @@
 
 		<dt>{{ Match::getWorldName( $match->blue_world_id ) }}</dt>
 		<dd class="score"><div class="scoreBar blue" data-score="{{$match->blue_score}}" style="width:{{ $match->blue_score / $maxScore * 100 }}%">
-				{{ number_format( $match->blue_score, 0, '.', ' ' ) }}
+				{{ number_format( $match->blue_score, 0, '.', ' ' ) }}
 		</div></dd>
 		<dd class="income"><div class="scoreBar blue" data-score="{{$match->blue_score}}" style="width:{{ $match->income[ Match::WORLD_BLUE ] / $maxIncome * 100 }}%">
 				+{{ $match->income[ Match::WORLD_BLUE ] }}
@@ -32,7 +32,7 @@
 
 		<dt>{{ Match::getWorldName( $match->green_world_id ) }}</dt>
 		<dd class="score"><div class="scoreBar green" data-score="{{$match->green_score}}" style="width:{{ $match->green_score / $maxScore * 100 }}%">
-				{{ number_format( $match->green_score, 0, '.', ' ' ) }}
+				{{ number_format( $match->green_score, 0, '.', ' ' ) }}
 		</div></dd>
 		<dd class="income"><div class="scoreBar green" data-score="{{$match->green_score}}" style="width:{{ $match->income[ Match::WORLD_GREEN ] / $maxIncome * 100 }}%">
 				+{{ $match->income[ Match::WORLD_GREEN ] }}

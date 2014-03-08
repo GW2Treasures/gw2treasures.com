1d2be13046b4379d56320ce79c8f0c6a6662d312
darthmaim <darth.maim@arcor.de>
2014-03-08 19:43:13 +0100

    fixed cut off scores and incomes

--- app/views/wvw/smallMatchBox.blade.php
+++ app/views/wvw/smallMatchBox.blade.php
@@ -4,12 +4,12 @@
 		<?php $maxIncome = max( $match->income[ Match::WORLD_RED ], $match->income[ Match::WORLD_BLUE ], $match->income[ Match::WORLD_GREEN ] ); ?>
 
 		<dt>{{ Match::getWorldName( $match->green_world_id ) }}</dt>
-		<dd class="score"><div class="scoreBar green" data-score="{{$match->green_score}}" style="width:{{ $match->green_score / $maxScore * 100 }}%">
-				{{ number_format( $match->green_score, 0, '.', ' ' ) }}
-		</div></dd>
-		<dd class="income"><div class="scoreBar green" data-score="{{$match->green_score}}" style="width:{{ $match->income[ Match::WORLD_GREEN ] / $maxIncome * 100 }}%">
-				+{{ $match->income[ Match::WORLD_GREEN ] }}
-		</div></dd>
+		<dd class="score"><span class="scoreBarLabel">{{ number_format( $match->green_score, 0, '.', ' ' ) }}</span>
+			<div class="scoreBar green" data-score="{{$match->green_score}}" style="width:{{ $match->green_score / $maxScore * 100 }}%">&nbsp;</div>
+		</dd>
+		<dd class="income"><span class="scoreBarLabel">+{{ $match->income[ Match::WORLD_GREEN ] }}</span>
+			<div class="scoreBar green" data-score="{{$match->green_score}}" style="width:{{ $match->income[ Match::WORLD_GREEN ] / $maxIncome * 100 }}%">&nbsp;</div>
+		</dd>
 		<dd class="objectives">
 			<span>{{ $match->objectiveCounts[ Match::WORLD_GREEN ][ Objective::TYPE_CAMP   ] }}</span>
 			<span>{{ $match->objectiveCounts[ Match::WORLD_GREEN ][ Objective::TYPE_TOWER  ] }}</span>
@@ -18,12 +18,12 @@
 		</dd>
 
 		<dt>{{ Match::getWorldName( $match->blue_world_id ) }}</dt>
-		<dd class="score"><div class="scoreBar blue" data-score="{{$match->blue_score}}" style="width:{{ $match->blue_score / $maxScore * 100 }}%">
-				{{ number_format( $match->blue_score, 0, '.', ' ' ) }}
-		</div></dd>
-		<dd class="income"><div class="scoreBar blue" data-score="{{$match->blue_score}}" style="width:{{ $match->income[ Match::WORLD_BLUE ] / $maxIncome * 100 }}%">
-				+{{ $match->income[ Match::WORLD_BLUE ] }}
-		</div></dd>
+		<dd class="score"><span class="scoreBarLabel">{{ number_format( $match->blue_score, 0, '.', ' ' ) }}</span>
+			<div class="scoreBar blue" data-score="{{$match->blue_score}}" style="width:{{ $match->blue_score / $maxScore * 100 }}%">&nbsp;</div>
+		</dd>
+		<dd class="income"><span class="scoreBarLabel">+{{ $match->income[ Match::WORLD_BLUE ] }}</span>
+			<div class="scoreBar blue" data-score="{{$match->blue_score}}" style="width:{{ $match->income[ Match::WORLD_BLUE ] / $maxIncome * 100 }}%">&nbsp;</div>
+		</dd>
 		<dd class="objectives">
 			<span>{{ $match->objectiveCounts[ Match::WORLD_BLUE ][ Objective::TYPE_CAMP   ] }}</span>
 			<span>{{ $match->objectiveCounts[ Match::WORLD_BLUE ][ Objective::TYPE_TOWER  ] }}</span>
@@ -32,12 +32,12 @@
 		</dd>
 
 		<dt>{{ Match::getWorldName( $match->red_world_id ) }}</dt>
-		<dd class="score"><div class="scoreBar red" data-score="{{$match->red_score}}" style="width:{{ $match->red_score / $maxScore * 100 }}%">
-				{{ number_format( $match->red_score, 0, '.', ' ' ) }}
-		</div></dd>
-		<dd class="income"><div class="scoreBar red" data-score="{{$match->red_score}}" style="width:{{ $match->income[ Match::WORLD_RED ] / $maxIncome * 100 }}%">
-				+{{ $match->income[ Match::WORLD_RED ] }}
-		</div></dd>
+		<dd class="score"><span class="scoreBarLabel">{{ number_format( $match->red_score, 0, '.', ' ' ) }}</span>
+			<div class="scoreBar red" data-score="{{$match->red_score}}" style="width:{{ $match->red_score / $maxScore * 100 }}%">&nbsp;</div>
+		</dd>
+		<dd class="income"><span class="scoreBarLabel">+{{ $match->income[ Match::WORLD_RED ] }}</span>
+			<div class="scoreBar red" data-score="{{$match->red_score}}" style="width:{{ $match->income[ Match::WORLD_RED ] / $maxIncome * 100 }}%">&nbsp;</div>
+		</dd>
 		<dd class="objectives">
 			<span>{{ $match->objectiveCounts[ Match::WORLD_RED ][ Objective::TYPE_CAMP   ] }}</span>
 			<span>{{ $match->objectiveCounts[ Match::WORLD_RED ][ Objective::TYPE_TOWER  ] }}</span>

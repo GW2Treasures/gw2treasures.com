d47402e27e85a871b760399c6b500fa8d95b6f53
darthmaim <darth.maim@arcor.de>
2014-03-17 14:09:08 +0100

    g-

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -184,17 +184,6 @@
 				cache.clear();
 			</script>
 		@endif
-
-		<!-- g+ -->
-		<script type="text/javascript">
-		  window.___gcfg = {lang: '{{ App::getLocale() }}' };
-
-		  (function() {
-		    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-		    po.src = 'https://apis.google.com/js/platform.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-		  })();
-		</script>
 	</div>
 </body>
 </html>

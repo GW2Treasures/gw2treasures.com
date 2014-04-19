b99510552f40b8601c8f297d9a679b26be6a85f4
darthmaim <darth.maim@arcor.de>
2014-04-19 21:51:45 +0200

    add Skin model

---
+++ app/models/Skin.php
@@ -0,0 +1,41 @@
+<?php
+
+class Skin extends BaseModel {
+	public function getName( $lang = null ) {
+		return $this->localized( 'name', $lang );
+	}
+
+	private $d = array();
+	public function getData( $lang = null ) {
+		if ( !array_key_exists( $lang, $this->d ) ) {
+			$this->d[ $lang ] = json_decode( 
+				str_replace( array( '<br>' ),
+				             array( '\n'   ),
+				             $this->localized( 'data', $lang )));
+		}
+		return $this->d[ $lang ];
+	}
+
+	public function getTypeData( $lang = null ) {
+		$type = strtolower( $this->type );
+		if( isset( $this->getData( $lang )->{$type} )) {
+			return $this->getData( $lang )->{$type};
+		}
+		return new stdClass();
+	}
+
+	public function getIconUrl( $size = 64 ) {
+		$size = intval( $size );
+		if( !in_array( $size, array( 16, 32, 64) )) {
+			if( $size <= 16 ) { 
+				$size = 16;
+			} elseif ( $size <= 32 ) {
+				$size = 32;
+			} else {
+				$size = 64;
+			}
+		}
+
+		return Helper::cdn( 'icons/' . $this->signature . '/' . $this->file_id . '-' . $size . 'px.png', $this->file_id );
+	}
+}

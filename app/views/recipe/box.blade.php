9ae04004cdc56c29fd9ad532fdbcca6edc32ecf9
darthmaim <darth.maim@arcor.de>
2014-09-01 01:31:35 +0200

    redesign recipe box

--- app/views/recipe/box.blade.php
+++ app/views/recipe/box.blade.php
@@ -1,47 +1,20 @@
-<div class="recipeBox">
-	{{-- unlockedBy --}}
-	@if( $recipe->hasFlag( 'LearnedFromItem' ) )
-		@if( !is_null( $recipe->unlockedBy ) && $recipe->unlockedBy->unlock_type == 'CraftingRecipe' )
-			<a data-item-id="{{ $recipe->unlockedBy->id }}" href="{{ $recipe->unlockedBy->getUrl() }}">
-				<img src="{{ $recipe->unlockedBy->getIconUrl( 16 ) }}" width="16" height="16" alt="">
-				{{ $recipe->unlockedBy->getName() }}
-			</a>
-		@else
-			???
-		@endif
+<div class="recipeBox {{ !is_null( $recipe->output ) ? 'border-' . $recipe->output->rarity : '' }}">
+	<header>
+		<div class="output color-{{ $recipe->output->rarity }}">
+			@if( $recipe->output_count == 1 )
+				{{ $recipe->output->link( 32 ) }}
+			@else
+				{{ $recipe->output->link( 32, null, $recipe->output_count . '× ' . $recipe->output->getName() ) }}
+				{{-- {{ $recipe->output->link( 32, null, $recipe->output->getName() . ' ×' . $recipe->output_count ) }} --}}
+			@endif
+		</div>
+	</header>
+
+	{{-- time to craft --}}
+	@if( $recipe->getData()->time_to_craft_ms > 0 )
+		<div class="timeToCraft">{{ Helper::duration( $recipe->getData()->time_to_craft_ms ) }} <i class="sprite-20-activation"></i></div>
 	@endif
 
-	{{-- Ingredients --}}
-	<ul class="ingredients">
-		<?php $counts = $recipe->getIngredientCounts(); ?>
-		@foreach( $recipe->getIngredients() as $i => $ingredient)
-			@if( $counts[ $i ] > 0 )
-				<li>
-					<span class="count">{{ $counts[ $i ] }}</span>
-					<a data-item-id="{{ $ingredient->id }}" href="{{ $ingredient->getUrl() }}">
-						<img src="{{ $ingredient->getIconUrl( 16 ) }}" width="16" height="16" alt="">
-						{{ $ingredient->getName() }}
-					</a>
-				</li>
-			@endif
-		@endforeach
-	</ul>
-	{{-- Output --}}
-	<div class="output">
-		<span class="count">{{ $recipe->output_count }}</span>
-		@if( !is_null( $recipe->output ) )
-			<a data-item-id="{{ $recipe->output->id }}" href="{{ $recipe->output->getUrl() }}">
-				<img src="{{ $recipe->output->getIconUrl( 16 ) }}" width="16" height="16" alt="">
-				{{ $recipe->output->getName() }}
-			</a>
-		@else
-			<span style="font-style: italic">???</span>
-		@endif
-	</div>
-	{{-- time to craft --}}
-	@if( $recipe->getData( )->time_to_craft_ms > 0 )
-		<div class="timeToCraft">{{ Helper::duration( $recipe->getData( )->time_to_craft_ms ) }} <i class="sprite-20-activation"></i></div>
-	@endif
 	{{-- disciplines --}}
 	<div class="disciplines">
 		<?php
@@ -53,4 +26,30 @@
 		?>
 		{{ $recipe->rating }}
 	</div>
+
+	{{-- Ingredients --}}
+	<ul class="ingredients">
+		<?php $counts = $recipe->getIngredientCounts(); ?>
+		@foreach( $recipe->getIngredients() as $i => $ingredient)
+			@if( $counts[ $i ] > 0 )
+				<li>
+					<span class="count">{{ $counts[ $i ] }}</span>
+					{{ $ingredient->link(16) }}
+				</li>
+			@endif
+		@endforeach
+	</ul>
+
+	{{-- unlockedBy --}}
+	@if( $recipe->hasFlag( 'LearnedFromItem' ) )
+		<div class="recipe">
+			<span class="recipeLabel">{{ trans('recipe.unlockedBy') }}:</span>
+			@if( !is_null( $recipe->unlockedBy ) && $recipe->unlockedBy->unlock_type == 'CraftingRecipe' )
+				{{ $recipe->unlockedBy->link(16) }}
+			@else
+				???
+			@endif
+		</div>
+	@endif
+	
 </div>

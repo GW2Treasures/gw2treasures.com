cbe11e81a2c83a8f200224debb027e4c8890c810
darthmaim <darth.maim@arcor.de>
2014-09-01 01:18:13 +0200

    show recently/most viewed items and statistics on start page

--- app/views/start.blade.php
+++ app/views/start.blade.php
@@ -12,12 +12,60 @@
 	</div>
 </div>
 
-<h2 class="pageWidth">{{ trans('footer.newItems') }}</h2>
+<div class="pageWidth grid">
+	<div class="row">
+		<div class="column2">
+			<h2>Recently viewed Items</h2>
+			<ul class="itemList">
+				@foreach ($recentlyViewedItems as $item)
+					<li>{{ $item->link(32) }}</li>
+				@endforeach
+			</ul>
+		</div>
+		<div class="column2">
+			<h2>Most viewed Items</h2>
+			<ul class="itemList">
+				@foreach ($popularItems as $item)
+					<li style="clear:right">{{ $item->link(32) }} <span style="float:right; line-height: 32px; white-space: nowrap;">{{ $item->views }} Views</span></li>
+				@endforeach
+			</ul>
+		</div>
+	</div>
+</div>
+
+<h2 class="pageWidth">Statistics</h2>
 <div class="pageWidth">
-	<ul class="itemList">
-		@foreach ($newItems as $item)
-			<?php $date_added = new \Carbon\Carbon($item->date_added) ?>
-			<li style="clear:right">{{ $item->link(32) }} <span style="float:right; line-height: 32px; white-space: nowrap;">{{ $date_added->diffForHumans() }}</span></li>
-		@endforeach
+	<ul class="statistics">
+		<li><strong>34 097</strong><em>Items</em>
+		<li><strong>82</strong><em>New Items<br>in the last 7 days</em>
+		<li><strong>8 532</strong><em>Recipes</em>
 	</ul>
+</div>
+
+<h2 class="pageWidth">{{ trans('footer.newItems') }}</h2>
+<div class="pageWidth grid">
+	<div class="row">
+		<div class="column2">
+			<ul class="itemList">
+				@for( $i = 0; $i < count( $newItems ) / 2; $i++ )
+					<?php
+						$item = $newItems[ $i ];
+						$date_added = new \Carbon\Carbon($item->date_added);
+					?>
+					<li style="clear:right">{{ $item->link(32) }} <span style="float:right; line-height: 32px; white-space: nowrap;">{{ $date_added->diffForHumans() }}</span></li>
+				@endfor
+			</ul>
+		</div>
+		<div class="column2">
+			<ul class="itemList">
+				@for( $i = count( $newItems ) / 2; $i < count( $newItems ); $i++ )
+					<?php
+						$item = $newItems[ $i ];
+						$date_added = new \Carbon\Carbon($item->date_added);
+					?>
+					<li style="clear:right">{{ $item->link(32) }} <span style="float:right; line-height: 32px; white-space: nowrap;">{{ $date_added->diffForHumans() }}</span></li>
+				@endfor
+			</ul>
+		</div>
+	</div>
 </div>

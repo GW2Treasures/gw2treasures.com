0622e96abb57c1cc280abcefd15a393dcd965ba4
darthmaim <darth.maim@arcor.de>
2014-10-12 18:37:54 +0200

    removed "spanish translator wanted" notification

--- app/views/static/notifications.blade.php
+++ app/views/static/notifications.blade.php
@@ -1,12 +1,8 @@
 <!-- notifications -->
 <ul id="notifications">
-	@if( in_array( App::getLocale(), array( 'es', 'fr' )))
+	@if( App::getLocale() == 'fr' )
 		<li class="notification"><div class="pageWidth clearfix"><div class="notificationContent">
-			@if( App::getLocale() == 'es' )
-				You <strong>speak spanish</strong> and want to support this project? <a href="{{ URL::route('contact', App::getLocale()) }}">Contact me now</a> to <strong>help translating</strong>!
-			@else
-				You <strong>speak french</strong> and want to support this project? <a href="{{ URL::route('contact', App::getLocale()) }}">Contact me now</a> to <strong>help translating</strong>!
-			@endif
+			You <strong>speak french</strong> and want to support this project? <a href="{{ URL::route('contact', App::getLocale()) }}">Contact me now</a> to <strong>help translating</strong>!
 		</div></div></li>
 	@endif
 	@foreach( Notification::Notifications() as $n )

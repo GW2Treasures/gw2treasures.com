146eaae4385f576f0d218e650e21791e28d700d0
darthmaim <darth.maim@arcor.de>
2014-12-21 20:10:53 +0100

    show meta title/description/image if available

--- app/controllers/ItemController.php
+++ app/controllers/ItemController.php
@@ -1,111 +1,115 @@
 <?php
 
 class ItemController extends BaseController {
-	protected $layout = 'layout';
+    /** @var \Illuminate\View\View|stdClass $layout  */
+    protected $layout = 'layout';
 
-	public function showDetails( $language, Item $item ) {
-		$this->layout->title = $item->getName();
+    public function showDetails( $language, Item $item ) {
+        $this->layout->title = $item->getName();
 
-		$key = CacheHelper::ItemDetails( $item, $language );
+        $key = CacheHelper::ItemDetails( $item, $language );
 
-		if( Cache::has( $key ) && !isset( $_GET['nocache'] )) {
-			$this->layout->content = Cache::get( $key );
-			$this->layout->cached = true;
-		} else {
-			$content = View::make( 'item.details', array( 'item' => $item ))->render();
-			$this->layout->content = $content;
+        if( Cache::has( $key ) && !isset( $_GET['nocache'] )) {
+            $this->layout->content = Cache::get( $key );
+            $this->layout->cached = true;
+        } else {
+            $content = View::make( 'item.details', array( 'item' => $item ))->render();
+            $this->layout->content = $content;
 
-			Cache::forever( $key, $content );
-		}
+            Cache::forever( $key, $content );
+        }
 
-		DB::table('item_views')->insert(array(
-			'item_id' => $item->id,
-			'language' => $language,
-		));
-	}
+        $this->layout->metaTitle = $item->getName();
+        $this->layout->metaImage = $item->getIconUrl(64);
 
-	public function json( $language, Item $item ) {
-		return Response::json( $item->getData() );
-	}
+        DB::table('item_views')->insert(array(
+            'item_id' => $item->id,
+            'language' => $language,
+        ));
+    }
 
-	public function random( $language ) {
-		$id = Item::random()->first()->id;
-		return Redirect::route( 'itemdetails', array( $language, $id ));
-	}
+    public function json( $language, Item $item ) {
+        return Response::json( $item->getData() );
+    }
 
-	public function tooltip( $language, Item $item ) {
-		return $item->getTooltip( $language );
-	}
+    public function random( $language ) {
+        $id = Item::random()->first()->id;
+        return Redirect::route( 'itemdetails', array( $language, $id ));
+    }
 
-	public function search( $language ) {
-		$searchTerm = trim( Input::get('q') );
-		$items = array();
+    public function tooltip( $language, Item $item ) {
+        return $item->getTooltip( $language );
+    }
 
-		if( strlen( $searchTerm ) > 0 ) {
-			// item id
-			if( preg_match('/^[0-9]+$/', $searchTerm ) ) {
-				$item = Item::find( intval( $searchTerm ) );
-				if( !is_null( $item ) ) {
-					return Redirect::route( 'itemdetails', array( $language, $searchTerm ));
-				}
-			}
+    public function search( $language ) {
+        $searchTerm = trim( Input::get('q') );
+        $items = array();
 
-			// chatlink
-			if(( $chatlink = Chatlink::TryDecode( $searchTerm )) !== false ) {
-				switch( $chatlink->type ) {
-					case Chatlink::TYPE_ITEM:
-						return Redirect::route( 'itemdetails', array( $language, $chatlink->id ));
-					case Chatlink::TYPE_RECIPE:
-						$recipe = Recipe::find( $chatlink->id );
-						return Redirect::route( 'itemdetails', array( $language, $recipe->output_id ));
-				}
-			}
+        if( strlen( $searchTerm ) > 0 ) {
+            // item id
+            if( preg_match('/^[0-9]+$/', $searchTerm ) ) {
+                $item = Item::find( intval( $searchTerm ) );
+                if( !is_null( $item ) ) {
+                    return Redirect::route( 'itemdetails', array( $language, $searchTerm ));
+                }
+            }
 
-			$items = Item::search( $searchTerm )->take( 1000 )->get();
-			if( $items->count() == 0 ) {
-				$items = Item::search( $searchTerm, true )->take( 1000 )->get();
-			}
-			$items = Item::sortSearchResult( $items , $searchTerm );
-		}
+            // chatlink
+            if(( $chatlink = Chatlink::TryDecode( $searchTerm )) !== false ) {
+                switch( $chatlink->type ) {
+                    case Chatlink::TYPE_ITEM:
+                        return Redirect::route( 'itemdetails', array( $language, $chatlink->id ));
+                    case Chatlink::TYPE_RECIPE:
+                        $recipe = Recipe::find( $chatlink->id );
+                        return Redirect::route( 'itemdetails', array( $language, $recipe->output_id ));
+                }
+            }
 
-		$this->layout->content = View::make( 'item.searchresults', array( 
-			'items' => $items, 'searchterm' => $searchTerm ));
-		$this->layout->title = 'searchresults';
-	}
+            $items = Item::search( $searchTerm )->take( 1000 )->get();
+            if( $items->count() == 0 ) {
+                $items = Item::search( $searchTerm, true )->take( 1000 )->get();
+            }
+            $items = Item::sortSearchResult( $items , $searchTerm );
+        }
 
-	public function searchAutocomplete( $language ) {
-		$searchTerm = trim( Input::get('q') );
+        $this->layout->content = View::make( 'item.searchresults', array(
+            'items' => $items, 'searchterm' => $searchTerm ));
+        $this->layout->title = 'searchresults';
+    }
 
-		if(( $chatlink = Chatlink::TryDecode( $searchTerm )) !== false ) {
-			switch( $chatlink->type ) {
-				case Chatlink::TYPE_ITEM:
-					$items = array( Item::find( $chatlink->id ));
-					break;
-				case Chatlink::TYPE_RECIPE:
-					$recipe = Recipe::find( $chatlink->id );
-					$items = array( Item::find( $recipe->output_id ));
-					break;
-			}
-		} else {
-			$items = Item::search( $searchTerm )->take( 10 )->get();
-		}
-		
-		$response = new stdClass();
-		$response->items = array();
+    public function searchAutocomplete( $language ) {
+        $searchTerm = trim( Input::get('q') );
 
-		foreach ( $items as $item ) {
-			if( is_null($item) ) {
-				continue;
-			}
-			$i = new stdClass();
-			$i->id = $item->id;
-			$i->name = $item->getName();
-			$i->icon16 = Helper::cdn( 'icons/' . $item->signature . '/' . $item->file_id . '-16px.png', $item->file_id );
-			$i->icon32 = Helper::cdn( 'icons/' . $item->signature . '/' . $item->file_id . '-32px.png', $item->file_id );
-			$i->icon64 = Helper::cdn( 'icons/' . $item->signature . '/' . $item->file_id . '-64px.png', $item->file_id );
-			$response->items[] = $i;
-		}
+        if(( $chatlink = Chatlink::TryDecode( $searchTerm )) !== false ) {
+            switch( $chatlink->type ) {
+                case Chatlink::TYPE_ITEM:
+                    $items = array( Item::find( $chatlink->id ));
+                    break;
+                case Chatlink::TYPE_RECIPE:
+                    $recipe = Recipe::find( $chatlink->id );
+                    $items = array( Item::find( $recipe->output_id ));
+                    break;
+            }
+        } else {
+            $items = Item::search( $searchTerm )->take( 10 )->get();
+        }
 
-		return Response::json( $response );
-	}
+        $response = new stdClass();
+        $response->items = array();
+
+        foreach ( $items as $item ) {
+            if( is_null($item) ) {
+                continue;
+            }
+            $i = new stdClass();
+            $i->id = $item->id;
+            $i->name = $item->getName();
+            $i->icon16 = Helper::cdn( 'icons/' . $item->signature . '/' . $item->file_id . '-16px.png', $item->file_id );
+            $i->icon32 = Helper::cdn( 'icons/' . $item->signature . '/' . $item->file_id . '-32px.png', $item->file_id );
+            $i->icon64 = Helper::cdn( 'icons/' . $item->signature . '/' . $item->file_id . '-64px.png', $item->file_id );
+            $response->items[] = $i;
+        }
+
+        return Response::json( $response );
+    }
 }
--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="utf-8">
     <title>{{ $title }} | GW2 Treasures</title>
-    <meta name="description" content="">
+    <meta name="description" content="{{ $metaDescription or 'GW2 Treasures: The Guild Wars 2 Item Database' }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
 
     <meta name="gw2treasures:domain" content="{{ Config::get('app.domain') }}">

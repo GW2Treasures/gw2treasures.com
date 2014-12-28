1b7a897bdc1e6ce30c5828a8f84af03fe6222947
darthmaim <darth.maim@arcor.de>
2014-12-28 18:21:46 +0100

    set metaDescription for items

--- app/controllers/ItemController.php
+++ app/controllers/ItemController.php
@@ -4,30 +4,41 @@ class ItemController extends BaseController {
     /** @var \Illuminate\View\View|stdClass $layout  */
     protected $layout = 'layout';
 
+
     public function showDetails( $language, Item $item ) {
         $this->layout->title = $item->getName();
 
         $key = CacheHelper::ItemDetails( $item, $language );
 
-        if( Cache::has( $key ) && !isset( $_GET['nocache'] )) {
+        if( Cache::has( $key ) && !isset($_GET[ 'nocache' ]) ) {
             $this->layout->content = Cache::get( $key );
             $this->layout->cached = true;
         } else {
-            $content = View::make( 'item.details', array( 'item' => $item ))->render();
+            $content = View::make( 'item.details', array( 'item' => $item ) )->render();
             $this->layout->content = $content;
 
             Cache::forever( $key, $content );
         }
 
+        if( isset($item->getTypeData()->type) ) {
+            $type = trans( 'item.type.' . $item->type ) . ' (' .
+                    trans( 'item.subtype.' . $item->type . '.' . $item->getTypeData()->type ) . ')';
+        } elseif( $item->sub_type != '' ) {
+            $type = trans( 'item.type.' . $item->type ) . ' (' .
+                    trans( 'item.subtype.' . $item->type . '.' . $item->subtype ) . ')';
+        } else {
+            $type = trans( 'item.type.' . $item->type );
+        }
+
         $this->layout->metaTitle = $item->getName();
         $this->layout->metaImage = $item->getIconUrl(64);
+        $this->layout->metaDescription = trans( 'item.rarity.' . $item->rarity ) . ' ' . $type;
 
         DB::table('item_views')->insert(array(
             'item_id' => $item->id,
             'language' => $language,
         ));
     }
-
     public function json( $language, Item $item ) {
         return Response::json( $item->getData() );
     }

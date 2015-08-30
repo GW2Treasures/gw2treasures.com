c74d25fd14a754cecf19177eec43d18cbc75b74a
darthmaim <darth.maim@arcor.de>
2015-08-30 10:01:05 +0200

    hide notification looking for french translators

--- app/views/static/notifications.blade.php
+++ app/views/static/notifications.blade.php
@@ -1,10 +1,5 @@
 <!-- notifications -->
 <ul id="notifications">
-	@if( App::getLocale() == 'fr' )
-		<li class="notification"><div class="pageWidth clearfix"><div class="notificationContent">
-			You <strong>speak french</strong> and want to support this project? <a href="{{ URL::route('contact', App::getLocale()) }}">Contact me now</a> to <strong>help translating</strong>!
-		</div></div></li>
-	@endif
 	@foreach( Notification::Notifications() as $n )
 		@include( 'static.notification', array( 'notification' => $n ) )
 	@endforeach

8a5c24a421488926499d8f7f81b1a6ff6aeef302
darthmaim <darth.maim@arcor.de>
2015-10-29 22:37:32 +0100

    set unlocked skins of transmutations

--- app/commands/ItemSetSkinCommand.php
+++ app/commands/ItemSetSkinCommand.php
@@ -15,9 +15,15 @@ class ItemSetSkinCommand extends Command {
 	public function fire() {
 		echo 'Setting Skin';
 		Item::where('skin_id','=','0')->chunk( 500, function( $items ) {
+			/** @var Item[] $items */
 			foreach( $items as $item ) {
 				if( isset( $item->getData( 'en' )->default_skin ) && $item->getData( 'en' )->default_skin != $item->skin_id ) {
 					$item->skin_id = $item->getData( 'en' )->default_skin;
+				} elseif( isset( $item->getTypeData('en')->skins ) && count( $item->getTypeData()->skins ) > 0 ) {
+					$item->skin_id = $item->getTypeData()->skins[0];
+				}
+
+				if( $item->getOriginal('skin_id') !== $item->skin_id ) {
 					CacheHelper::ClearItemDetails( $item );
 					CacheHelper::ClearItemTooltip( $item );
 					$item->save();

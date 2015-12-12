1c7fa1edb04dd0a0d99df3e6dffb710fd68f59fa
darthmaim <darth.maim@arcor.de>
2015-12-12 11:11:58 +0100

    Add winter 2015 design
    This reverts commit ead9c4e3fe5fa5927031c5d8facff83b21f3e8e1

--- app/views/start.blade.php
+++ app/views/start.blade.php
@@ -1,4 +1,30 @@
 <div class="banner">
+	<canvas id="snowcanvas"></canvas>
+
+	@section('winter2015.script')
+		<script src="{{ Helper::cdn('assets/js/jquery.let_it_snow.min.js') }}"></script>
+		<script type="text/javascript">
+			$(document).ready( function() {
+				var canvas = document.getElementById('snowcanvas');
+				var resizeCanvas = function() {
+					var scale = window.devicePixelRatio || 1;
+					var width  = canvas.offsetWidth,
+							height = canvas.offsetHeight;
+					if( canvas.width != width * scale || canvas.height != height * scale ) {
+						canvas.width = width * scale;
+						canvas.height = height * scale;
+					}
+				};
+				resizeCanvas();
+				$(window).on('resize', resizeCanvas);
+				$(canvas).let_it_snow({
+					speed: 0.1337,
+					size: 2 * (window.devicePixelRatio || 1)
+				});
+			});
+		</script>
+	@endsection
+
 	<div class="header">
 		{{ Helper::webp( Helper::cdn('assets2/img/header_img.webp'), Helper::cdn('assets2/img/header_img.png'), 800, 150, "GW2 Treasures" ) }}
 	</div>

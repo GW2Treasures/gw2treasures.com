fc29f229851ddca88f0e90d4470f832f5f7938ac
darthmaim <darth.maim@arcor.de>
2015-12-13 17:09:16 +0100

    update dependencies

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * An helper file for Laravel 4, to provide autocomplete information to your IDE
- * Generated for Laravel 4.2.17 on 2015-08-30.
+ * Generated for Laravel 4.2.17 on 2015-12-13.
  *
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  * @see https://github.com/barryvdh/laravel-ide-helper

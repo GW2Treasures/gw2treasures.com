c0dd14ffa2761b7c87ece4a1b4071821ee115476
darthmaim <darth.maim@arcor.de>
2016-03-26 10:18:42 +0100

    refactor commands loading entries from api

--- app/commands/LoadSkinsCommand.php
+++ app/commands/LoadSkinsCommand.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Console\Command;
 use GW2Treasures\GW2Api\GW2Api;
+use Symfony\Component\Console\Input\InputOption;
 
 class LoadSkinsCommand extends Command {
+    use LoadsEntries;
+
     protected $name = 'gw2treasures:loadskins';
     protected $description = 'Load Skins from API and store in database';
 
@@ -12,91 +15,24 @@ class LoadSkinsCommand extends Command {
         parent::__construct();
     }
 
-    private function insertIntoDB( $data ) {
-        if( count( $data ) == 0 ) {
-            return array();
+    public function fire() {
+        $api = new GW2Api();
+
+        $updating = $this->option('update');
+
+        if($updating) {
+            $this->info('updating existing entries');
         }
-        $this->info('Inserting ' . count( $data ) . ' entries into database...');
-        DB::table('skins')->insert( $data );
-        return array();
+
+        $this->loadEntries('skins', $api->skins(), ['id',
+            'name_de', 'name_en', 'name_es', 'name_fr',
+            'type', 'signature', 'file_id',
+            'data_de', 'data_en', 'data_es', 'data_fr'], $updating);
     }
 
-    public function fire()
-    {
-        $this->info( 'loading skins.json' );
-        $api = (new GW2Api())->skins();
-
-        $skins = $api->ids();
-
-        $count = count( $skins );
-        $this->info( $count . ' skins loaded' );
-
-
-        $this->info( 'loading known skins from database' );
-        $knownSkins = DB::table('skins')->lists( 'id' );
-        $this->info( count( $knownSkins ) . ' already known' );
-
-        $this->info( 'loading skin_details.json' );
-
-        $unknownSkins = [];
-        foreach( $skins as $id ) {
-            if( !in_array( $id, $knownSkins )) {
-                $unknownSkins[] = $id;
-            }
-        }
-
-        $keyByID = function(array $skins) {
-            $results = [];
-
-            foreach( $skins as $skin ) {
-                $results[$skin->id] = $skin;
-            }
-
-            return $results;
-        };
-
-        $skins_de = $keyByID($api->lang('de')->many($unknownSkins));
-        $skins_en = $keyByID($api->lang('en')->many($unknownSkins));
-        $skins_es = $keyByID($api->lang('es')->many($unknownSkins));
-        $skins_fr = $keyByID($api->lang('fr')->many($unknownSkins));
-
-        $data = array();
-
-        foreach( $unknownSkins as $i => $id ) {
-            if( !isset( $skins_en[$id]->name ) ) {
-                continue;
-            }
-
-            $this->info( '(' . ($i + 1) . '/' . $count . ') Loading [' . $id . '] ' . $skins_en[$id]->name );
-
-            preg_match('/\/(?<signature>[^\/]*)\/(?<file_id>[^\/]*)\.png$/', $skins_en[$id]->icon, $icon);
-            $signature = $icon['signature'];
-            $file_id = $icon['file_id'];
-
-            $data[] = array(
-                'id' => $id,
-
-                'name_de' => $skins_de[$id]->name,
-                'name_en' => $skins_en[$id]->name,
-                'name_es' => $skins_es[$id]->name,
-                'name_fr' => $skins_fr[$id]->name,
-
-                'type' => $skins_en[$id]->type,
-
-                'signature' => $signature,
-                'file_id'   => $file_id,
-
-                'data_de' => json_encode( $skins_de[$id] ),
-                'data_en' => json_encode( $skins_en[$id] ),
-                'data_es' => json_encode( $skins_es[$id] ),
-                'data_fr' => json_encode( $skins_fr[$id] )
-            );
-
-            if( count( $data ) == 250 ) {
-                $data = $this->insertIntoDB( $data );
-            }
-        }
-        $this->insertIntoDB( $data );
-        $this->info( 'Done.' );
+    protected function getOptions() {
+        return [
+            ['update', 'u', InputOption::VALUE_NONE, 'Update existing achievements']
+        ];
     }
 }
---
+++ app/commands/LoadsEntries.php
@@ -0,0 +1,110 @@
+<?php
+
+use GW2Treasures\GW2Api\V2\Bulk\IBulkEndpoint;
+use GW2Treasures\GW2Api\V2\Localization\ILocalizedEndpoint;
+
+trait LoadsEntries
+{
+    public abstract function info($string);
+
+    private function insertIntoDB( $table, $data ) {
+        if( count( $data ) == 0 ) {
+            return array();
+        }
+        $this->info('Inserting ' . count( $data ) . ' entries into database...');
+        DB::table($table)->insert( $data );
+        return array();
+    }
+
+    public function loadEntries($name, IBulkEndpoint $endpoint, array $columns, $updating) {
+        if(!($endpoint instanceof ILocalizedEndpoint)) {
+            return;
+        }
+
+        $this->info('loading '.$name);
+
+        $ids = $endpoint->ids();
+
+        $count = count( $ids );
+        $this->info( $count . ' '.$name.' loaded' );
+
+        $this->info( 'loading known '.$name.' from database' );
+        $knownIds = DB::table($name)->lists('id');
+        $this->info( count( $knownIds ) . ' already known' );
+
+        $this->info( 'loading '.$name.' details' );
+
+        $unknownEntries = [];
+        foreach( $ids as $id ) {
+            if( !in_array( $id, $knownIds ) || $updating) {
+                $unknownEntries[] = $id;
+            }
+        }
+
+        $keyByID = function(array $entries) {
+            $results = [];
+
+            foreach($entries as $entry) {
+                $results[$entry->id] = $entry;
+            }
+
+            return $results;
+        };
+
+        $entries_de = $keyByID($endpoint->lang('de')->many($unknownEntries));
+        $entries_en = $keyByID($endpoint->lang('en')->many($unknownEntries));
+        $entries_es = $keyByID($endpoint->lang('es')->many($unknownEntries));
+        $entries_fr = $keyByID($endpoint->lang('fr')->many($unknownEntries));
+
+        $data = [];
+
+        foreach( $unknownEntries as $i => $id ) {
+            if( !isset( $entries_en[$id]->name ) ) {
+                continue;
+            }
+
+            $this->info( '(' . ($i + 1) . '/' . $count . ') Loading [' . $id . '] ' . $entries_en[$id]->name );
+
+            $entryData = ['id' => $id];
+
+            foreach($columns as $column) {
+                if(Str::endsWith($column, ['_de', '_en', '_es', '_fr'])) {
+                    $strlen = strlen($column);
+                    $columnName = substr($column, 0, $strlen - 3);
+                    $lang = substr($column, $strlen - 2);
+
+                    $entry = ${'entries_' . $lang}[$id];
+                    $entryData[$column] = $columnName === 'data'
+                        ? json_encode($entry)
+                        : $entry->{$columnName};
+                } elseif($column === 'signature' || $column === 'file_id') {
+                    if( isset($entries_en[$id]->icon) ) {
+                        preg_match('/\/(?<signature>[^\/]*)\/(?<file_id>[^\/]*)\.png$/', $entries_en[$id]->icon, $icon);
+                        $entryData[$column] = $icon[$column];
+                    } else {
+                        $entryData[$column] = $column === 'file_id' ? 0 : '';
+                    }
+                } elseif($column === 'created_at' || $column === 'updated_at') {
+                    $entryData[$column] = date('Y-m-d H:i:s');
+                } else {
+                    $entryData[$column] = $entries_en[$id]->{$column};
+                }
+            }
+
+            if($updating && in_array($id, $knownIds)) {
+                if(array_key_exists('created_at', $entryData)) {
+                    unset($entryData['created_at']);
+                }
+                DB::table($name)->where('id', '=', $id)->update($entryData);
+            } else {
+                $data[] = $entryData;
+            }
+
+            if( count( $data ) == 250 ) {
+                $data = $this->insertIntoDB( $name, $data );
+            }
+        }
+        $this->insertIntoDB( $name, $data );
+        $this->info( 'Done.' );
+    }
+}

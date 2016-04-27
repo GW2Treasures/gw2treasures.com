02f55987637da9992e646513ef1048434f7ddd57
darthmaim <git@darthmaim.de>
2016-04-27 18:49:33 +0200

    move some inline styles into css

--- app/views/item/search/form.blade.php
+++ app/views/item/search/form.blade.php
@@ -11,39 +11,3 @@
         {{ Form::close() }}
     </div>
 </div>
-
-<style>
-    .search-box {
-        background: #eeeeee;
-        padding: 20px 0;
-    }
-    .search-box h2 {
-        margin-top: 0;
-    }
-    .search-box__main-input {
-        position: relative;
-    }
-    .search-box__main-input__input {
-        float: left;
-        width: calc(100% - 200px);
-        min-width: 66%;
-
-        font: inherit;
-        border: none;
-        padding: 8px 16px;
-    }
-    .search-box__main-input__button {
-        float: right;
-        width: 200px;
-        max-width: 34%;
-
-        font: inherit;
-        background: #ddd;
-        border: none;
-        padding: 8px 16px;
-    }
-    .search-box__toggle-advanced {
-        float: right;
-        line-height: 26px;
-    }
-</style>

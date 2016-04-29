51f717cacf018357ab2e84a46e376ef8ce3be5f5
darthmaim <git@darthmaim.de>
2016-04-29 13:03:16 +0200

    hide notifications from bots

--- app/views/static/notifications.blade.php
+++ app/views/static/notifications.blade.php
@@ -1,6 +1,8 @@
 <!-- notifications -->
 <ul id="notifications">
-	@foreach( Notification::Notifications() as $n )
-		@include( 'static.notification', array( 'notification' => $n ) )
-	@endforeach
+	@if(!str_contains(Request::header('User-Agent', ''), ['Googlebot', 'Yahoo! Slurp', 'bingbot', 'Yandex', 'DuckDuckBot']))
+		@foreach( Notification::Notifications() as $n )
+			@include( 'static.notification', array( 'notification' => $n ) )
+		@endforeach
+	@endif
 </ul>

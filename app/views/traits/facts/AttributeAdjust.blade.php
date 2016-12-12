b3b39436579022699cc8be3f35c736cb21bbf525
darthmaim <git@darthmaim.de>
2016-12-12 19:33:24 +0100

    update rendering of attribute adjust facts

--- app/views/traits/facts/AttributeAdjust.blade.php
+++ app/views/traits/facts/AttributeAdjust.blade.php
@@ -1,5 +1,12 @@
 @extends('traits.fact')
 
 @section($section.'text')
-    +{{ $fact->value }} {{ trans('item.attribute.'.$fact->target) }}
+    @if($fact->text)
+        {{$fact->text}}: +{{ $fact->value }}
+        @if($fact->target != 'None')
+            {{ trans('item.attribute.'.$fact->target) }}
+        @endif
+    @else
+    {{ trans('item.attribute.'.$fact->target) }}: +{{ $fact->value }}
+    @endif
 @endsection

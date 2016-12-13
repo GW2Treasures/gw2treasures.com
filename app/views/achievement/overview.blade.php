d562baef8d9331c60a1b87cf8086c036abc97d3c
darthmaim <git@darthmaim.de>
2016-12-13 13:08:38 +0100

    fix achievement sorting on overview page

--- app/views/achievement/overview.blade.php
+++ app/views/achievement/overview.blade.php
@@ -2,13 +2,13 @@
 
 <div class="pageWidth">
     <h2>{{trans('achievement.overview')}}</h2>
-    @foreach($groups->sort(Helper::sortByName()) as $group)
+    @foreach($groups as $group)
         @if(!in_array($group->id, $hidden['groups']))
             <h3 id="{{ $group->id }}">{{ $group->getName() }}</h3>
             <p>{{ $group->getDescription() }}</p>
 
             <ul class="itemList">
-                @foreach($group->categories as $category)
+                @foreach($group->categories->sort(Helper::sortByName()) as $category)
                     @if(!in_array($category->id, $hidden['categories']))
                         <li>{{ $category->link(32) }}
                     @endif

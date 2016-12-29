69df9f06f1efb71c5b6352a372df3f6c5e9fad28
darthmaim <git@darthmaim.de>
2016-12-29 09:53:21 +0100

    fix PrefixedBuff fact rendering

--- app/views/traits/facts/Buff.blade.php
+++ app/views/traits/facts/Buff.blade.php
@@ -3,11 +3,13 @@
 
 @section($section.'text')
     {{ isset($fact->apply_count) && $fact->apply_count > 1 ? $fact->apply_count.'&times; ' : '' }}
-    {{ $fact->status }}{{ isset($fact->duration) && $fact->duration > 0 ? ' ('.$fact->duration.'s)' : '' }}:
+    {{ isset($fact->status) ? $fact->status : $fact->prefix->status }}{{ isset($fact->duration) && $fact->duration > 0 ? ' ('.$fact->duration.'s)' : '' }}:
 @endsection
 
 @section($section.'extra')
-    @if(isset($fact->description))
+    @if(isset($fact->description) && $fact->description !== "")
         <div class="trait-fact__buff">{{ $fact->description }}</div>
+    @elseif(isset($fact->prefix->description) && $fact->prefix->description !== "")
+        <div class="trait-fact__buff">{{ $fact->prefix->description }}</div>
     @endif
 @endsection

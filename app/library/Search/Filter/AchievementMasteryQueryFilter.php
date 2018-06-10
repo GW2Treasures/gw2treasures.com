284cd166960115ec05508a2e4b563952453119de
darthmaim <git@darthmaim.de>
2018-06-10 18:47:29 +0200

    Add advanced search filter for achievement masteries

---
+++ app/library/Search/Filter/AchievementMasteryQueryFilter.php
@@ -0,0 +1,26 @@
+<?php
+
+class AchievementMasteryQueryFilter extends EnumSearchQueryFilter {
+    protected static $REGIONS = [
+        'Tyria' => 'Tyria',
+        'Maguuma' => 'Maguuma (Path of Fire)',
+        'Desert' => 'Crystal Desert (Heart of Thorns)'
+    ];
+    protected static $ANY = '*';
+
+    public function __construct($name) {
+        parent::__construct($name, [self::$ANY => 'Any mastery'] + self::$REGIONS);
+    }
+
+    public function query($query) {
+        $value = $this->getValue();
+
+        if($value && array_key_exists($value, self::$REGIONS)) {
+            return $query->where('data_en', 'like', '%"type":"Mastery","region":"'. $value.'"%');
+        } else if ($value === self::$ANY) {
+            return $query->where('data_en', 'like', '%"type":"Mastery","region"%');
+        }
+
+        return $query;
+    }
+}
--- app/library/Search/Results/AchievementSearchQueryResult.php
+++ app/library/Search/Results/AchievementSearchQueryResult.php
@@ -2,10 +2,16 @@
 
 class AchievementSearchQueryResult extends DatabaseSearchQueryResult {
     protected function getQuery() {
-        return $this->queryNameContains(Achievement::query(), $this->query->searchTerms)->with('category');
+        return $this->queryNameContains(Achievement::query(), $this->getSearchTermsWithoutFilters())->with('category');
     }
 
     public function render($data) {
         return View::make('search.result.generic', $data)->with('result', $this);
+    }
+
+    public function getInternalFilters() {
+        return [
+            'mastery' => new AchievementMasteryQueryFilter('mastery')
+        ];
     }
 }

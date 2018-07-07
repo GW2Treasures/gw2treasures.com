86f4f09262b5a401c364ce8a34b132cc28a431ed
darthmaim <git@darthmaim.de>
2018-07-07 14:46:45 +0200

    Merge both wvw regions into one table on overview
    This ensures that both regions use the same column width

--- app/views/wvw/overview.blade.php
+++ app/views/wvw/overview.blade.php
@@ -1,20 +1,23 @@
-<h2>{{ trans('wvw.region.na') }}</h2>
 <table class="wvw-table">
+    <thead><tr class="wvw-table__header-row"><th colspan="3">{{ trans('wvw.region.na') }}</th></tr></thead>
 	@include('wvw.head')
 	@foreach( $matches as $match )
 		@if( $match->region == Match::REGION_US )
 			@include( 'wvw.smallMatchBox' )
 		@endif
 	@endforeach
-</table>
-
-<h2>{{ trans('wvw.region.eu') }}</h2>
-
-<table class="wvw-table">
-	@include('wvw.head')
+    <thead><tr class="wvw-table__header-row"><th colspan="3">{{ trans('wvw.region.eu') }}</th></tr></thead>
+    @include('wvw.head')
 	@foreach( $matches as $match )
 		@if( $match->region == Match::REGION_EU )
 			@include( 'wvw.smallMatchBox' )
 		@endif
 	@endforeach
 </table>
+
+<style>
+    .wvw-table__header-row th {
+        font: 20px Bitter, sans-serif;
+        padding: 8px 4px;
+    }
+</style>

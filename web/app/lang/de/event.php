26ee7dd12dd49150cff9d96e154e3b2abd91cd05
darthmaim <git@darthmaim.de>
2021-03-27 17:21:13 +0100

    [web] Add missing event translations

---
+++ web/app/lang/de/event.php
@@ -0,0 +1,12 @@
+<?php
+
+return array (
+  'breadcrumb' => 'Event',
+  'rewards' => 
+  array (
+    'header' => 'Belohnung (Level :level)',
+    'gold' => 'Gold',
+    'silver' => 'Silber',
+    'bronze' => 'Bronze',
+  ),
+);
---
+++ web/app/lang/en/event.php
@@ -0,0 +1,12 @@
+<?php
+
+return array (
+  'breadcrumb' => 'Event',
+  'rewards' => 
+  array (
+    'header' => 'Rewards (level :level)',
+    'gold' => 'Gold',
+    'silver' => 'Silver',
+    'bronze' => 'Bronze',
+  ),
+);

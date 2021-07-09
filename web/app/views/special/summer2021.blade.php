8c3dec812e56f5522343e32df7094c53e93c268c
darthmaim <git@darthmaim.de>
2021-07-09 16:36:02 +0200

    Add more achievements to summer 2021 page

--- web/app/views/special/summer2021.blade.php
+++ web/app/views/special/summer2021.blade.php
@@ -32,7 +32,7 @@
                         <a href="{{ URL::route('wvw', App::getLocale()) }}">{{ trans('header.wvw') }}</a>
                     @endif
                     @if($key === '2021-5-25-living-world')
-                        <?php $achievements = Achievement::remember(60)->whereIn('id', [5773, 5804, 5829, 5758])->get(); ?>
+                        <?php $achievements = Achievement::remember(60)->whereIn('id', [5773, 5804, 5829, 5758, 5742, 5743])->get(); ?>
                         <ul class="itemList">
                             @foreach($achievements as $achievement)
                                 <li>{{ $achievement->link(32) }}</li>

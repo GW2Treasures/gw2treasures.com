f308baa2c65bb67ff531827d2dae8b430c0d6788
darthmaim <git@darthmaim.de>
2021-08-24 14:03:20 +0200

    Unset removed_from_api for items

--- web/app/commands/RemovedItemsCommand.php
+++ web/app/commands/RemovedItemsCommand.php
@@ -4,32 +4,33 @@ use GW2Treasures\GW2Api\GW2Api;
 use Illuminate\Console\Command;
 
 class RemovedItemsCommand extends Command {
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'gw2treasures:removed-items';
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'gw2treasures:removed-items';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Marks items as removed from API';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Marks items as removed from API';
 
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function fire()
-	{
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
         $api = new GW2Api();
 
         $ids = $api->items()->ids();
 
+        Item::query()->whereIn('id', $ids)->update(['removed_from_api' => false]);
         Item::query()->whereNotIn('id', $ids)->update(['removed_from_api' => true]);
-	}
+    }
 }

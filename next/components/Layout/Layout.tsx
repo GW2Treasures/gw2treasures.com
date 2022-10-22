e0cba0b6939b3e6b9756f4c5f065d68005fa11f2
darthmaim <git@darthmaim.de>
2022-10-22 13:39:49 +0200

    Some more layout

--- next/components/Layout/Layout.tsx
+++ next/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, ReactNode, useState } from 'react';
+import { FunctionComponent, ReactNode, useEffect, useState } from 'react';
 import styles from './Layout.module.css';
 import Icon from '../../icons/Icon';
 import Navigation from './Navigation';
@@ -10,26 +10,42 @@ interface LayoutProps {
 
 const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [scrolledDown, setScrolledDown] = useState('window' in global && window.scrollY > 0);
+
+  useEffect(() => {
+    const listener = () => {
+      setScrolledDown(window.scrollY > 0);
+    };
+    window.addEventListener('scroll', listener, { passive: true });
+    return () => window.removeEventListener('scroll', listener);
+  }, []);
 
   return (
-    <div className={styles.layout}>
-      <div className={styles.header}>
-        <header className={styles.headerMain}>
-          <button className={styles.menuButton} onClick={() => setMenuOpen(!menuOpen)}>
-            <Icon icon="menu"/>
-          </button>
-          <Icon icon="gw2treasures"/>
-          <div className={styles.title}>
-            <Link href="/">gw2treasures.com</Link>
-          </div>
-        </header>
-        <nav className={menuOpen ? styles.headerNavVisible : styles.headerNav}>
-          <Navigation/>
-        </nav>
+    <div>
+      <div className={styles.layout}>
+        <div className={styles.header}>
+          <header className={scrolledDown ? styles.headerMainScrolled : styles.headerMain}>
+            <button className={styles.menuButton} onClick={() => setMenuOpen(!menuOpen)}>
+              <Icon icon="menu"/>
+            </button>
+            <div className={styles.title}>
+              <Icon icon="gw2treasures"/>
+              <Link href="/">gw2treasures.com</Link>
+            </div>
+          </header>
+          <nav className={menuOpen ? styles.headerNavVisible : styles.headerNav}>
+            <Navigation/>
+          </nav>
+        </div>
+        <hr className={styles.headerShadow}/>
+          {children}
+        <footer className={styles.footer}>
+          <span><b>gw2treasures.com</b> by darthmaim &copy; {new Date().getFullYear()}</span>
+          <Link href="/about">About</Link>
+        </footer>
       </div>
-      <hr className={styles.headerShadow}/>
-        {children}
-      <footer className={styles.footer}>Footer</footer>
+      <div className={styles.disclaimer}>This site is not affiliated with ArenaNet, Guild Wars 2, or any of their partners. All copyrights reserved to their respective owners.</div>
+      <div className={styles.disclaimer}>© 2014 ArenaNet, Inc. All rights reserved. NCsoft, the interlocking NC logo, ArenaNet, Guild Wars, Guild Wars Factions, Guild Wars Nightfall, Guild Wars: Eye of the North, Guild Wars 2, and all associated logos and designs are trademarks or registered trademarks of NCsoft Corporation. All other trademarks are the property of their respective owners.</div>
     </div>
   );
 };

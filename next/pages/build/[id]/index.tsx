d61bca7dd0a5fb4a14bd0b664f15b85ac1cb2da5
darthmaim <git@darthmaim.de>
2022-11-21 14:55:20 +0100

    Add build detail page

---
+++ next/pages/build/[id]/index.tsx
@@ -0,0 +1,69 @@
+import { GetStaticPaths, NextPage } from 'next';
+import { Build, Item, Language, Skill } from '@prisma/client';
+import DetailLayout from '../../../components/Layout/DetailLayout';
+import { Skeleton } from '../../../components/Skeleton/Skeleton';
+import { db } from '../../../lib/prisma';
+import { getStaticSuperProps, withSuperProps } from '../../../lib/superprops';
+import { FormatDate } from '../../../components/Format/FormatDate';
+import { Headline } from '../../../components/Headline/Headline';
+
+export interface BuildDetailProps {
+  build: Build;
+  items: Item[];
+  skills: Skill[];
+}
+
+const BuildDetail: NextPage<BuildDetailProps> = ({ build, items, skills }) => {
+  if(!build) {
+    return <DetailLayout title={<Skeleton/>} breadcrumb={<Skeleton/>}><Skeleton/></DetailLayout>;
+  }
+
+  return (
+    <DetailLayout title={build.id} breadcrumb="Build">
+      Released on <FormatDate date={build.createdAt}/>
+
+      <Headline id="items">Updated items</Headline>
+      {items.length}
+
+      <Headline id="skills">Updated skills</Headline>
+      {skills.length}
+    </DetailLayout>
+  );
+};
+
+export const getStaticProps = getStaticSuperProps<BuildDetailProps>(async ({ params, locale }) => {
+  const id: number = Number(params!.id!.toString())!;
+  const language = (locale ?? 'en') as Language;
+
+  const [build, items, skills] = await Promise.all([
+    db.build.findUnique({
+      where: { id },
+    }),
+    db.item.findMany({
+      where: { history: { some: { revision: { buildId: id, description: 'Updated in API' }}}},
+    }),
+    db.skill.findMany({
+      where: { history: { some: { revision: { buildId: id, description: 'Updated in API' }}}},
+    }),
+  ]);
+
+  if(!build) {
+    return {
+      notFound: true
+    };
+  }
+
+  return {
+    props: { build, items, skills },
+    revalidate: 600 /* 10 minutes */
+  };
+});
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+export default withSuperProps(BuildDetail);
--- next/pages/build/index.tsx
+++ next/pages/build/index.tsx
@@ -4,10 +4,7 @@ import { db } from '../../lib/prisma';
 import { FormatDate } from '../../components/Format/FormatDate';
 import { Headline } from '../../components/Headline/Headline';
 import { ItemList } from '../../components/ItemList/ItemList';
-import { SkinLink } from '../../components/Skin/SkinLink';
 import { getServerSideSuperProps, withSuperProps } from '../../lib/superprops';
-import { ItemIcon } from '../../components/Item/ItemIcon';
-import Link from 'next/link';
 import { FormatNumber } from '../../components/Format/FormatNumber';
 
 interface BuildPageProps {
@@ -32,7 +29,7 @@ const BuildPage: NextPage<BuildPageProps> = ({ builds }) => {
 export const getServerSideProps = getServerSideSuperProps<BuildPageProps>(async ({}) => {
   const builds = await db.build.findMany({
     orderBy: { id: 'desc' },
-    include: { _count: { select: { revisions: true }}},
+    include: { _count: { select: { revisions: { where: { description: 'Updated in API' }}}}},
     where: { id: { not: 0 }}
   });
 

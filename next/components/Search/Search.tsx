7f05d192f98410ff577055bbd0d584439f81285c
darthmaim <git@darthmaim.de>
2022-11-29 16:56:17 +0100

    add useListNavigation to search

--- next/components/Search/Search.tsx
+++ next/components/Search/Search.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router';
 import { useBuildsResults, useItemResults, usePageResults, useSkillResults, useSkinResults } from './useSearchResults';
 import Link from 'next/link';
 import { useDebounce } from '../../lib/useDebounce';
-import { autoUpdate, offset, useDismiss, useFloating, useFocus, useInteractions } from '@floating-ui/react-dom-interactions';
+import { autoUpdate, offset, useClick, useDismiss, useFloating, useFocus, useInteractions, useListNavigation } from '@floating-ui/react-dom-interactions';
 
 export interface SearchProps {
   // TODO: add props
@@ -16,14 +16,10 @@ export const Search: FC<SearchProps> = ({ }) => {
   const [value, setValue] = useState('');
   const searchValue = useDebounce(value);
   const searchForm = useRef<HTMLFormElement>(null);
+  const listRef = useRef<(HTMLAnchorElement | null)[]>([]);
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
   const router = useRouter();
-
-  useEffect(() => {
-    if(window.document.activeElement && searchForm.current?.contains(window.document.activeElement)) {
-      (window.document.activeElement as any).blur();
-    }
-  }, [router.asPath]);
 
   const { x, y, reference, floating, strategy, context, middlewareData, placement } = useFloating({
     open,
@@ -36,9 +32,11 @@ export const Search: FC<SearchProps> = ({ }) => {
   });
 
   // Merge all the interactions into prop getters
-  const { getReferenceProps, getFloatingProps } = useInteractions([
+  const { getReferenceProps, getFloatingProps, getItemProps } = useInteractions([
     useFocus(context, { keyboardOnly: false }),
+    useClick(context, { toggle: false }),
     useDismiss(context),
+    useListNavigation(context, { listRef, activeIndex, onNavigate: setActiveIndex, virtual: true, loop: true }),
   ]);
 
   const searchResults = [
@@ -49,29 +47,42 @@ export const Search: FC<SearchProps> = ({ }) => {
     useBuildsResults(searchValue),
   ];
 
+  let index = 0;
+
   return (
     <form className={styles.search} ref={searchForm}>
       <Icon icon="search"/>
       {/* <div className={styles.restriciton}>Item</div> */}
-      <input className={styles.searchInput} placeholder="Search (ALT + Q)" accessKey="q" value={value} onChange={(e) => {setValue(e.target.value); setOpen(true)}} ref={reference} {...getReferenceProps()}/>
+      <input className={styles.searchInput} placeholder="Search (ALT + Q)" accessKey="q" value={value} onChange={(e) => { setValue(e.target.value); setOpen(true); }} ref={reference} {...getReferenceProps({
+        onKeyDown: (e) => {
+          if(e.key === 'Enter' && activeIndex !== null) {
+            router.push(listRef.current[activeIndex]!.href);
+            setOpen(false);
+            e.preventDefault();
+          }
+        }
+      })}/>
       {open && (
         <div className={styles.dropdown} ref={floating} {...getFloatingProps()}>
           {searchResults.map(({ title, results }) => results.length > 0 && (
             <>
               <div className={styles.category}>{title}</div>
-              {results.map((result) => (
-                <Link href={result.href} key={result.href} className={styles.result}>
-                  {result.icon}
-                  <div className={styles.title}>
-                    {result.title}
-                  </div>
-                  {result.subtitle && (
-                    <div className={styles.subtitle}>
-                      {result.subtitle}
+              {results.map((result) => {
+                const currentIndex = index++;
+                return (
+                  <Link tabIndex={-1} href={result.href} key={result.href} className={activeIndex === currentIndex ? styles.resultActive : styles.result} id={result.href} ref={(node) => listRef.current[currentIndex] = node} {...getItemProps()} onClick={() => setOpen(false)}>
+                    {result.icon}
+                    <div className={styles.title}>
+                      {result.title}
                     </div>
-                  )}
-                </Link>
-              ))}
+                    {result.subtitle && (
+                      <div className={styles.subtitle}>
+                        {result.subtitle}
+                      </div>
+                    )}
+                  </Link>
+                );
+              })}
             </>
           ))}
         </div>

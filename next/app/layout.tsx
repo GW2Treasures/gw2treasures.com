8b36fb18a8b13d2d5f97d980bcec7424b1984d5d
darthmaim <git@darthmaim.de>
2023-02-27 14:35:56 +0100

    Add workaround for facebook/react#25994

--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -6,6 +6,53 @@ import '@fontsource/bitter/700.css';
 import { FormatProvider } from '@/components/Format/FormatContext';
 import Layout from '@/components/Layout/Layout';
 
+const __html = `
+/**
+ * !!WARNING!!
+ * TEMPORARILY WORKAROUND A REACT DEVTOOLS ISSUE https://github.com/facebook/react/issues/25994
+ * REMOVE AFTER THE ISSUE IS FIXED
+ */
+// Save the original __REACT_DEVTOOLS_GLOBAL_HOOK__.inject
+const reactDevToolsHookInject = window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject;
+// Override the original __REACT_DEVTOOLS_GLOBAL_HOOK__.inject
+// This will allow us to intercept and modify incoming injectProfilingHooks
+window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = function inject(...args) {
+  const newArgs = args.map(arg => {
+    // Only modify the original arguments when injectProfilingHooks is present
+    if (!arg || !arg.injectProfilingHooks) return arg;
+
+    const { injectProfilingHooks: originalInjectProfilingHooks, ...rest } = arg;
+    return {
+      // Override the original injectProfilingHooks
+      // This will allow us to intercept and modify incoming hooks
+      injectProfilingHooks(...hooks) {
+        const newHooks = hooks.map(hook => {
+          // Only modify the original hooks when markComponentSuspended is present
+          if (!hook || !hook.markComponentSuspended) return hook;
+
+          // Override the original markComponentSuspended from the hook
+          const { markComponentSuspended: orignalMarkComponentSuspended, ...rest2 } = hook;
+          return {
+            markComponentSuspended(fiber, wakeable, lanes) {
+              if (typeof wakeable.then === 'function') {
+                return orignalMarkComponentSuspended.call(this, fiber, wakeable, lanes);
+              } else {
+                // If "wakeable.then" is not a function, log a warning.
+                console.warn('React DevTools issue detected and mitigated! See https://github.com/facebook/react/issues/25994 for more information.', { fiber, wakeable, lanes });
+              }
+            },
+            ...rest2
+          };
+        });
+        originalInjectProfilingHooks.apply(this, newHooks);
+      },
+      ...rest
+    };
+  });
+  return reactDevToolsHookInject.apply(this, newArgs);
+};
+`;
+
 export default function RootLayout({
   children,
 }: {
@@ -13,7 +60,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body><FormatProvider><Layout>{children}</Layout></FormatProvider></body>
+      <body>
+        <FormatProvider><Layout>{children}</Layout></FormatProvider>
+        <script dangerouslySetInnerHTML={{ __html }}/>
+      </body>
     </html>
   );
 }

1d52b724baefd336afaf13b90452882594d97030
darthmaim <git@darthmaim.de>
2023-02-28 14:44:15 +0100

    Force dynamic rendering

--- next/app/achievement/random/route.ts
+++ next/app/achievement/random/route.ts
@@ -1,7 +1,11 @@
 import { notFound, redirect } from 'next/navigation';
 import { db } from '@/lib/prisma';
+import { cookies } from 'next/headers';
 
 export async function GET() {
+  // force dynamic rendering, because the db is not availabe at build time
+  cookies();
+
   const count = await db.achievement.count();
 
   const achievement = await db.achievement.findFirst({ take: 1, skip: Math.floor(Math.random() * count), select: { id: true }});

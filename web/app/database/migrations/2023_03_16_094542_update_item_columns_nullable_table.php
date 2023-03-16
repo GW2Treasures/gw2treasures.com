f8d1f741fd60b2fc84b3b6cd880dfc31e9ea226b
darthmaim <git@darthmaim.de>
2023-03-16 10:07:12 +0100

    Fix migration name

--- web/app/database/migrations/2023_03_16_094542_update_item_columns_nullable_table.php
+++ web/app/database/migrations/2023_03_16_094542_update_item_columns_nullable_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateItemColumnNullable extends Migration {
+class UpdateItemColumnsNullableTable extends Migration {
 
     /**
      * Run the migrations.

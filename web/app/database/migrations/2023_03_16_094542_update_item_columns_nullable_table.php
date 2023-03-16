6bf6ba9c2213cfb624387f3d96d1af14b38d6f06
darthmaim <git@darthmaim.de>
2023-03-16 10:10:04 +0100

    Fix migration

--- web/app/database/migrations/2023_03_16_094542_update_item_columns_nullable_table.php
+++ web/app/database/migrations/2023_03_16_094542_update_item_columns_nullable_table.php
@@ -13,7 +13,7 @@ class UpdateItemColumnsNullableTable extends Migration {
     public function up()
     {
         // set default to ''
-        DB::query("ALTER TABLE `items`
+        DB::statement("ALTER TABLE `items`
             CHANGE `desc_de` `desc_de` TEXT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT '',
             CHANGE `desc_en` `desc_en` TEXT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT '',
             CHANGE `desc_es` `desc_es` TEXT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT '',
@@ -28,7 +28,7 @@ class UpdateItemColumnsNullableTable extends Migration {
     public function down()
     {
         // set default to ''
-        DB::query("ALTER TABLE `items`
+        DB::statement("ALTER TABLE `items`
             CHANGE `desc_de` `desc_de` TEXT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
             CHANGE `desc_en` `desc_en` TEXT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
             CHANGE `desc_es` `desc_es` TEXT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,

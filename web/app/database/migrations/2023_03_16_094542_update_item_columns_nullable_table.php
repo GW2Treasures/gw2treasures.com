341f2e5d89dfb71055f2a7b7d8038caaf28d0fbf
darthmaim <git@darthmaim.de>
2023-03-16 09:55:34 +0100

    Add default value for item description column

---
+++ web/app/database/migrations/2023_03_16_094542_update_item_columns_nullable_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGuildUpgradeTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // set default to ''
+        DB::query("ALTER TABLE `items`
+            CHANGE `desc_de` `desc_de` TEXT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT '',
+            CHANGE `desc_en` `desc_en` TEXT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT '',
+            CHANGE `desc_es` `desc_es` TEXT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT '',
+            CHANGE `desc_fr` `desc_fr` TEXT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT '';");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // set default to ''
+        DB::query("ALTER TABLE `items`
+            CHANGE `desc_de` `desc_de` TEXT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+            CHANGE `desc_en` `desc_en` TEXT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+            CHANGE `desc_es` `desc_es` TEXT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+            CHANGE `desc_fr` `desc_fr` TEXT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL;");
+    }
+}

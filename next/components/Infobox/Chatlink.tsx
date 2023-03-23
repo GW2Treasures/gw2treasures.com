96f6df635c54f20f1828933633352fa405dd46b3
darthmaim <git@darthmaim.de>
2023-03-23 19:13:55 +0100

    Extract Chatlink into own component

---
+++ next/components/Infobox/Chatlink.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import Icon from 'icons/Icon';
+import { FC } from 'react';
+import styles from './Chatlink.module.css';
+
+export interface ChatlinkProps {
+  chatlink: string;
+}
+
+export const Chatlink: FC<ChatlinkProps> = ({ chatlink }) => {
+  return (
+    <label className={styles.box}>
+      <span className={styles.icon}><Icon icon="chatlink"/></span>
+      <input readOnly value={chatlink} className={styles.input} onFocus={(e) => e.target.select()}/>
+    </label>
+  );
+};
--- next/components/Item/ItemInfobox.tsx
+++ next/components/Item/ItemInfobox.tsx
@@ -3,6 +3,7 @@ import { Gw2Api } from 'gw2-api-types';
 import { FC } from 'react';
 import Icon from '../../icons/Icon';
 import { Headline } from '../Headline/Headline';
+import { Chatlink } from '../Infobox/Chatlink';
 import { LanguageLinks } from '../Infobox/LanguageLinks';
 import { ExternalLink } from '../Link/ExternalLink';
 import { ItemLink } from './ItemLink';
@@ -37,10 +38,7 @@ export const ItemInfobox: FC<ItemInfoboxProps> = ({ item, data, language }) => {
 
       <ExternalLink href={`https://api.guildwars2.com/v2/items/${item.id}?v=latest&lang=${language}`} target="api">API</ExternalLink>
 
-      <div style={{ display: 'flex', gap: 12, margin: '8px 0', alignItems: 'center', borderRadius: 0, border: '1px solid var(--color-border)', paddingLeft: 12 }}>
-        <Icon icon="chatlink"/>
-        <input readOnly value={data.chat_link} style={{ margin: 0, padding: '7px 12px 9px 12px', border: 0, background: 'inherit', color: 'inherit', flex: 1, font: 'inherit', borderLeft: '1px solid var(--color-border)' }}/>
-      </div>
+      <Chatlink chatlink={data.chat_link}/>
     </div>
   );
 };

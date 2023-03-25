b54fc2bdae80077438d23e90bc6a139cf7b6d441
darthmaim <git@darthmaim.de>
2023-03-25 16:33:06 +0100

    Add cache header for tooltip

--- next/app/[language]/item/[id]/tooltip/route.ts
+++ next/app/[language]/item/[id]/tooltip/route.ts
@@ -24,5 +24,5 @@ export async function GET(request: NextRequest, { params: { language, id }}: { p
   const data: Gw2Api.Item = JSON.parse(revision.data);
   const tooltip = await createTooltip(data, language);
 
-  return NextResponse.json(tooltip);
+  return NextResponse.json(tooltip, { headers: { 'cache-control': 'public, max-age=3600' }});
 }

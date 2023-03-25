11e53df5ef2045c9b5aa780c9428f13894014f23
darthmaim <git@darthmaim.de>
2023-03-25 15:24:36 +0100

    Cache tooltip route db query

--- next/app/[language]/item/[id]/tooltip/route.ts
+++ next/app/[language]/item/[id]/tooltip/route.ts
@@ -4,13 +4,18 @@ import { Language } from '@prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
 import { createTooltip } from '@/components/Item/ItemTooltip';
 import { Gw2Api } from 'gw2-api-types';
+import { remember } from '@/lib/remember';
 
 export const dynamic = 'force-dynamic';
+
+const getItemRevision = remember(60, function getItemRevision(id: number, language: Language) {
+  return db.revision.findFirst({ where: { [`currentItem_${language}`]: { id }}});
+});
 
 export async function GET(request: NextRequest, { params: { language, id }}: { params: { language: Language, id: string }}) {
   const itemId = Number(id);
 
-  const revision = await db.revision.findFirst({ where: { [`currentItem_${language}`]: { id: itemId }}});
+  const revision = await getItemRevision(itemId, language);
 
   if(!revision) {
     notFound();

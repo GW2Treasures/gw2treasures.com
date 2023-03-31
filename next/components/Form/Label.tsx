2a5793d246d6f3bb0ba200c9a5241cbd0ce3ee0a
darthmaim <git@darthmaim.de>
2023-03-31 22:58:55 +0200

    Add Label component

---
+++ next/components/Form/Label.tsx
@@ -0,0 +1,16 @@
+import { FC, ReactNode } from 'react';
+import styles from './Label.module.css';
+
+export interface LabelProps {
+  label: ReactNode;
+  children: ReactNode;
+}
+
+export const Label: FC<LabelProps> = ({ label, children }) => {
+  return (
+    <label className={styles.label}>
+      <div>{label}</div>
+      {children}
+    </label>
+  );
+};

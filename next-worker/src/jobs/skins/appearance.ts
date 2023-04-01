fc84e87b3a59c65e716dbe8c665ef1adea00f1a9
darthmaim <git@darthmaim.de>
2023-04-01 15:14:39 +0200

    Add skin appearance from sets

--- next-worker/src/jobs/skins/appearance.ts
+++ next-worker/src/jobs/skins/appearance.ts
@@ -3,11 +3,12 @@ import { db } from '../../db';
 import { Job } from '../job';
 
 interface WikiAskResponse {
+  'query-continue-offset'?: number,
   query: {
     results: Record<string, {
       printouts: {
-        'Has appearance': Array<{ fulltext: string }>
         'Has game id': Array<number>
+        'Has appearance': { 0?: { fulltext: string }}
       }
       fulltext: string
     }>,
@@ -15,12 +16,41 @@ interface WikiAskResponse {
 }
 
 export const SkinsAppearance: Job = {
-  run: async () => {
-    const query = '[[Has context::Skin]][[Has appearance::+]][[Has game id::+]]|?Has appearance|?Has game id|limit=10000';
+  run: async ({ offset = 0, set }: { offset?: number, set?: boolean }) => {
+    if(set === undefined) {
+      await db.job.createMany({
+        data: [
+          { type: 'skins.appearance', data: { offset: 0, set: false }},
+          { type: 'skins.appearance', data: { offset: 0, set: true }}
+        ]
+      });
+
+      return 'Queued follow up jobs';
+    }
+
+    const query = set
+      ? `[[Has context::Skin]][[Has game id::+]][[Has skin set.Has appearance::+]]|?Has game id|?Has skin set.Has appearance|limit=5000|offset=${offset}`
+      : `[[Has context::Skin]][[Has game id::+]][[Has appearance::+]]|?Has game id|?Has appearance|limit=5000|offset=${offset}`;
     const url = `https://wiki.guildwars2.com/api.php?action=ask&query=${encodeURIComponent(query)}&format=json`;
 
     console.log('> Fetch images for skins from wiki');
-    const data: WikiAskResponse = await fetch(url).then((r) => r.json());
+    console.log(url);
+
+    const data: WikiAskResponse = await fetch(url).then((r) => {
+      if(r.status !== 200) {
+        throw new Error(`${url} returned ${r.status} ${r.statusText}`);
+      }
+
+      return r.json();
+    });
+
+    if(data['query-continue-offset']) {
+      if(data['query-continue-offset'] > offset) {
+        await db.job.create({ data: { type: 'skins.appearance', data: { set, offset: data['query-continue-offset'] }}});
+      } else {
+        console.warn('> Reached max offset');
+      }
+    }
 
     const results = Object.values(data.query.results);
     console.log(`> Update images for ${results.length} skins`);
@@ -29,7 +59,12 @@ export const SkinsAppearance: Job = {
 
     for(const result of results) {
       const ids = result.printouts['Has game id'];
-      const image = result.printouts['Has appearance'][0].fulltext;
+
+      const image = result.printouts['Has appearance'][0]?.fulltext;
+
+      if(!image) {
+        continue;
+      }
 
       const u = await db.skin.updateMany({
         where: {

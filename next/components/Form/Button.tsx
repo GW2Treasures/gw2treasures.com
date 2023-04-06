69e4753dd908be800c720470330ca58f353db422
darthmaim <git@darthmaim.de>
2023-04-06 02:58:55 +0200

    Add icon prop to Button

--- next/components/Form/Button.tsx
+++ next/components/Form/Button.tsx
@@ -1,18 +1,24 @@
 import { cx } from '@/lib/classNames';
+import { IconName } from 'icons';
+import Icon, { IconProps } from 'icons/Icon';
 import Link from 'next/link';
 import { forwardRef, ReactNode } from 'react';
 import styles from './Button.module.css';
 
 export interface ButtonProps {
   children: ReactNode;
+  icon?: IconProps['icon'];
   appearance?: 'primary' | 'secondary' | 'menu';
   iconOnly?: boolean;
   onClick?: () => void;
 };
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button({ children, appearance = 'secondary', iconOnly, onClick }, ref) {
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button({ children, icon, appearance = 'secondary', iconOnly, onClick }, ref) {
   return (
-    <button ref={ref} onClick={onClick} className={cx(styles[appearance], iconOnly && styles.iconOnly)}>{children}</button>
+    <button ref={ref} onClick={onClick} className={cx(styles[appearance], iconOnly && styles.iconOnly)}>
+      {icon && <Icon icon={icon}/>}
+      <span>{children}</span>
+    </button>
   );
 });
 
@@ -22,8 +28,11 @@ export interface LinkButtonProps extends ButtonProps {
   prefetch?: boolean;
 }
 
-export const LinkButton = forwardRef<HTMLAnchorElement, LinkButtonProps & Pick<React.AnchorHTMLAttributes<HTMLAnchorElement>, 'target' | 'rel'>>(function Button({ children, appearance = 'secondary', iconOnly, onClick, href, locale, prefetch, ...props }, ref) {
+export const LinkButton = forwardRef<HTMLAnchorElement, LinkButtonProps & Pick<React.AnchorHTMLAttributes<HTMLAnchorElement>, 'target' | 'rel'>>(function Button({ children, icon, appearance = 'secondary', iconOnly, onClick, href, locale, prefetch, ...props }, ref) {
   return (
-    <Link ref={ref} className={cx(styles[appearance], iconOnly && styles.iconOnly)} href={href} locale={locale} onClick={onClick} prefetch={prefetch} {...props}>{children}</Link>
+    <Link ref={ref} className={cx(styles[appearance], iconOnly && styles.iconOnly)} href={href} locale={locale} onClick={onClick} prefetch={prefetch} {...props}>
+      {icon && <Icon icon={icon}/>}
+      <span>{children}</span>
+    </Link>
   );
 });

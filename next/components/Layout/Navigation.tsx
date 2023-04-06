59952647aa88dc54a0b2fd2b33899ab81d0a09c9
darthmaim <git@darthmaim.de>
2023-04-06 03:54:06 +0200

    Use button icon prop in Navigation

--- next/components/Layout/Navigation.tsx
+++ next/components/Layout/Navigation.tsx
@@ -1,6 +1,4 @@
-import Link from 'next/link';
 import React, { FunctionComponent } from 'react';
-import Icon from '../../icons/Icon';
 import { LinkButton } from '../Form/Button';
 import styles from './Navigation.module.css';
 
@@ -11,14 +9,14 @@ interface NavigationProps {
 const Navigation: FunctionComponent<NavigationProps> = () => {
   return (
     <ul className={styles.navigation}>
-      <li className={styles.item}><LinkButton appearance="menu" href="/item"><Icon icon="item"/> Items</LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/achievement"><Icon icon="achievement"/> Achievements</LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/skin"><Icon icon="skin"/> Skins</LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/profession"><Icon icon="profession"/> Professions</LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/specialization"><Icon icon="specialization"/> Specializations</LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/skill"><Icon icon="skill"/> Skills</LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/mount"><Icon icon="mount"/> Mounts</LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/wvw"><Icon icon="wvw"/> World versus World</LinkButton></li>
+      <li className={styles.item}><LinkButton appearance="menu" href="/item" icon="item">Items</LinkButton></li>
+      <li className={styles.item}><LinkButton appearance="menu" href="/achievement" icon="achievement">Achievements</LinkButton></li>
+      <li className={styles.item}><LinkButton appearance="menu" href="/skin" icon="skin">Skins</LinkButton></li>
+      <li className={styles.item}><LinkButton appearance="menu" href="/profession" icon="profession">Professions</LinkButton></li>
+      <li className={styles.item}><LinkButton appearance="menu" href="/specialization" icon="specialization">Specializations</LinkButton></li>
+      <li className={styles.item}><LinkButton appearance="menu" href="/skill" icon="skill">Skills</LinkButton></li>
+      <li className={styles.item}><LinkButton appearance="menu" href="/mount" icon="mount">Mounts</LinkButton></li>
+      <li className={styles.item}><LinkButton appearance="menu" href="/wvw" icon="wvw">World versus World</LinkButton></li>
     </ul>
   );
 };

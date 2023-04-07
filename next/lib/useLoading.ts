fbd172e6c846bb40f3c59eba49457a42edfb43c1
darthmaim <git@darthmaim.de>
2023-04-07 03:01:28 +0200

    Comment unused imports in useLoading

--- next/lib/useLoading.ts
+++ next/lib/useLoading.ts
@@ -1,5 +1,5 @@
-import { useRouter } from 'next/router';
-import { useCallback, useEffect, useState } from 'react';
+// import { useRouter } from 'next/router';
+// import { useCallback, useEffect, useState } from 'react';
 
 export function useLoading(): boolean {
   // const router = useRouter();

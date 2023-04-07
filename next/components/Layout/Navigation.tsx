6b2a6c5e6fbba5f77d68d89339f4d7206e86870e
darthmaim <git@darthmaim.de>
2023-04-07 03:06:53 +0200

    Translate navigation

--- next/components/Layout/Navigation.tsx
+++ next/components/Layout/Navigation.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { LinkButton } from '../Form/Button';
+import { Trans } from '../I18n/Trans';
 import styles from './Navigation.module.css';
 
 interface NavigationProps {
@@ -9,14 +10,14 @@ interface NavigationProps {
 const Navigation: FunctionComponent<NavigationProps> = () => {
   return (
     <ul className={styles.navigation}>
-      <li className={styles.item}><LinkButton appearance="menu" href="/item" icon="item">Items</LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/achievement" icon="achievement">Achievements</LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/skin" icon="skin">Skins</LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/profession" icon="profession">Professions</LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/specialization" icon="specialization">Specializations</LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/skill" icon="skill">Skills</LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/mount" icon="mount">Mounts</LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/wvw" icon="wvw">World versus World</LinkButton></li>
+      <li className={styles.item}><LinkButton appearance="menu" href="/item" icon="item"><Trans id="navigation.items"/></LinkButton></li>
+      <li className={styles.item}><LinkButton appearance="menu" href="/achievement" icon="achievement"><Trans id="navigation.achievements"/></LinkButton></li>
+      <li className={styles.item}><LinkButton appearance="menu" href="/skin" icon="skin"><Trans id="navigation.skins"/></LinkButton></li>
+      <li className={styles.item}><LinkButton appearance="menu" href="/profession" icon="profession"><Trans id="navigation.professions"/></LinkButton></li>
+      <li className={styles.item}><LinkButton appearance="menu" href="/specialization" icon="specialization"><Trans id="navigation.specializations"/></LinkButton></li>
+      <li className={styles.item}><LinkButton appearance="menu" href="/skill" icon="skill"><Trans id="navigation.skills"/></LinkButton></li>
+      <li className={styles.item}><LinkButton appearance="menu" href="/mount" icon="mount"><Trans id="navigation.mounts"/></LinkButton></li>
+      <li className={styles.item}><LinkButton appearance="menu" href="/wvw" icon="wvw"><Trans id="navigation.wvw"/></LinkButton></li>
     </ul>
   );
 };

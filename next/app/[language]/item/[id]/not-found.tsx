9e6b6af71b72388e80360e30d01c40176d437bc3
darthmaim <git@darthmaim.de>
2023-04-09 12:58:18 +0200

    Add item not-found page

---
+++ next/app/[language]/item/[id]/not-found.tsx
@@ -0,0 +1,10 @@
+import { Headline } from '@/components/Headline/Headline';
+import { HeroLayout } from '@/components/Layout/HeroLayout';
+
+export default function ItemNotFound(params: any) {
+  return (
+    <HeroLayout hero={<Headline id="test">Item not found</Headline>}>
+      <p>We couldn&apos;t find the item. The item might not have been added to the API yet.</p>
+    </HeroLayout>
+  );
+}

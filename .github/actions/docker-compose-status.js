1eae32697c50be685e79bb2828570b29a042d2ae
darthmaim <git@darthmaim.de>
2023-04-11 17:40:33 +0200

    Add database-migration service

--- .github/actions/docker-compose-status.js
+++ .github/actions/docker-compose-status.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
 });
 
 // acf45ebc18f8bbc44e5a084e31051212ba6078c0f6764cb6ab5102663dadcf89 /gw2treasurescom-database-1 true 0
-const regex = /^([0-9a-f]{64}) ([^ ]+) (true|false) ([0-9]+)$/
+const regex = /^([0-9a-f]{64}) ([^ ]+) ([^ ]+) ([0-9]+)$/
 
 rl.on('line', (line) => {
   const match = line.match(regex);
@@ -18,11 +18,11 @@ rl.on('line', (line) => {
   }
 
   const name = match[2];
-  const running = match[3] === 'true';
+  const status = match[3];
   const restarts = Number(match[4]);
   
-  if(!running) {
-    console.error(`${name} is not running`);
+  if(!['exited', 'running'].includes(status)) {
+    console.error(`${name} has status ${status}`);
     process.exit(1);
   }
 

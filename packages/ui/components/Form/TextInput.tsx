0b3ef4c35520b2f876dfaaa7b21d82abfb6afecc
darthmaim <git@darthmaim.de>
2023-04-29 15:15:58 +0200

    Allow unmananged use of TextInput

--- packages/ui/components/Form/TextInput.tsx
+++ packages/ui/components/Form/TextInput.tsx
@@ -3,17 +3,22 @@ import styles from './TextInput.module.css';
 
 export interface TextInputProps {
   type?: 'text' | 'password' | 'search'
-  value: string;
-  onChange: (value: string) => void;
+  value?: string;
+  onChange?: (value: string) => void;
   placeholder?: string;
+  name?: string;
 };
 
-export const TextInput: FC<TextInputProps> = ({ type = 'text', value, onChange, placeholder }) => {
+export const TextInput: FC<TextInputProps> = ({ type = 'text', value, onChange, placeholder, name }) => {
   const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    onChange(e.target.value);
+    onChange?.(e.target.value);
   }, [onChange]);
 
+  const managedProps = value !== undefined || onChange !== undefined
+    ? { onChange: handleChange, value }
+    : {};
+
   return (
-    <input type={type} value={value} onChange={handleChange} className={styles.input} placeholder={placeholder}/>
+    <input type={type} {...managedProps} className={styles.input} placeholder={placeholder} name={name}/>
   );
 };

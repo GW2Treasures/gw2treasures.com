37b6aec3cbef119735b3a2019fe3829a8735e403
darthmaim <git@darthmaim.de>
2023-04-29 12:54:05 +0200

    Fix @gw2treasures/ui types

--- packages/ui/types.d.ts
+++ packages/ui/types.d.ts
@@ -8,3 +8,14 @@ declare module '*.module.css' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
+
+import type * as CSS from 'csstype';
+
+declare module 'csstype' {
+
+  interface Properties {
+    // add known custom properties
+    '--icon-size'?: `${string}px`;
+    '--icon-color'?: Property.Color,
+  }
+}

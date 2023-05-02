31b84e8ceec17a9f10c6962861e9d5bab19dde5b
darthmaim <git@darthmaim.de>
2023-05-02 10:40:19 +0200

    Add defaultValue to Checkbox and TextInput

--- packages/ui/components/Form/Checkbox.tsx
+++ packages/ui/components/Form/Checkbox.tsx
@@ -1,15 +1,19 @@
+'use client';
+
 import { Icon } from '../../icons';
 import { KeyboardEventHandler, ReactNode, forwardRef, useCallback, useEffect, useId, useRef } from 'react';
 import styles from './Checkbox.module.css';
 
 export interface CheckboxProps {
-  checked: boolean;
+  checked?: boolean;
+  defaultChecked?: boolean;
   indeterminate?: boolean;
-  onChange: (checked: boolean) => void;
+  onChange?: (checked: boolean) => void;
+  name?: string;
   children: ReactNode;
 }
 
-export const Checkbox = forwardRef<HTMLLabelElement, CheckboxProps>(({ checked, indeterminate = false, onChange, children }, ref) => {
+export const Checkbox = forwardRef<HTMLLabelElement, CheckboxProps>(({ checked, defaultChecked, indeterminate = false, onChange, name, children }, ref) => {
   const inputRef = useRef<HTMLInputElement>(null);
   const id = useId();
 
@@ -28,7 +32,7 @@ export const Checkbox = forwardRef<HTMLLabelElement, CheckboxProps>(({ checked, 
 
   return (
     <label htmlFor={id} className={styles.wrapper} tabIndex={0} onKeyDown={labelOnKeyDown} ref={ref}>
-      <input id={id} ref={inputRef} type="checkbox" checked={checked} onChange={(e) => onChange(e.target.checked)} className={styles.input} tabIndex={-1}/>
+      <input id={id} ref={inputRef} type="checkbox" checked={checked} defaultChecked={defaultChecked} onChange={(e) => onChange?.(e.target.checked)} className={styles.input} tabIndex={-1} name={name}/>
       <div className={styles.checkbox}><Icon icon="checkmark"/></div>
       <div className={styles.label}>
         {children}

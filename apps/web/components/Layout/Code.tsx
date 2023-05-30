1301a6aff04e3822cf03c3a41b81ba009e427310
darthmaim <git@darthmaim.de>
2023-05-30 14:39:25 +0200

    Add Source Code Pro as font for Code component

--- apps/web/components/Layout/Code.tsx
+++ apps/web/components/Layout/Code.tsx
@@ -1,13 +1,20 @@
 import { FC, ReactNode } from 'react';
 import styles from './Code.module.css';
+import { Source_Code_Pro } from 'next/font/google';
+import { cx } from '@gw2treasures/ui';
 
 interface CodeProps {
   children: ReactNode;
 };
 
-export const Code: FC<CodeProps> = ({ children }) => {
+const font = Source_Code_Pro({
+  subsets: ['latin'],
+  weight: 'variable',
+  fallback: ['monospace'],
+});
 
+export const Code: FC<CodeProps> = ({ children }) => {
   return (
-    <pre className={styles.code}>{children}</pre>
+    <pre className={cx(styles.code, font.className)}>{children}</pre>
   );
 };

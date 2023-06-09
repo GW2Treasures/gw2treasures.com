3d5e7b9cccf499117c73e1f4fa964aa00e6c41fd
darthmaim <git@darthmaim.de>
2023-06-09 15:47:18 +0200

    Add actions to detail layout

--- apps/web/components/Layout/DetailLayout.tsx
+++ apps/web/components/Layout/DetailLayout.tsx
@@ -3,6 +3,10 @@ import styles from './DetailLayout.module.css';
 import { TableOfContentContext, TableOfContent } from '@gw2treasures/ui/components/TableOfContent/TableOfContent';
 import { Icon } from '@gw2treasures/database';
 import { EntityIcon, EntityIconType } from '../Entity/EntityIcon';
+import { Button } from '@gw2treasures/ui/components/Form/Button';
+import { Icon as IconComponent } from '@gw2treasures/ui';
+import { DropDown } from '../DropDown/DropDown';
+import { MenuList } from '../MenuList/MenuList';
 
 interface DetailLayoutProps {
   title: ReactNode;
@@ -12,9 +16,10 @@ interface DetailLayoutProps {
   children: ReactNode;
   infobox?: ReactNode;
   className?: string;
+  actions?: ReactNode[];
 };
 
-const DetailLayout: FunctionComponent<DetailLayoutProps> = ({ title, icon, breadcrumb, children, infobox, className, iconType }) => {
+const DetailLayout: FunctionComponent<DetailLayoutProps> = ({ title, icon, breadcrumb, children, infobox, className, iconType, actions }) => {
   return (
     <TableOfContentContext>
       <main className={[styles.main, className].filter(Boolean).join(' ')}>
@@ -23,6 +28,13 @@ const DetailLayout: FunctionComponent<DetailLayoutProps> = ({ title, icon, bread
           {icon && typeof icon === 'object' && (isValidElement<any>(icon) ? cloneElement(icon, { className: styles.icon }) : <EntityIcon icon={icon} size={48} className={styles.icon} type={iconType}/>)}
           <h1 className={styles.title}>{title}</h1>
           {breadcrumb && <div className={styles.breadcrumb}>{breadcrumb}</div>}
+          {actions && (
+            <div className={styles.actions}>
+              <DropDown button={<Button iconOnly appearance="menu"><IconComponent icon="more"/></Button>}>
+                <MenuList>{actions}</MenuList>
+              </DropDown>
+            </div>
+          )}
         </div>
         <aside className={styles.tableOfContent}>
           <TableOfContent/>

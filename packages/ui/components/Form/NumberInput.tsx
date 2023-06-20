b0465fe69beb2b4b0c8c1981406ee69662d2aed8
darthmaim <git@darthmaim.de>
2023-06-20 23:08:45 +0200

    Add min/max/step to NumberInput

--- packages/ui/components/Form/NumberInput.tsx
+++ packages/ui/components/Form/NumberInput.tsx
@@ -2,21 +2,24 @@ import { ChangeEvent, FC, useCallback } from 'react';
 import styles from './TextInput.module.css';
 
 export interface NumberInputProps {
-  value?: number;
+  value?: number | null;
   defaultValue?: number;
   onChange?: (value: number) => void;
   placeholder?: string;
   name?: string;
   readOnly?: boolean;
+  min?: number;
+  max?: number;
+  step?: number;
 };
 
-export const NumberInput: FC<NumberInputProps> = ({ value, defaultValue, onChange, placeholder, name, readOnly }) => {
+export const NumberInput: FC<NumberInputProps> = ({ value, defaultValue, onChange, placeholder, name, readOnly, min, max, step }) => {
   const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     const number = parseInt(e.target.value);
     onChange?.(number);
   }, [onChange]);
 
   return (
-    <input type="number" value={value} defaultValue={defaultValue} onChange={onChange && handleChange} className={styles.input} placeholder={placeholder} name={name} readOnly={readOnly}/>
+    <input type="number" value={value ?? ''} defaultValue={defaultValue} onChange={onChange && handleChange} className={styles.input} placeholder={placeholder} name={name} readOnly={readOnly} min={min} max={max} step={step}/>
   );
 };

290f6828ad932b24d06f6f9a91c76c4c2693cf7d
darthmaim <git@darthmaim.de>
2023-07-19 15:54:25 +0200

    Add id ItemTable column

--- apps/web/components/ItemTable/columns.tsx
+++ apps/web/components/ItemTable/columns.tsx
@@ -21,15 +21,16 @@ function createColumn<Select extends Prisma.ItemSelect>(column: ItemTableColumn<
   return column;
 }
 
-export type DefaultColumnName = 'item' | 'name_de' | 'name_en' | 'name_es' | 'name_fr' | 'icon' | 'level' | 'rarity' | 'type' | 'vendorValue';
+export type DefaultColumnName = 'id' | 'item' | 'icon' | 'name_de' | 'name_en' | 'name_es' | 'name_fr' | 'level' | 'rarity' | 'type' | 'vendorValue';
 
 export const defaultColumnDefinitions = {
+  id: createColumn({ id: 'id', select: { id: true }, render: ({ id }) => id, align: 'right' }),
   item: createColumn({ id: 'item', select: { id: true, rarity: true, name_de: true, name_en: true, name_es: true, name_fr: true, icon: true }, render: (item) => <ItemLink item={item}/> }),
+  icon: createColumn({ id: 'icon', select: { icon: true }, render: (item) => item.icon && <EntityIcon size={32} icon={item.icon}/> }),
   name_de: createColumn({ id: 'name_de', select: { name_de: true }, render: (item) => item.name_de }),
   name_en: createColumn({ id: 'name_en', select: { name_en: true }, render: (item) => item.name_en }),
   name_es: createColumn({ id: 'name_es', select: { name_es: true }, render: (item) => item.name_es }),
   name_fr: createColumn({ id: 'name_fr', select: { name_fr: true }, render: (item) => item.name_fr }),
-  icon: createColumn({ id: 'icon', select: { icon: true }, render: (item) => item.icon && <EntityIcon size={32} icon={item.icon}/> }),
   level: createColumn({ id: 'level', select: { level: true }, render: (item) => item.level, align: 'right' }),
   rarity: createColumn({ id: 'rarity', select: { rarity: true }, render: (item) => <Rarity rarity={item.rarity}/> }),
   type: createColumn({ id: 'type', select: { type: true, subtype: true }, render: (item) => <>{item.type} {item.subtype && `(${item.subtype})`}</> }),

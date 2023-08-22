fa60cdf81145ffa3f1db246475371dd534431df0
darthmaim <git@darthmaim.de>
2023-08-22 12:25:52 +0200

    Add tertiary appearance and flex prop to Button

--- packages/ui/components/Form/Button.tsx
+++ packages/ui/components/Form/Button.tsx
@@ -7,7 +7,8 @@ import { IconProp, Icon } from '../../icons';
 export interface CommonButtonProps {
   children: ReactNode;
   icon?: IconProp;
-  appearance?: 'primary' | 'secondary' | 'menu';
+  appearance?: 'primary' | 'secondary' | 'tertiary' | 'menu';
+  flex?: boolean;
   intent?: 'delete';
   iconOnly?: boolean;
   className?: string;
@@ -24,9 +25,9 @@ export interface ButtonProps extends CommonButtonProps {
   formAction?: string | ((...args: any[]) => Promise<unknown>);
 };
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button({ children, icon, appearance = 'secondary', intent, iconOnly, onClick, className, type = 'button', disabled, form, name, value, formAction }, ref) {
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button({ children, icon, appearance = 'secondary', flex, intent, iconOnly, onClick, className, type = 'button', disabled, form, name, value, formAction }, ref) {
   return (
-    <button ref={ref} onClick={onClick} className={cx(styles[appearance], iconOnly && styles.iconOnly, intent && styles[intent], className)} form={form} type={type} disabled={disabled} name={name} value={value} formAction={formAction as any}>
+    <button ref={ref} onClick={onClick} className={cx(styles[appearance], iconOnly && styles.iconOnly, flex && styles.flex, intent && styles[intent], className)} form={form} type={type} disabled={disabled} name={name} value={value} formAction={formAction as any}>
       {icon && <Icon icon={icon}/>}
       <span>{children}</span>
     </button>
@@ -41,11 +42,11 @@ export interface LinkButtonProps extends CommonButtonProps {
   external?: boolean;
 }
 
-export const LinkButton = forwardRef<HTMLAnchorElement, LinkButtonProps & Pick<React.AnchorHTMLAttributes<HTMLAnchorElement>, 'target' | 'rel'>>(function Button({ children, icon, appearance = 'secondary', intent, iconOnly, onClick, className, href, locale, prefetch, external, ...props }, ref) {
+export const LinkButton = forwardRef<HTMLAnchorElement, LinkButtonProps & Pick<React.AnchorHTMLAttributes<HTMLAnchorElement>, 'target' | 'rel'>>(function Button({ children, icon, appearance = 'secondary', flex, intent, iconOnly, onClick, className, href, locale, prefetch, external, ...props }, ref) {
   const LinkElement = external ? 'a' : Link;
 
   return (
-    <LinkElement ref={ref} className={cx(styles[appearance], iconOnly && styles.iconOnly, intent && styles[intent], className)} href={href} locale={locale} onClick={onClick} prefetch={prefetch} {...props}>
+    <LinkElement ref={ref} className={cx(styles[appearance], iconOnly && styles.iconOnly, flex && styles.flex, intent && styles[intent], className)} href={href} locale={locale} onClick={onClick} prefetch={prefetch} {...props}>
       {icon && <Icon icon={icon}/>}
       <span>{children}</span>
     </LinkElement>

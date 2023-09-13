c49d731675ef720288ad809154ad0e5bfa64ecd2
darthmaim <git@darthmaim.de>
2023-09-13 20:14:08 +0200

    Add Composite to Navigation

--- apps/web/components/Layout/Header/Navigation.tsx
+++ apps/web/components/Layout/Header/Navigation.tsx
@@ -1,7 +1,6 @@
 import React, { FunctionComponent } from 'react';
-import { LinkButton } from '@gw2treasures/ui/components/Form/Button';
 import { Trans } from '../../I18n/Trans';
-import styles from './Navigation.module.css';
+import { ClientNavigation, NavigationItem } from './Navigation.client';
 
 interface NavigationProps {
   // TODO: define props
@@ -9,17 +8,17 @@ interface NavigationProps {
 
 const Navigation: FunctionComponent<NavigationProps> = () => {
   return (
-    <ul className={styles.navigation}>
-      <li className={styles.item}><LinkButton appearance="menu" href="/item" icon="item"><Trans id="navigation.items"/></LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/achievement" icon="achievement"><Trans id="navigation.achievements"/></LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/skin" icon="skin"><Trans id="navigation.skins"/></LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/profession" icon="profession"><Trans id="navigation.professions"/></LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/specialization" icon="specialization"><Trans id="navigation.specializations"/></LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/skill" icon="skill"><Trans id="navigation.skills"/></LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/mount" icon="mount"><Trans id="navigation.mounts"/></LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/wvw" icon="wvw"><Trans id="navigation.wvw"/></LinkButton></li>
-      <li className={styles.item}><LinkButton appearance="menu" href="/dev" icon="developer"><Trans id="navigation.developer"/></LinkButton></li>
-    </ul>
+    <ClientNavigation>
+      <NavigationItem href="/item" icon="item"><Trans id="navigation.items"/></NavigationItem>
+      <NavigationItem href="/achievement" icon="achievement"><Trans id="navigation.achievements"/></NavigationItem>
+      <NavigationItem href="/skin" icon="skin"><Trans id="navigation.skins"/></NavigationItem>
+      <NavigationItem href="/profession" icon="profession"><Trans id="navigation.professions"/></NavigationItem>
+      <NavigationItem href="/specialization" icon="specialization"><Trans id="navigation.specializations"/></NavigationItem>
+      <NavigationItem href="/skill" icon="skill"><Trans id="navigation.skills"/></NavigationItem>
+      <NavigationItem href="/mount" icon="mount"><Trans id="navigation.mounts"/></NavigationItem>
+      <NavigationItem href="/wvw" icon="wvw"><Trans id="navigation.wvw"/></NavigationItem>
+      <NavigationItem href="/dev" icon="developer"><Trans id="navigation.developer"/></NavigationItem>
+    </ClientNavigation>
   );
 };
 

1c68b97c07b93135203d63e064a348fc57051761
darthmaim <git@darthmaim.de>
2023-10-03 15:11:06 +0200

    Replace /auth/login route with action

--- apps/web/app/[language]/login/page.tsx
+++ apps/web/app/[language]/login/page.tsx
@@ -1,9 +1,11 @@
-import { LinkButton } from '@gw2treasures/ui/components/Form/Button';
+import { Button } from '@gw2treasures/ui/components/Form/Button';
 import { PageLayout } from '@/components/Layout/PageLayout';
 import { Notice } from '@gw2treasures/ui/components/Notice/Notice';
 import { getUser } from '@/lib/getUser';
 import { redirect } from 'next/navigation';
 import { Icon } from '@gw2treasures/ui';
+import { Scope, getAuthorizationUrl } from '@gw2me/api';
+import { getCurrentUrl } from '@/lib/url';
 
 export default async function LoginPage({ searchParams }: { searchParams: { logout?: '', error?: '' }}) {
   const user = await getUser();
@@ -15,14 +17,16 @@ export default async function LoginPage({ searchParams }: { searchParams: { logo
   return (
     <PageLayout>
       {searchParams.error !== undefined && (
-        <Notice type="warning">Unknown error</Notice>
+        <Notice type="error">Unknown error</Notice>
       )}
 
       {searchParams.logout !== undefined && (
         <Notice>Logout successful</Notice>
       )}
 
-      <LinkButton href="/auth/login" external><Icon icon="gw2me" color="#b7000d"/> Login with gw2.me</LinkButton>
+      <form>
+        <Button type="submit" formAction={redirectToGw2Me}><Icon icon="gw2me" color="#b7000d"/> Login with gw2.me</Button>
+      </form>
     </PageLayout>
   );
 }
@@ -30,3 +34,25 @@ export default async function LoginPage({ searchParams }: { searchParams: { logo
 export const metadata = {
   title: 'Login'
 };
+
+
+const client_id = process.env.GW2ME_CLIENT_ID;
+
+// eslint-disable-next-line require-await
+async function redirectToGw2Me() {
+  'use server';
+
+  if(!client_id) {
+    console.error('GW2ME_CLIENT_ID not set');
+    redirect('/login?error');
+  }
+
+  // build redirect url
+  const redirect_uri = new URL('/auth/callback', getCurrentUrl()).toString();
+
+  // get gw2.me auth url
+  const url = getAuthorizationUrl({ redirect_uri, client_id, scopes: [Scope.Identify, Scope.Email] });
+
+  // redirect to gw2.me
+  redirect(url);
+}

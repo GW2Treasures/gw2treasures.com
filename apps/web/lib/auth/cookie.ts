5999115e7b589ed51bc84dcf48b074d7ac3f1510
darthmaim <git@darthmaim.de>
2023-10-26 19:30:55 +0200

    Fix warnings after updating to Next.js 14

--- apps/web/lib/auth/cookie.ts
+++ apps/web/lib/auth/cookie.ts
@@ -1,4 +1,4 @@
-import { ResponseCookie } from 'next/dist/compiled/@edge-runtime/cookies';
+import type { ResponseCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 
 const baseDomain = process.env.GW2T_NEXT_DOMAIN;
 

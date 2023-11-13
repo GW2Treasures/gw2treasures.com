e5ddef9d77032c19ee1666c38478e5082595a21a
darthmaim <git@darthmaim.de>
2023-11-13 13:38:44 +0100

    Configure serverActions.allowedOrigins

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,6 +4,14 @@ const path = require('path');
 const nextConfig = {
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../../'),
+    serverActions: {
+      allowedOrigins: process.env.NODE_ENV === 'development' ? [
+        'de.gw2treasures.localhost:3000', 
+        'en.gw2treasures.localhost:3000', 
+        'es.gw2treasures.localhost:3000', 
+        'fr.gw2treasures.localhost:3000'
+      ] : undefined
+    }
   },
   transpilePackages: ['@gw2treasures/ui'],
   reactStrictMode: true,

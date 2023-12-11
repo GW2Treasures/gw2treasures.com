537ac67542125d0000ab77b8169943d9d4163af6
darthmaim <git@darthmaim.de>
2023-12-11 11:24:48 +0100

    Show unlocked guild upgrades for items

--- apps/web/app/[language]/item/[id]/data.ts
+++ apps/web/app/[language]/item/[id]/data.ts
@@ -15,6 +15,7 @@ export const getItem = remember(60, function getItem(id: number, language: Langu
       },
       icon: true,
       unlocksSkin: { select: { ...linkProperties, weight: true, type: true, subtype: true, achievementBits: { select: linkPropertiesWithoutRarity }}},
+      unlocksGuildUpgrade: { select: linkPropertiesWithoutRarity },
       recipeOutput: { include: { currentRevision: true, itemIngredients: { include: { Item: { select: linkProperties }}}, currencyIngredients: { include: { Currency: { select: linkPropertiesWithoutRarity }}}, guildUpgradeIngredients: { include: { GuildUpgrade: { select: linkPropertiesWithoutRarity }}}, unlockedByItems: { select: linkProperties }}},
       unlocksRecipe: { include: { currentRevision: true, itemIngredients: { include: { Item: { select: linkProperties }}}, currencyIngredients: { include: { Currency: { select: linkPropertiesWithoutRarity }}}, guildUpgradeIngredients: { include: { GuildUpgrade: { select: linkPropertiesWithoutRarity }}}, unlockedByItems: { select: linkProperties }, outputItem: { select: linkProperties }}},
       achievementBits: { select: linkPropertiesWithoutRarity, orderBy: { id: 'asc' }},

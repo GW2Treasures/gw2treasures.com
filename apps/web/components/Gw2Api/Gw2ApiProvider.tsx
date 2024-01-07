210fe4eb4f8dc689cb7d1aa1104701f9b9e7e8fe
darthmaim <git@darthmaim.de>
2024-01-07 14:13:47 +0100

    Prompt user for authorization

--- apps/web/components/Gw2Api/Gw2ApiProvider.tsx
+++ apps/web/components/Gw2Api/Gw2ApiProvider.tsx
@@ -10,6 +10,7 @@ import { Button } from '@gw2treasures/ui/components/Form/Button';
 import { reauthorize } from './reauthorize';
 import { FlexRow } from '@gw2treasures/ui/components/Layout/FlexRow';
 import { useUser } from '../User/use-user';
+import { SubmitButton } from '@gw2treasures/ui/components/Form/Buttons/SubmitButton';
 
 export interface Gw2ApiProviderProps {
   children: ReactNode;
@@ -18,11 +19,16 @@ export interface Gw2ApiProviderProps {
 export const Gw2ApiProvider: FC<Gw2ApiProviderProps> = ({ children }) => {
   const accounts = useRef<Promise<Gw2Account[]>>();
   const [error, setError] = useState<ErrorCode>();
+  const [dismissed, setDismissed] = useState(false);
   const { user } = useUser();
 
   // eslint-disable-next-line require-await
   const getAccounts = useCallback(async () => {
     if(!user || typeof window === 'undefined') {
+      if(!dismissed) {
+        setError(ErrorCode.NOT_LOGGED_IN);
+      }
+
       return [];
     }
 
@@ -40,20 +46,25 @@ export const Gw2ApiProvider: FC<Gw2ApiProviderProps> = ({ children }) => {
     }
 
     return accounts.current;
-  }, [user]);
+  }, [dismissed, user]);
+
+  const handleDismiss = useCallback(() => {
+    setError(undefined);
+    setDismissed(true);
+  }, []);
 
   const value = useMemo(() => ({ getAccounts }), [getAccounts]);
 
   return (
     <Gw2ApiContext.Provider value={value}>
       {children}
-      {(error === ErrorCode.REAUTHORIZE || error === ErrorCode.MISSING_PERMISSION) && (
+      {(error === ErrorCode.REAUTHORIZE || error === ErrorCode.MISSING_PERMISSION || error === ErrorCode.NOT_LOGGED_IN) && (
         <form className={styles.dialog} action={reauthorize}>
           Authorize gw2treasures.com to view your progress.
           <div>
             <FlexRow>
-              <Button onClick={() => setError(undefined)}>Later</Button>
-              <Button type="submit" icon="gw2me-outline">Authorize</Button>
+              <Button onClick={handleDismiss}>Later</Button>
+              <SubmitButton type="submit" icon="gw2me-outline">Authorize</SubmitButton>
             </FlexRow>
           </div>
         </form>

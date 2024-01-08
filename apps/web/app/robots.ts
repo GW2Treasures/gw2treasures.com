0217aa42ee679e2a6b9227dc99485b9eb8cf1053
darthmaim <git@darthmaim.de>
2024-01-08 15:12:37 +0100

    Allow robots.txt

--- apps/web/app/robots.ts
+++ apps/web/app/robots.ts
@@ -7,9 +7,8 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      // allow: '/',
+      allow: '/',
       disallow: [
-        '/', // prevent indexing until the rewrite is hosted at gw2treasures.com
         '/auth',
       ]
     },

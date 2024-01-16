eff9effbe19c81acedd60557bd595f76f95b96f1
darthmaim <git@darthmaim.de>
2024-01-16 04:06:11 +0100

    Flatten sitemap index

--- apps/web/app/[language]/sitemap/route.ts
+++ apps/web/app/[language]/sitemap/route.ts
@@ -1,12 +1,10 @@
 import { getCurrentUrl } from '@/lib/url';
-import { sitemaps } from './sitemaps';
+import { getSitemapsForType, sitemaps } from './sitemaps';
 
-export function GET() {
+export async function GET() {
   const url = getCurrentUrl();
 
-  const sitemapXml = Object.keys(sitemaps)
-    .map((type) => `<sitemap><loc>${url}/${type}</loc></sitemap>`)
-    .join('');
+  const sitemapXml = (await Promise.all(Object.keys(sitemaps).map(getSitemapsForType(url.toString())))).join('');
 
   return new Response(`<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${sitemapXml}</sitemapindex>`, {
     headers: {

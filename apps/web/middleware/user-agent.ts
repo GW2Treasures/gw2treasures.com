e0ad52a6e1220bf048e697d153365de2c8e2d763
darthmaim <git@darthmaim.de>
2024-01-29 18:28:20 +0100

    Improve bot detection

--- apps/web/middleware/user-agent.ts
+++ apps/web/middleware/user-agent.ts
@@ -1,11 +1,24 @@
-import { userAgent } from 'next/server';
 import type { NextMiddleware } from './types';
+import { isbot } from 'isbot';
 
 export const userAgentMiddleware: NextMiddleware = (request, next) => {
-  const ua = userAgent(request);
+  // parse useragent from request
+  const ua = request.headers.get('User-Agent');
 
-  request.headers.append('x-gw2t-is-bot', ua.isBot ? '1' : '0');
-  console.log('  ' + (ua.isBot ? '(BOT) ' : '') + ua.ua);
+  // check if useragent is a bot
+  const isBot = isbot(ua);
+
+  // check if request is a prefetch request
+  const isPrefetch = request.headers.get('Next-Router-Prefetch') === '1';
+
+  // build flags array (bot, prefetch)
+  const flags = [isBot && 'Bot', isPrefetch && 'Prefetch'].filter(Boolean);
+
+  // log flags and useragent
+  console.log('  ' + (flags.length > 0 ? `(${flags.join(', ')}) ` : '') + (ua ?? '-'));
+
+  // append header if UA is a bot
+  request.headers.append('x-gw2t-is-bot', isBot ? '1' : '0');
 
   return next(request);
 };

3ee49e330736f91612e8b682ca17ab2566d439b0
darthmaim <git@darthmaim.de>
2024-01-30 09:09:35 +0100

    Log prisma queries during `items.check` job

--- apps/worker/src/db.ts
+++ apps/worker/src/db.ts
@@ -1,5 +1,17 @@
 import { PrismaClient } from '@gw2treasures/database';
 
-export const db = new PrismaClient();
+export const db = new PrismaClient({
+  log: ['info', { level: 'query', emit: 'event' }, 'error', 'warn']
+});
 
 export type PrismaTransaction = Omit<PrismaClient, '$connect' | '$disconnect' | '$on' | '$transaction' | '$use' | '$extends'>;
+
+export const dbDebug = { log: false };
+
+db.$on('query', (event) => {
+  if(!dbDebug.log) {
+    return;
+  }
+
+  console.log('query', event.query, `(${event.duration} ms)`);
+});

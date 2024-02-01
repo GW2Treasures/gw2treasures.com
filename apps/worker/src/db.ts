d30d8a234acd31c0d492d251e42ce3aa712dfd04
darthmaim <git@darthmaim.de>
2024-02-02 00:26:12 +0100

    Increase prisma connection pool size and set application name

--- apps/worker/src/db.ts
+++ apps/worker/src/db.ts
@@ -1,7 +1,11 @@
 import { PrismaClient } from '@gw2treasures/database';
 
+const datasourceUrl = new URL(process.env.DATABASE_URL!);
+datasourceUrl.searchParams.set('application_name', 'worker');
+
 export const db = new PrismaClient({
-  log: ['info', { level: 'query', emit: 'event' }, 'error', 'warn']
+  log: ['info', { level: 'query', emit: 'event' }, 'error', 'warn'],
+  datasourceUrl: datasourceUrl.toString()
 });
 
 export type PrismaTransaction = Omit<PrismaClient, '$connect' | '$disconnect' | '$on' | '$transaction' | '$use' | '$extends'>;

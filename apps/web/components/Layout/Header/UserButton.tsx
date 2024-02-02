f21ae1ab5154ac9b363f507715879b5834e2a888
darthmaim <git@darthmaim.de>
2024-02-02 17:27:54 +0100

    Create suspended user button component

---
+++ apps/web/components/Layout/Header/UserButton.tsx
@@ -0,0 +1,43 @@
+import { getUser, type SessionUser } from '@/lib/getUser';
+import { Icon } from '@gw2treasures/ui';
+import { LinkButton } from '@gw2treasures/ui/components/Form/Button';
+import { Suspense, type FC } from 'react';
+import { Skeleton } from '@/components/Skeleton/Skeleton';
+import styles from '../Layout.module.css';
+import 'server-only';
+
+
+// exported suspended wrapper around the actual button
+
+export interface UserButtonProps {}
+
+export const UserButton: FC<UserButtonProps> = () => {
+  return (
+    <Suspense fallback={<UserButtonButton user="loading"/>}>
+      <UserButtonLoader/>
+    </Suspense>
+  );
+};
+
+
+// internal component to load the user
+
+const UserButtonLoader: FC<UserButtonProps> = async ({}) => {
+  const user = await getUser();
+
+  return <UserButtonButton user={user}/>;
+};
+
+
+// internal button component to show loader / user / login
+
+interface UserButtonButtonProps {
+  user?: SessionUser | 'loading'
+}
+const UserButtonButton: FC<UserButtonButtonProps> = ({ user }) => {
+  return (
+    <LinkButton appearance="menu" href={user ? '/profile' : '/login'}>
+      <Icon icon="user"/><span className={styles.responsive}> {user === 'loading' ? <Skeleton width={90}/> : user ? user.name : 'Login'}</span>
+    </LinkButton>
+  );
+};

3db1f07d888933a2f740eb5bd929f21309bb3548
darthmaim <git@darthmaim.de>
2024-02-02 18:43:32 +0100

    Move exports from search route to new file

--- apps/web/app/[language]/api/item/search/route.ts
+++ apps/web/app/[language]/api/item/search/route.ts
@@ -1,6 +1,6 @@
 import type { UnwrapJsonResponse } from '../../helper';
 import { NextResponse } from 'next/server';
-import { searchItems, splitSearchTerms } from '../../search/route';
+import { searchItems, splitSearchTerms } from '../../search/helper';
 import { decode } from 'gw2e-chat-codes';
 import { isTruthy } from '@gw2treasures/ui';
 

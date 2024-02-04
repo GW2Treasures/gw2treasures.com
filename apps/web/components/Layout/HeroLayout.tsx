89ed93616757a8efde7f899c1bac5c924a4db1ed
darthmaim <git@darthmaim.de>
2024-02-05 00:44:11 +0100

    Preload hero mask image

--- apps/web/components/Layout/HeroLayout.tsx
+++ apps/web/components/Layout/HeroLayout.tsx
@@ -1,6 +1,8 @@
 import type { CSSProperties, FC, ReactNode } from 'react';
 import styles from './HeroLayout.module.css';
 import { PageLayout } from './PageLayout';
+import { preload } from 'react-dom';
+import heroMask from './hero-mask.jpg';
 
 export interface HeroLayoutProps {
   children: ReactNode;
@@ -10,6 +12,8 @@ export interface HeroLayoutProps {
 }
 
 export const HeroLayout: FC<HeroLayoutProps> = ({ children, hero, color, toc }) => {
+  preload(heroMask.src, { as: 'image' });
+
   return (
     <div style={{ '--hero-color': color ?? '#b7000d' }}>
       <div className={styles.hero}>{hero}</div>

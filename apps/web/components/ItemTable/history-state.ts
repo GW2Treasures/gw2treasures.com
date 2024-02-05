0bb536c941293d0f5f2a6a33ac9d8b75db5c53bd
darthmaim <git@darthmaim.de>
2024-02-05 22:53:08 +0100

    Fix ItemTable state restoration only working for one ItemTable

--- apps/web/components/ItemTable/history-state.ts
+++ apps/web/components/ItemTable/history-state.ts
@@ -6,13 +6,18 @@ export interface ItemTableHistoryState<ColumnId> {
   orderBy?: { column: ColumnId, order: 'asc' | 'desc'}
 }
 
-export function getHistoryState<T = GlobalColumnId>(id: string): ItemTableHistoryState<T> {
+export function getState() {
   return typeof window !== 'undefined'
-    ? window.history?.state[`itemTable.${id}`] ?? {}
+    ? window.history?.state ?? {}
     : {};
 }
 
+export function getHistoryState<T = GlobalColumnId>(id: string): ItemTableHistoryState<T> {
+  return getState()[`itemTable.${id}`] ?? {};
+}
+
 export function updateHistoryState<T = GlobalColumnId>(id: string, update: Partial<ItemTableHistoryState<T>>) {
+  const state = getState();
   const currentState = getHistoryState(id);
-  history.replaceState({ [`itemTable.${id}`]: { ...currentState, ...update }}, '');
+  history.replaceState({ ...state, [`itemTable.${id}`]: { ...currentState, ...update }}, '');
 }

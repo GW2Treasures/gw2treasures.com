bf5068e916e119e452d90aae1be384321e43bb22
darthmaim <git@darthmaim.de>
2024-02-07 22:05:52 +0100

    Add `@gw2treasures/helper` package

---
+++ packages/helper/src/is.ts
@@ -0,0 +1,26 @@
+export type Undefined = undefined | null;
+export type Falsy = false | 0 | -0 | 0n | '' | Undefined;
+
+export function isUndefined(value: unknown): value is Undefined {
+  return value == null;
+}
+
+export function isDefinied<T>(value: T | Undefined): value is T {
+  return value != null;
+}
+
+export function isTruthy<T>(value: T | Falsy): value is T {
+  return !!value;
+}
+
+export function isFalsy(value: unknown): value is Falsy {
+  return !value;
+}
+
+export function isEmptyObject(obj: unknown): obj is Record<string, never> {
+  return typeof obj === 'object' && obj != undefined && Object.keys(obj).length === 0;
+}
+
+export function isString(value: unknown): value is string {
+  return typeof value === 'string';
+}

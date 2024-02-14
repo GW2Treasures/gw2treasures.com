b6359c29969066f216edfaaf52571426a90753fd
darthmaim <git@darthmaim.de>
2024-02-14 14:57:23 +0100

    Fix text wrap in `ExternalLink`

--- packages/ui/components/Link/ExternalLink.tsx
+++ packages/ui/components/Link/ExternalLink.tsx
@@ -10,5 +10,12 @@ export interface ExternalLinkProps {
 }
 
 export const ExternalLink: FC<ExternalLinkProps> = ({ href, target = '_blank', children }) => {
-  return <a href={href} target={target} rel="noreferrer noopener" className={styles.link}><span className={styles.content}>{children}</span> <Icon icon="external-link"/></a>;
+  // the `&#8203;` in combination with `white-space: nowrap` on `.icon` prevents wrapping
+  // between the link content and the icon, but still allows wrapping within the content.
+  return (
+    <a href={href} target={target} rel="noreferrer noopener" className={styles.link}>
+      <span className={styles.content}>{children}</span>
+      <span className={styles.icon}>&#8203;<Icon icon="external-link"/></span>
+    </a>
+  );
 };

e1fa593700a7d36da963b93c6165ec2886350d49
darthmaim <git@darthmaim.de>
2024-02-29 12:48:37 +0100

    Add wizards vault purchase limit job

--- apps/worker/src/jobs/cron.ts
+++ apps/worker/src/jobs/cron.ts
@@ -48,6 +48,7 @@ export async function registerCronJobs() {
   await registerJob('colors', '*/5 * * * *');
   await registerJob('guild-upgrades', '*/5 * * * *');
   await registerJob('wizardsvault.listings', '*/5 * * * *');
+  await registerJob('wizardsvault.purchase-limit', '47 * * * *');
 
   await registerJob('tp', '* * * * *');
 
---
+++ apps/worker/src/jobs/wizardsvault/purchase-limit.ts
@@ -0,0 +1,40 @@
+import { db } from '../../db';
+import { fetchApi } from '../helper/fetchApi';
+import { groupEntitiesById } from '../helper/groupById';
+import { Job } from '../job';
+
+export const WizardsVaultPurchaseLimitJob: Job = {
+  async run() {
+    const apiKey = process.env.WORKER_GW2_API_KEY;
+
+    if(!apiKey) {
+      throw new Error('GW2 API key not set.');
+    }
+
+    const apiListings = await fetchApi<{ id: number, purchase_limit?: number }[]>(`/v2/account/wizardsvault/listings?access_token=${apiKey}`);
+    const dbListings = await db.wizardsVaultListing.findMany({ where: { removedFromApi: false }});
+
+    const listingsById = groupEntitiesById(dbListings);
+
+    let updated = 0;
+
+    for(const listing of apiListings) {
+      const limit = listing.purchase_limit ?? null;
+      const dbListing = listingsById.get(listing.id);
+
+      if(!dbListing) {
+        console.warn(`Found listing ${listing.id} in /v2/account/wizardsvault/listings but not in db`);
+      } else {
+        if(limit !== dbListing.limit) {
+          await db.wizardsVaultListing.updateMany({
+            where: { id: listing.id },
+            data: { limit }
+          });
+          updated++;
+        }
+      }
+    }
+
+    return `Updated purchase limit of ${updated} entries`;
+  }
+};

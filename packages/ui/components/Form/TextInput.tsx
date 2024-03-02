78aff0927520f08526192619a63ec8c9b91c2cb3
darthmaim <git@darthmaim.de>
2024-03-02 01:38:53 +0100

    Add autoFocus to TextInput

--- packages/ui/components/Form/TextInput.tsx
+++ packages/ui/components/Form/TextInput.tsx
@@ -9,14 +9,15 @@ export interface TextInputProps {
   placeholder?: string;
   name?: string;
   readOnly?: boolean;
+  autoFocus?: boolean;
 };
 
-export const TextInput: FC<TextInputProps> = ({ type = 'text', value, defaultValue, onChange, placeholder, name, readOnly }) => {
+export const TextInput: FC<TextInputProps> = ({ type = 'text', value, defaultValue, onChange, placeholder, name, readOnly, autoFocus }) => {
   const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     onChange?.(e.target.value);
   }, [onChange]);
 
   return (
-    <input type={type} value={value} defaultValue={defaultValue} onChange={onChange && handleChange} className={styles.input} placeholder={placeholder} name={name} readOnly={readOnly}/>
+    <input type={type} value={value} defaultValue={defaultValue} onChange={onChange && handleChange} className={styles.input} placeholder={placeholder} name={name} readOnly={readOnly} autoFocus={autoFocus}/>
   );
 };

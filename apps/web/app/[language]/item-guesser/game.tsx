986e16afcbf6b0e0d4ded7c8947c53db4fc46b89
darthmaim <git@darthmaim.de>
2024-03-02 01:39:06 +0100

    First round of improvements for item guesser game

--- apps/web/app/[language]/item-guesser/game.tsx
+++ apps/web/app/[language]/item-guesser/game.tsx
@@ -17,14 +17,18 @@ import { Skeleton } from '@/components/Skeleton/Skeleton';
 import { FlexRow } from '@gw2treasures/ui/components/Layout/FlexRow';
 import { Rarity } from '@/components/Item/Rarity';
 import { Coins } from '@/components/Format/Coins';
+import { Headline } from '@gw2treasures/ui/components/Headline/Headline';
 
 export interface ItemGuesserGameProps {
   challengeItem: WithIcon<Item>
 }
 
 export const ItemGuesserGame: FC<ItemGuesserGameProps> = ({ challengeItem }) => {
+  const [guessedItems, setGuessedItems] = useState<number[]>([]);
+
   const [guessed, setGuessed] = useState({
     count: 0,
+    hints: 0,
     correct: false,
     icon: false,
     rarity: false,
@@ -55,9 +59,10 @@ export const ItemGuesserGame: FC<ItemGuesserGameProps> = ({ challengeItem }) => 
 
   const handleGuess = useCallback((item: ApiItemSearchResponse['items'][0]) => {
     const correct = item.name_en === challengeItem.name_en;
-
+    setGuessedItems((guessedItems) => [...guessedItems, item.id]);
     setGuessed((guessed) => ({
       count: guessed.count + 1,
+      hints: guessed.hints,
       correct,
       icon: correct || guessed.icon || item.iconId === challengeItem.iconId,
       rarity: correct || guessed.rarity || item.rarity === challengeItem.rarity,
@@ -71,46 +76,58 @@ export const ItemGuesserGame: FC<ItemGuesserGameProps> = ({ challengeItem }) => 
 
   const handleHint = useCallback(() => {
     if(!guessed.type) {
-      return setGuessed({ ...guessed, count: guessed.count + 1, type: true });
+      return setGuessed({ ...guessed, count: guessed.count + 1, hints: guessed.hints + 1, type: true });
     } else if(!guessed.subtype && challengeItem.subtype) {
-      return setGuessed({ ...guessed, count: guessed.count + 1, subtype: true });
+      return setGuessed({ ...guessed, count: guessed.count + 1, hints: guessed.hints + 1, subtype: true });
     } else if(!guessed.weight && challengeItem.weight) {
-      return setGuessed({ ...guessed, count: guessed.count + 1, weight: true });
+      return setGuessed({ ...guessed, count: guessed.count + 1, hints: guessed.hints + 1, weight: true });
     } else if(!guessed.rarity) {
-      return setGuessed({ ...guessed, count: guessed.count + 1, rarity: true });
+      return setGuessed({ ...guessed, count: guessed.count + 1, hints: guessed.hints + 1, rarity: true });
     } else if(!guessed.level) {
-      return setGuessed({ ...guessed, count: guessed.count + 1, level: true });
+      return setGuessed({ ...guessed, count: guessed.count + 1, hints: guessed.hints + 1, level: true });
     } else if(!guessed.vendorValue) {
-      return setGuessed({ ...guessed, count: guessed.count + 1, vendorValue: true });
+      return setGuessed({ ...guessed, count: guessed.count + 1, hints: guessed.hints + 1, vendorValue: true });
     } else if(!guessed.icon) {
-      return setGuessed({ ...guessed, count: guessed.count + 1, icon: true });
+      return setGuessed({ ...guessed, count: guessed.count + 1, hints: guessed.hints + 1, icon: true });
     } else {
       setGuess(challengeItem.name_en.substring(0, 1));
     }
   }, [challengeItem, guessed]);
 
+  const unknown = <span style={{ color: 'var(--color-text-muted)' }}>?</span>;
+
   return (
     <div>
       <div style={{ background: guessed.correct ? 'var(--color-diff-added)' : 'var(--color-background-light)', padding: 32 }}>
         <div style={{ display: 'flex', flexDirection: 'column', gap: 16, maxWidth: 500, marginInline: 'auto' }}>
-          {guessed.correct ? <ItemLink item={challengeItem}/> : (
-            <FlexRow>
-              {guessed.icon ? <EntityIcon icon={challengeItem.icon!} size={32}/> : <Skeleton width={32} height={32}/>}
-              <Skeleton/>
-            </FlexRow>
-          )}
+          <div>
+            <Headline id="guess">Guess the Item</Headline>
+          </div>
 
-          <div>Rarity: {guessed.rarity ? <Rarity rarity={challengeItem.rarity}/> : '???'}</div>
-          <div>Type: {guessed.type ? <>{challengeItem.type} {challengeItem.subtype && (guessed.subtype ? ` / ${challengeItem.subtype}` : ' / ???')}</> : '???'}</div>
+          <div>
+            {guessed.correct ? <ItemLink item={challengeItem}/> : (
+              <FlexRow>
+                {guessed.icon ? <EntityIcon icon={challengeItem.icon!} size={32}/> : <Skeleton width={32} height={32}/>}
+                <Skeleton/>
+              </FlexRow>
+            )}
+          </div>
+
+          <div>Rarity: {guessed.rarity ? <Rarity rarity={challengeItem.rarity}/> : unknown}</div>
+          <div>Type: {guessed.type ? <>{challengeItem.type} {challengeItem.subtype && (guessed.subtype ? ` / ${challengeItem.subtype}` : <> / {unknown}</>)}</> : unknown}</div>
           {challengeItem.weight && guessed.type && <div>{guessed.weight ? challengeItem.weight : 'Unknown Weight'}</div>}
-          <div>Vendor Value: {guessed.vendorValue ? (challengeItem.vendorValue === null ? 'Not sellable' : <Coins value={challengeItem.vendorValue}/>) : '???'}</div>
-          <div>Level: {guessed.level ? challengeItem.level : '???'}</div>
+          <div>Vendor Value: {guessed.vendorValue ? (challengeItem.vendorValue === null ? 'Not sellable' : <Coins value={challengeItem.vendorValue}/>) : unknown}</div>
+          <div>Level: {guessed.level ? challengeItem.level : unknown}</div>
         </div>
       </div>
 
       {guessed.correct ? (
         <div style={{ display: 'flex', flexDirection: 'column', gap: 16, maxWidth: 500, marginInline: 'auto', paddingBlock: 32 }}>
-          You guessed correct after {guessed.count} guesses!
+          You guessed correct after {guessed.count} guesses and {guessed.hints} hints!
+
+          <div>
+            <Button onClick={() => location.reload()}>Play again!</Button>
+          </div>
         </div>
       ) : (
         <div style={{ display: 'flex', flexDirection: 'column', gap: 16, maxWidth: 500, marginInline: 'auto', paddingBlock: 32 }}>
@@ -119,9 +136,9 @@ export const ItemGuesserGame: FC<ItemGuesserGameProps> = ({ challengeItem }) => 
             <Button onClick={handleHint}>Hint</Button>
           </FlexRow>
 
-          <TextInput value={guess} onChange={setGuess}/>
+          <TextInput value={guess} onChange={setGuess} placeholder="Name / Chatlink / ID" autoFocus/>
 
-          {search.loading ? (
+          {(debouncedValue.length === 0 && guessed.count === 0) ? null : search.loading ? (
             <SkeletonTable columns={['Item', 'Select']} rows={2}/>
           ) : search.data.items.length === 0 ? (
             <p>No items found</p>
@@ -140,13 +157,17 @@ export const ItemGuesserGame: FC<ItemGuesserGameProps> = ({ challengeItem }) => 
                       <ItemLink item={item}/>
                     </td>
                     <td>
-                      <Button onClick={() => handleGuess(item)}>Guess</Button>
+                      <Button onClick={() => handleGuess(item)} disabled={guessedItems.includes(item.id)}>Guess</Button>
                     </td>
                   </tr>
                 ))}
               </tbody>
             </Table>
           )}
+
+          <Headline id="howto">How to play</Headline>
+
+          <p>Try to find the item by entering your guess in the text box above and selecting one of the suggestions. If your guess has any properties in common with the item in question, they will be revealed at the top. Use hints if you are stuck.</p>
         </div>
       )}
     </div>

0be08250be71535f391e924074a25753f17aa229
darthmaim <git@darthmaim.de>
2024-03-28 13:35:21 +0100

    Add achievements API endpoints

--- apps/web/app/[language]/dev/api/page.tsx
+++ apps/web/app/[language]/dev/api/page.tsx
@@ -5,7 +5,6 @@ import { ExternalLink } from '@gw2treasures/ui/components/Link/ExternalLink';
 import { getCurrentUrl } from '@/lib/url';
 import { Headline } from '@gw2treasures/ui/components/Headline/Headline';
 import { Table } from '@gw2treasures/ui/components/Table/Table';
-import { headers } from 'next/headers';
 import Link from 'next/link';
 
 const exampleCodeFetchWithAuthorizationHeader =
@@ -30,7 +29,7 @@ export default function DeveloperIconsPage() {
 
       <p>You can <Link href="/dev/app/create">create</Link> as many applications as you want. After creating a new application you can access its API key. You can view all your applications on the <Link href="/dev#applications">Developer page</Link>.</p>
 
-      <p>The API key can be passed as header (<Code inline>Authorization: Bearer &lt;api-key&gt;</Code>) or as <Code inline>apiKey</Code> query paramer.</p>
+      <p>The API key can be passed as header (<Code inline>Authorization: Bearer &lt;api-key&gt;</Code>) or as <Code inline>apiKey</Code> query parameter.</p>
 
       <Code><Highlight language="javascript" code={exampleCodeFetchWithAuthorizationHeader}/></Code>
 
@@ -45,8 +44,16 @@ export default function DeveloperIconsPage() {
         </thead>
         <tbody>
           <tr>
+            <th><Link href="#/achievements"><Code inline>/achievements</Code></Link></th>
+            <td>Get a list of all achievement IDs</td>
+          </tr>
+          <tr>
+            <th><Link href="#/achievements/:id/data"><Code inline>/achievements/:id/data</Code></Link></th>
+            <td>Get data of an achievement (same format as returned by the official Guild Wars 2 API)</td>
+          </tr>
+          <tr>
             <th><Link href="#/items"><Code inline>/items</Code></Link></th>
-            <td>Get a list of all item ids</td>
+            <td>Get a list of all item IDs</td>
           </tr>
           <tr>
             <th><Link href="#/items/:id/data"><Code inline>/items/:id/data</Code></Link></th>
@@ -55,8 +62,30 @@ export default function DeveloperIconsPage() {
         </tbody>
       </Table>
 
+      <Headline id="/achievements">GET <Code inline>/achievements</Code></Headline>
+      <p>Get a list of all achievement IDs.</p>
+
+      <Headline id="/achievements/:id/data">GET <Code inline>/achievements/:id/data</Code></Headline>
+      <p>Get data of an achievement (same format as returned by the official Guild Wars 2 API).</p>
+      <p>The HTTP response will contain a <Code inline>X-Created-At</Code> header with an ISO 8601 timestamp of when the item was first discovered.</p>
+
+      <Table>
+        <thead>
+          <tr>
+            <Table.HeaderCell small>Parameter</Table.HeaderCell>
+            <Table.HeaderCell>Description</Table.HeaderCell>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th><Code inline>lang</Code></th>
+            <td>The language of the achievement (<Code inline>de</Code> / <Code inline>en</Code> / <Code inline>es</Code> / <Code inline>fr</Code>)</td>
+          </tr>
+        </tbody>
+      </Table>
+
       <Headline id="/items">GET <Code inline>/items</Code></Headline>
-      <p>Get a list of all item ids.</p>
+      <p>Get a list of all item IDs.</p>
 
       <Table>
         <thead>
@@ -87,7 +116,7 @@ export default function DeveloperIconsPage() {
 
       <Headline id="/items/:id/data">GET <Code inline>/items/:id/data</Code></Headline>
       <p>Get data of an item (same format as returned by the official Guild Wars 2 API).</p>
-      <p>The HTTP response will contain a <Code inline>X-Created-At</Code> header with an ISO 8601 timestamp when the item was first discovered.</p>
+      <p>The HTTP response will contain a <Code inline>X-Created-At</Code> header with an ISO 8601 timestamp of when the item was first discovered.</p>
 
       <Table>
         <thead>

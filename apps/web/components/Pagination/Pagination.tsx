e093673a0fed0069b2eed1922571ee4de6fce141
darthmaim <git@darthmaim.de>
2024-04-17 18:25:05 +0200

    Add translations to `ItemTable`

--- apps/web/components/Pagination/Pagination.tsx
+++ apps/web/components/Pagination/Pagination.tsx
@@ -2,15 +2,18 @@ import { Button } from '@gw2treasures/ui/components/Form/Button';
 import { type FC, type MouseEventHandler, useCallback } from 'react';
 import styles from './Pagination.module.css';
 import { Icon } from '@gw2treasures/ui';
+import type { TranslationSubset } from '@/lib/translate';
+import { Tip } from '@gw2treasures/ui/components/Tip/Tip';
 
-interface PaginationProps {
+export interface PaginationProps {
   current: number;
   total: number;
   onPageChange: (page: number) => void;
   disabled?: boolean;
+  translations: TranslationSubset<'pagination.next' | 'pagination.previous'>
 };
 
-export const Pagination: FC<PaginationProps> = ({ current, total, onPageChange, disabled }) => {
+export const Pagination: FC<PaginationProps> = ({ current, total, onPageChange, disabled, translations }) => {
   const handlePrev = useCallback<MouseEventHandler<HTMLButtonElement>>((e) => {
     onPageChange(e.shiftKey ? 0 : current - 1);
   }, [current, onPageChange]);
@@ -21,9 +24,13 @@ export const Pagination: FC<PaginationProps> = ({ current, total, onPageChange, 
 
   return (
     <div className={styles.container}>
-      <Button iconOnly onClick={handlePrev} disabled={disabled || current < 1}><Icon icon="chevron-left"/></Button>
+      <Tip tip={translations['pagination.previous']} preferredPlacement="bottom">
+        <Button iconOnly onClick={handlePrev} disabled={disabled || current < 1} aria-label={translations['pagination.previous']}><Icon icon="chevron-left"/></Button>
+      </Tip>
       <span className={styles.page}>{current + 1} / {total}</span>
-      <Button iconOnly onClick={handleNext} disabled={disabled || current > total - 2}><Icon icon="chevron-right"/></Button>
+      <Tip tip={translations['pagination.next']} preferredPlacement="bottom">
+        <Button iconOnly onClick={handleNext} disabled={disabled || current > total - 2} aria-label={translations['pagination.next']}><Icon icon="chevron-right"/></Button>
+      </Tip>
     </div>
   );
 };

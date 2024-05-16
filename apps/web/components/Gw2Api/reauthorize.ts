b12005a96928f79189b3da58fbde57876307f733
darthmaim <git@darthmaim.de>
2024-05-16 13:44:34 +0200

    Remove email scope
    Emails are not used on gw2treasures.com

--- apps/web/components/Gw2Api/reauthorize.ts
+++ apps/web/components/Gw2Api/reauthorize.ts
@@ -14,7 +14,7 @@ export async function reauthorize(requiredScopes: Scope[], prompt?: Authorizatio
   const redirect_uri = new URL('/auth/callback', currentUrl).toString();
 
   // get scopes
-  const scopes = Array.from(new Set([Scope.Identify, Scope.Email, Scope.Accounts, Scope.Accounts_DisplayName, ...requiredScopes]));
+  const scopes = Array.from(new Set([Scope.Identify, Scope.Accounts, Scope.Accounts_DisplayName, ...requiredScopes]));
 
   // get gw2.me auth url
   const url = gw2me.getAuthorizationUrl({ redirect_uri, scopes, prompt, include_granted_scopes: true });

305afc11a4fe21c076e2501474286c51fa03eb18
darthmaim <git@darthmaim.de>
2024-06-09 16:58:08 +0200

    Add origins to application page

---
+++ apps/web/app/[language]/dev/app/[id]/actions.ts
@@ -0,0 +1,99 @@
+'use server';
+import { getUser } from '@/lib/getUser';
+import { db } from '@/lib/prisma';
+import { redirect } from 'next/navigation';
+import { getLoginUrlWithReturnTo } from '@/lib/login-url';
+import { type FormState } from '@gw2treasures/ui/components/Form/Form';
+import { revalidatePath } from 'next/cache';
+
+export async function deleteApplication(id: string, data: FormData) {
+  const user = await getUser();
+
+  if(!user) {
+    redirect(getLoginUrlWithReturnTo());
+  }
+
+  await db.application.deleteMany({ where: { id, ownerId: user.id }});
+
+  redirect('/dev#applications');
+}
+
+export async function saveApplication(id: string, prev: FormState, data: FormData): Promise<FormState> {
+  const user = await getUser();
+
+  if(!user) {
+    redirect(getLoginUrlWithReturnTo());
+  }
+
+  const name = data.get('name');
+
+  if(!name || typeof name !== 'string') {
+    return { error: 'Invalid name' };
+  }
+
+  await db.application.update({
+    where: { id },
+    data: { name }
+  });
+
+  revalidatePath(`/dev/app/${id}`);
+  return { success: 'Application saved' };
+}
+
+export async function updateOrigins(id: string, prev: FormState, data: FormData): Promise<FormState> {
+  const user = await getUser();
+
+  if(!user) {
+    redirect(getLoginUrlWithReturnTo());
+  }
+
+  const application = await db.application.findUnique({ where: { id }, select: { origins: true }});
+
+  if(!application) {
+    return { error: 'Application not found' };
+  }
+
+  const origin = data.get('origin');
+  const originToDelete = data.get('delete');
+
+  const hasNewOrigin = origin !== null && typeof origin === 'string';
+  const hasDeleteOrigin = originToDelete !== null && typeof originToDelete === 'string';
+
+  if(!hasNewOrigin && !hasDeleteOrigin) {
+    return { error: 'Bad request' };
+  }
+
+  if(hasDeleteOrigin) {
+    const updatedOrigins = application.origins.filter((origin) => origin !== originToDelete);
+
+    if(updatedOrigins.length === application.origins.length) {
+      return { error: 'Origin to delete not found' };
+    }
+
+    await db.application.update({
+      where: { id },
+      data: { origins: updatedOrigins }
+    });
+  } else if(hasNewOrigin) {
+    let url;
+    try {
+      url = new URL(origin);
+    } catch {
+      return { error: 'Wrong format. Please provide the origin format <schema>://<domain>(:<port>)' };
+    }
+
+    const formattedOrigin = url.origin;
+
+    if(application.origins.includes(formattedOrigin)) {
+      return { error: 'Duplicate origin' };
+    }
+
+    await db.application.update({
+      where: { id },
+      data: { origins: { push: formattedOrigin }}
+    });
+  }
+
+  revalidatePath(`/dev/app/${id}`);
+  return { success: 'Application saved' };
+}
--- apps/web/app/[language]/dev/app/[id]/page.tsx
+++ apps/web/app/[language]/dev/app/[id]/page.tsx
@@ -9,6 +9,11 @@ import { TextInput } from '@gw2treasures/ui/components/Form/TextInput';
 import { Headline } from '@gw2treasures/ui/components/Headline/Headline';
 import { notFound, redirect } from 'next/navigation';
 import { getLoginUrlWithReturnTo } from '@/lib/login-url';
+import { Form } from '@gw2treasures/ui/components/Form/Form';
+import Link from 'next/link';
+import { ExternalLink } from '@gw2treasures/ui/components/Link/ExternalLink';
+import { Table } from '@gw2treasures/ui/components/Table/Table';
+import { deleteApplication, saveApplication, updateOrigins } from './actions';
 
 async function getApplication(id: string) {
   const user = await getUser();
@@ -17,28 +22,15 @@ async function getApplication(id: string) {
     redirect(getLoginUrlWithReturnTo());
   }
 
-  const application = await db.application.findUnique({ where: { id }});
+  const application = await db.application.findUnique({
+    where: { id, ownerId: user.id }
+  });
 
-  if(!application || application.ownerId !== user.id) {
+  if(!application) {
     notFound();
   }
 
   return application;
-}
-
-async function deleteApplication(data: FormData) {
-  'use server';
-
-  const id = data.get('id')?.toString();
-  const user = await getUser();
-
-  if(!user) {
-    redirect(getLoginUrlWithReturnTo());
-  }
-
-  await db.application.deleteMany({ where: { id, ownerId: user.id }});
-
-  redirect('/dev#applications');
 }
 
 interface DevAppPageProps {
@@ -51,21 +43,62 @@ export default async function DevAppPage({ params: { id }}: DevAppPageProps) {
   const application = await getApplication(id);
 
   return (
-    <PageLayout>
+    <PageLayout toc>
       <Headline id="application">{application.name}</Headline>
 
-      <form>
-        <input type="hidden" name="id" value={application.id}/>
-
-        <Label label="API Key">
-          <TextInput value={application.apiKey} readOnly/>
-          <CopyButton copy={application.apiKey} icon="copy">Copy</CopyButton>
+      <Form action={saveApplication.bind(null, application.id)}>
+        <Label label="Name">
+          <TextInput defaultValue={application.name} name="name"/>
         </Label>
 
         <FlexRow>
-          <Button intent="delete" icon="delete" type="submit" formAction={deleteApplication}>Delete Application</Button>
+          <Button type="submit">Save</Button>
+          <Button intent="delete" icon="delete" type="submit" formAction={deleteApplication.bind(null, application.id)}>
+            Delete Application
+          </Button>
         </FlexRow>
-      </form>
+      </Form>
+
+      <Headline id="api-key">API Key</Headline>
+      <p>
+        Use this API key to access the <Link href="/dev/api">gw2treasures.com API</Link>.
+        It is okay to include this API key in native or mobile apps or client-side web applications.
+      </p>
+
+      <Label label="API Key">
+        <TextInput value={application.apiKey} readOnly/>
+        <CopyButton copy={application.apiKey} icon="copy">Copy</CopyButton>
+      </Label>
+
+      <Headline id="origins">Origins</Headline>
+      <p>
+        To access the gw2treasures.com API directly from a web browser, you&apos;ll need to register
+        the <ExternalLink href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Origin">origins</ExternalLink> your
+        application uses. This is not required if you are making requests from a server-side environment
+        without <ExternalLink href="https://developer.mozilla.org/en-US/docs/Glossary/CORS">CORS</ExternalLink> restrictions.
+      </p>
+      <Form action={updateOrigins.bind(null, application.id)}>
+        <Table>
+          <thead>
+            <tr>
+              <Table.HeaderCell>Origin</Table.HeaderCell>
+              <Table.HeaderCell small>Actions</Table.HeaderCell>
+            </tr>
+          </thead>
+          <tbody>
+            {application.origins.map((origin) => (
+              <tr key={origin}>
+                <th>{origin}</th>
+                <td><Button type="submit" name="delete" value={origin}>Delete</Button></td>
+              </tr>
+            ))}
+            <tr>
+              <td><FlexRow><TextInput name="origin"/></FlexRow></td>
+              <td><Button type="submit" icon="add">Add Origin</Button></td>
+            </tr>
+          </tbody>
+        </Table>
+      </Form>
     </PageLayout>
   );
 }

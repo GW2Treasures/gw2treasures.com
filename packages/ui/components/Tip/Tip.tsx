83153553d79f9f776ee331a5696cc14f0e245daf
darthmaim <git@darthmaim.de>
2024-06-13 21:00:10 +0200

    Use `FloatingArrow` in `Tip`

--- packages/ui/components/Tip/Tip.tsx
+++ packages/ui/components/Tip/Tip.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { arrow, autoUpdate, flip, FloatingPortal, offset, type Placement, shift, type Side, useDismiss, useFloating, useFocus, useHover, useInteractions, useRole, useTransitionStyles } from '@floating-ui/react';
+import { arrow, autoUpdate, flip, FloatingPortal, offset, type Placement, shift, type Side, useDismiss, useFloating, useFocus, useHover, useInteractions, useRole, useTransitionStyles, FloatingArrow } from '@floating-ui/react';
 import { Children, cloneElement, type FC, type ReactElement, type ReactNode, useRef, useState, type HTMLProps } from 'react';
 import styles from './Tip.module.css';
 
@@ -12,9 +12,9 @@ export interface TipProps {
 
 export const Tip: FC<TipProps> = ({ children, tip, preferredPlacement = 'top' }) => {
   const [open, setOpen] = useState(false);
-  const arrowRef = useRef<HTMLDivElement>(null);
+  const arrowRef = useRef<SVGSVGElement>(null);
 
-  const { x, y, refs, strategy, context, middlewareData, placement } = useFloating({
+  const { refs, context, floatingStyles } = useFloating({
     open,
     onOpenChange: setOpen,
     placement: preferredPlacement,
@@ -37,20 +37,6 @@ export const Tip: FC<TipProps> = ({ children, tip, preferredPlacement = 'top' })
 
   const { styles: transitionStyles, isMounted } = useTransitionStyles(context);
 
-  const staticSide = {
-    top: 'bottom' as Side,
-    right: 'left' as Side,
-    bottom: 'top' as Side,
-    left: 'right' as Side,
-  }[placement.split('-')[0]] ?? 'top';
-
-  const sideIndex = {
-    bottom: 2,
-    right: 1,
-    top: 0,
-    left: 3,
-  };
-
   return (
     <>
       {cloneElement(Children.only(children), { ref: refs.setReference, ...getReferenceProps(children.props) })}
@@ -59,22 +45,11 @@ export const Tip: FC<TipProps> = ({ children, tip, preferredPlacement = 'top' })
           <div
             ref={refs.setFloating}
             className={styles.tip}
-            style={{
-              ...transitionStyles,
-              position: strategy,
-              top: y ?? 0,
-              left: x ?? 0,
-              width: 'max-content',
-            }}
+            style={{ ...transitionStyles, ...floatingStyles }}
             {...getFloatingProps()}
           >
-            <div>{tip}</div>
-            <div className={styles.arrow} ref={arrowRef} style={{
-              left: middlewareData.arrow?.x ?? undefined,
-              top: middlewareData.arrow?.y ?? undefined,
-              '--arrow-side': sideIndex[staticSide],
-              ...{ [staticSide]: 'calc(var(--arrow-size) * -0.5)' }
-            }}/>
+            {tip}
+            <FloatingArrow context={context} ref={arrowRef} width={12} height={6} tipRadius={1} fill="var(--color-background)"/>
           </div>
         </FloatingPortal>
       )}

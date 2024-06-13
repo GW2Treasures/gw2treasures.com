cecb744c93b1400cb1b4efb97a83fcd229344820
darthmaim <git@darthmaim.de>
2024-06-13 05:32:47 +0200

    Log correct client IP behind cloudflare

--- apps/web/middleware/log.ts
+++ apps/web/middleware/log.ts
@@ -10,7 +10,9 @@ export const logMiddleware: NextMiddleware = async (request, next, data) => {
   const requestId = crypto.randomUUID();
   data.requestId = requestId;
 
-  const ip = request.ip ?? request.headers.get('x-real-ip') ?? request.headers.get('x-forwarded-for');
+  const ip = request.headers.get('cf-connecting-ip')
+    ?? request.headers.get('x-real-ip')
+    ?? request.headers.get('x-forwarded-for');
 
   console.log(`> [${requestId}] ${request.method} ${request.nextUrl.pathname}${request.nextUrl.search}${ip ? ` (${ip})` : ''}`);
 

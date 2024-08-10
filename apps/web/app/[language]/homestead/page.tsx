22ced8dc92ae6614a0975adc36d73bbe3ed36ab4
darthmaim <git@darthmaim.de>
2024-08-10 16:49:25 +0200

    Add homestead account unlocks

--- apps/web/app/[language]/homestead/page.tsx
+++ apps/web/app/[language]/homestead/page.tsx
@@ -16,6 +16,8 @@ import { FlexRow } from '@gw2treasures/ui/components/Layout/FlexRow';
 import { groupById } from '@gw2treasures/helper/group-by';
 import type { FC } from 'react';
 import { cache } from '@/lib/cache';
+import { Gw2AccountBodyCells, Gw2AccountHeaderCells } from '@/components/Gw2Api/Gw2AccountTableCells';
+import { AccountHomeCatCell, AccountHomeNodeCell, requiredScopes } from './page.client';
 
 
 const getItems = cache(
@@ -49,6 +51,9 @@ export default async function HomesteadPage() {
       <HomeNode.Table>
         <HomeNode.Column id="id" title="Id" small hidden>{({ id }) => id}</HomeNode.Column>
         <HomeNode.Column id="node" title="Node" sortBy="name">{({ unlock_items: [id], name }) => unlockItems[id] ? <ItemLink item={unlockItems[id]}/> : name}</HomeNode.Column>
+        <HomeNode.DynamicColumns headers={<Gw2AccountHeaderCells requiredScopes={requiredScopes} small/>}>
+          {({ id }) => <Gw2AccountBodyCells requiredScopes={requiredScopes}><AccountHomeNodeCell nodeId={id} accountId={undefined as never}/></Gw2AccountBodyCells>}
+        </HomeNode.DynamicColumns>
       </HomeNode.Table>
 
       <Headline id="cats" actions={<ColumnSelect table={HomeCats}/>}>Cats</Headline>
@@ -56,10 +61,13 @@ export default async function HomesteadPage() {
         <HomeCats.Column id="id" title="Id" align="right" small hidden>{({ id }) => id}</HomeCats.Column>
         <HomeCats.Column id="name" title="Cat" sortBy="name">{({ name, icon }) => <FlexRow><MaybeRenderIcon src={icon}/> {name}</FlexRow>}</HomeCats.Column>
         <HomeCats.Column id="desc" title="Description" sortBy="description">{({ description }) => <CatDescription description={description}/>}</HomeCats.Column>
+        <HomeCats.DynamicColumns headers={<Gw2AccountHeaderCells requiredScopes={requiredScopes} small/>}>
+          {({ id }) => <Gw2AccountBodyCells requiredScopes={requiredScopes}><AccountHomeCatCell catId={id} accountId={undefined as never}/></Gw2AccountBodyCells>}
+        </HomeCats.DynamicColumns>
       </HomeCats.Table>
 
       <Headline id="decorations">Decorations</Headline>
-      <Notice>Decorations will be soon after the launch of Guild Wars 2: Janthir Wilds.</Notice>
+      <Notice>Decorations will be available soon after the launch of Guild Wars 2: Janthir Wilds.</Notice>
     </HeroLayout>
   );
 }

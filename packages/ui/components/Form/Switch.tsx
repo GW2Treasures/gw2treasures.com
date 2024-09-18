7cda7799de8313d272777314e298e30461175f72
darthmaim <git@darthmaim.de>
2024-09-18 09:30:13 +0200

    Add `Switch` component

---
+++ packages/ui/components/Form/Switch.tsx
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+import { type FC, type ReactElement, type ReactNode } from 'react';
+import styles from './Switch.module.css';
+import { Icon, type IconProp } from '../../icons';
+import { Tip } from '../Tip/Tip';
+import { Composite, CompositeItem } from '../Focus/Composite';
+
+export interface SwitchProps {
+  children: ReactElement<SwitchControlProps>[]
+}
+
+export type SwitchControlProps = {
+  children?: ReactNode;
+  active?: boolean;
+  clickAction?: () => void;
+  icon?: IconProp;
+  tip?: ReactNode;
+} & (
+  | { type?: 'button', href?: never, clickAction: () => void, name?: string, value?: string }
+  | { type: 'link', href: string, name?: never, value?: never }
+  | { type: 'radio', href?: never, name: string, value: string }
+);
+
+export const Switch: FC<SwitchProps> & { Control: FC<SwitchControlProps> } = ({ children }) => {
+  return (
+    <Composite className={styles.wrapper}>
+      {children}
+    </Composite>
+  );
+};
+
+Switch.Control = ({ children, active, type = 'button', href, clickAction, name, value, icon, tip }) => {
+  const Element = type === 'link' ? Link : type === 'radio' ? 'label' : 'button';
+
+  const element = (
+    <Element href={href!} onClick={clickAction} className={active && type !== 'radio' ? styles.controlActive : styles.control} name={type === 'button' ? name : undefined} value={type === 'button' ? value : undefined}>
+      {type === 'radio' && <input type="radio" name={name} value={value} className={styles.radio} defaultChecked={active}/>}
+      {icon && (<Icon icon={icon} className={styles.icon}/>)}
+      {children && (<span>{children}</span>)}
+    </Element>
+  );
+
+  return tip
+    ? <Tip tip={tip}><CompositeItem render={element} data-active={active}/></Tip>
+    : <CompositeItem render={element}/>;
+};
+
+Switch.Control.displayName = 'Switch.Control';

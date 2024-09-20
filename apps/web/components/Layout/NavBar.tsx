94a18e0f91c2ba96602ef47893f9352ef867a112
darthmaim <git@darthmaim.de>
2024-09-20 15:59:44 +0200

    Use `Composite` to manage focus in `NavBar`

--- apps/web/components/Layout/NavBar.tsx
+++ apps/web/components/Layout/NavBar.tsx
@@ -5,6 +5,7 @@ import type { FC, ReactNode } from 'react';
 import styles from './NavBar.module.css';
 import { LinkButton } from '@gw2treasures/ui/components/Form/Button';
 import { HorizontalOverflowContainer } from '@/components/Layout/HorizontalOverflowContainer';
+import { Composite, CompositeItem } from '@floating-ui/react';
 
 interface NavBarProps {
   items: { label: ReactNode, segment: string }[]
@@ -17,15 +18,15 @@ export const NavBar: FC<NavBarProps> = ({ items, base = '/' }) => {
   return (
     <div className={styles.bar}>
       <HorizontalOverflowContainer inverted>
-        <ul className={styles.list}>
+        <Composite render={<ul className={styles.list}/>}>
           {items.map((item) => (
             <li key={item.segment} className={segment === item.segment ? styles.active : styles.button}>
-              <LinkButton href={base + item.segment} appearance="menu" className={styles.link}>
+              <CompositeItem render={<LinkButton href={base + item.segment} appearance="menu" className={styles.link}/>}>
                 {item.label}
-              </LinkButton>
+              </CompositeItem>
             </li>
           ))}
-        </ul>
+        </Composite>
       </HorizontalOverflowContainer>
     </div>
   );

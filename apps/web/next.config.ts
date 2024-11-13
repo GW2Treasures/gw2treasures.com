42ed9a0c033c524d3909e2d3509a8c039b069a8a
darthmaim <git@darthmaim.de>
2024-11-13 20:03:43 +0100

    Add `@prisma/nextjs-monorepo-workaround-plugin`

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next';
 import path from 'path';
 
+// @ts-expect-error no types available
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
+
 const nextConfig: NextConfig = {
   // publish as standalone docker images
   output: 'standalone',
@@ -17,6 +20,15 @@ const nextConfig: NextConfig = {
   // transpile @gw2treasures/ui package
   outputFileTracingRoot: path.join(__dirname, '../../'),
   transpilePackages: ['@gw2treasures/ui'],
+
+  // add PrismaPlugin to ensure that prisma engines are bundled correctly
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
 
   // permanently redirected pages
   redirects: () => Promise.resolve([

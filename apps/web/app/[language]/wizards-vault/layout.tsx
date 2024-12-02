20a03fd821ed425d6f7bbae7badec8f4974dac35
darthmaim <git@darthmaim.de>
2024-12-02 10:05:16 +0100

    Fix headline id in wizardsvault layout

--- apps/web/app/[language]/wizards-vault/layout.tsx
+++ apps/web/app/[language]/wizards-vault/layout.tsx
@@ -9,7 +9,7 @@ import type { Metadata } from 'next';
 export default function WizardsVaultLayout({ children }: LayoutProps) {
   return (
     <HeroLayout color="#ff9800" skipLayout
-      hero={<Headline id="homestead"><Trans id="navigation.wizardsVault"/></Headline>}
+      hero={<Headline id="wizardsvault"><Trans id="navigation.wizardsVault"/></Headline>}
       navBar={(
         <NavBar base="/wizards-vault/" items={[
           { segment: '(index)', href: '/wizards-vault', label: 'Objectives' },

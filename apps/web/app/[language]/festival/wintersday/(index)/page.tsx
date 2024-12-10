413e2dd60170c1747dfe733ac297f3139b5d29ff
darthmaim <git@darthmaim.de>
2024-12-10 13:25:32 +0100

    Add item inventories to wintersday page

--- apps/web/app/[language]/festival/wintersday/(index)/page.tsx
+++ apps/web/app/[language]/festival/wintersday/(index)/page.tsx
@@ -1,4 +1,6 @@
+import { Gw2Accounts } from '@/components/Gw2Api/Gw2Accounts';
 import { Trans } from '@/components/I18n/Trans';
+import { ItemInventoryTable } from '@/components/Item/ItemInventoryTable';
 import { ItemTable } from '@/components/ItemTable/ItemTable';
 import { ItemTableColumnsButton } from '@/components/ItemTable/ItemTableColumnsButton';
 import { ItemTableContext } from '@/components/ItemTable/ItemTableContext';
@@ -6,6 +8,11 @@ import { PageLayout } from '@/components/Layout/PageLayout';
 import { pageView } from '@/lib/pageView';
 import { Headline } from '@gw2treasures/ui/components/Headline/Headline';
 import type { Metadata } from 'next';
+import { requiredScopes } from '../helper';
+import { db } from '@/lib/prisma';
+import { linkProperties } from '@/lib/linkProperties';
+import { cache } from '@/lib/cache';
+import { ItemLink } from '@/components/Item/ItemLink';
 
 const itemIds = [
   86601,
@@ -13,7 +20,20 @@ const itemIds = [
   77604,
 ];
 
+const loadData = cache(async function loadData() {
+  const [items] = await Promise.all([
+    db.item.findMany({
+      where: { id: { in: itemIds }},
+      select: linkProperties
+    })
+  ]);
+
+  return { items };
+}, ['wintersday-items'], { revalidate: 60 * 60 });
+
+
 export default async function WintersdayPage() {
+  const { items } = await loadData();
   await pageView('festival/wintersday');
 
   return (
@@ -23,6 +43,16 @@ export default async function WintersdayPage() {
         <Headline actions={<ItemTableColumnsButton/>} id="items"><Trans id="navigation.items"/></Headline>
         <ItemTable query={{ where: { id: { in: itemIds }}}} defaultColumns={['item', 'rarity', 'type', 'buyPrice', 'buyPriceTrend', 'sellPrice', 'sellPriceTrend']}/>
       </ItemTableContext>
+
+      <Gw2Accounts requiredScopes={requiredScopes} loading={null} loginMessage={<Trans id="festival.wintersday.items.login"/>} authorizationMessage={<Trans id="festival.wintersday.items.authorize"/>}>
+        {items.map((item) => (
+          <>
+            <Headline id={item.id.toString()}><ItemLink item={item}/></Headline>
+            <ItemInventoryTable itemId={item.id}/>
+          </>
+        ))}
+      </Gw2Accounts>
+
     </PageLayout>
   );
 }

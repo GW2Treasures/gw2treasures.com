903eb8634a5c562b2d5701c1e72e1f2d5016574a
darthmaim <git@darthmaim.de>
2024-12-18 15:05:08 +0100

    (WIP) Lunar New Year festival

---
+++ apps/web/app/[language]/festival/lunar-new-year/snow.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import type { LetItGo } from 'let-it-go';
+import { useEffect, useRef, type FC, type ReactNode } from 'react';
+
+export interface SnowProps {
+  children: ReactNode,
+  className?: string,
+}
+
+export const Snow: FC<SnowProps> = ({ children }) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let snow: LetItGo;
+
+    import(/* webpackChunkName: "snow" */'let-it-go').then(({ LetItGo }) => {
+      snow = new LetItGo({
+        root: ref.current!,
+        style: { pointerEvents: 'none' },
+        radiusRange: [1, 2],
+        alphaRange: [.5, .8],
+        number: 128,
+      });
+    });
+
+    return () => {
+      snow?.clear();
+    };
+  }, []);
+
+  return (
+    <div ref={ref} style={{ margin: '-32px -16px' }}>
+      <div style={{ padding: '32px 16px' }}>
+        {children}
+      </div>
+    </div>
+  );
+};

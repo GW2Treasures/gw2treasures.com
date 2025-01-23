14320ee43b1a02367c22d4850496c5f022634567
darthmaim <git@darthmaim.de>
2025-01-23 14:51:08 +0100

    Add festival timer component

--- apps/web/app/[language]/festival/festivals.ts
+++ apps/web/app/[language]/festival/festivals.ts
@@ -10,9 +10,19 @@ export interface FestivalInfo {
 
 export function getActiveFestival(): FestivalInfo | undefined {
   const now = new Date();
-  return festivals.find((festival) => festival.startsAt <= now && festival.endsAt > now);
+  return festivals.find((festival) => isFestivalActive(festival, now));
+}
+
+export function getFestival(type: Festival): FestivalInfo {
+  return festivals.findLast((festival) => festival.type === type)!;
+}
+
+export function isFestivalActive(festival: FestivalInfo, timestamp?: Date) {
+  timestamp ??= new Date();
+
+  return festival.startsAt <= timestamp && festival.endsAt > timestamp;
 }
 
 export const festivals: FestivalInfo[] = [
-  { type: Festival.Wintersday, startsAt: new Date('2024-01-10T16:00:00.000Z'), endsAt: new Date('2025-01-02T17:00:00.000Z') },
+  { type: Festival.Wintersday, startsAt: new Date('2024-12-10T16:00:00.000Z'), endsAt: new Date('2025-01-02T17:00:00.000Z') },
 ];

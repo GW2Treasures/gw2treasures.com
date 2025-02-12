c54a2649c9eca88f48e2a170c8bfc9526ff95d93
darthmaim <git@darthmaim.de>
2025-02-12 22:22:30 +0100

    Add categories to `/currency` page

---
+++ apps/web/app/[language]/currency/data.ts
@@ -0,0 +1,98 @@
+
+// TODO(static): Extract to static data
+export const currencyCategories = {
+  general: [
+    4, // Gem
+    1, // Coin
+    2, // Karma
+    23, // Spirit Shard
+    3, // Laurel
+    16, // Guild Commendation
+    18, // Transmutation Charge
+    63, // Astral Acclaim
+  ],
+  competitive: [
+    30, // PvP League Ticket
+    15, // Badge of Honor
+    26, // WvW Skirmish Claim Ticket
+    31, // Proof of Heroics
+    36, // Testimony of Desert Heroics
+    65, // Testimony of Jade Heroics
+    33, // Ascended Shards of Glory
+  ],
+  map: [
+    25, // Geode
+    27, // Bandit Crest
+    19, // Airship Part
+    22, // Lump of Aurillium
+    20, // Ley Line Crystal
+    29, // Provisioner Token
+    32, // Unbound Magic
+    34, // Trade Contract
+    35, // Elegy Mosaic
+    45, // Volatile Magic
+    47, // Racing Medallion
+    50, // Festival Token
+    57, // Blue Prophet Shard
+    58, // War Supplies
+    60, // Tyrian Defense Seal
+    67, // Canach Coins
+    72, // Static Charge
+    73, // Pinch of Stardust
+    75, // Calcified Gasp
+    76, // Ursus Oblige
+    28, // Magnetite Shard
+    70, // Legendary Insight
+    53, // Green Prophet Shard
+    77, // Gaeting Crystal
+  ],
+  key: [
+    43, // Zephyrite Lockpick
+    41, // Pact Crowbar
+    37, // Exalted Key
+    42, // Vial of Chak Acid
+    38, // Machete
+    44, // Trader's Key
+    49, // Mistborn Key
+    51, // Cache Key
+    54, // Blue Prophet Crystal
+    71, // Jade Miner's Keycard
+    40, // Bandit Skeleton Key
+  ],
+  dungeon: [
+    7, // Fractal Relic
+    24, // Pristine Fractal Relic
+    69, // Tales of Dungeon Delving
+    59, // Unstable Fractal Essence
+  ],
+  blacklion: [
+    4, // Gem
+    18, // Transmutation Charge
+  ],
+  historic: [
+    39, // Gaeting Crystal
+    55, // Green Prophet Crystal
+    52, // Red Prophet Shard
+    56, // Red Prophet Crystal
+    5, // Ascalonian Tear
+    9, // Seal of Beetletun
+    11, // Deadly Bloom
+    10, // Manifesto of the Moletariate
+    13, // Flame Legion Charr Carving
+    12, // Symbol of Koda
+    14, // Knowledge Crystal
+    6, // Shard of Zhaitan
+    74, // [empty name, icon of Astral Acclaim]
+  ]
+};
+
+export const categoryById: Record<number, (keyof typeof currencyCategories)[]> = {};
+
+for(const [category, ids] of Object.entries(currencyCategories) as [keyof typeof currencyCategories, number[]][]) {
+  for(const id of ids) {
+    if(!categoryById[id]) {
+      categoryById[id] = [];
+    }
+    categoryById[id].push(category);
+  }
+}
--- apps/web/app/[language]/currency/page.tsx
+++ apps/web/app/[language]/currency/page.tsx
@@ -6,9 +6,14 @@ import { cache } from '@/lib/cache';
 import { createDataTable } from '@gw2treasures/ui/components/Table/DataTable';
 import { compareLocalizedName } from '@/lib/localizedName';
 import { ColumnSelect } from '@/components/Table/ColumnSelect';
-import { FlexRow } from '@gw2treasures/ui/components/Layout/FlexRow';
 import { AccountHeader, AccountWalletRow } from './account-data';
 import type { PageProps } from '@/lib/next';
+import { getTranslate } from '@/lib/translate';
+import { Trans } from '@/components/I18n/Trans';
+import { categoryById } from './data';
+import type { Metadata } from 'next';
+import { Description } from '@/components/Layout/Description';
+import { getAlternateUrls } from '@/lib/url';
 
 const getCurrencies = cache(
   () => db.currency.findMany({
@@ -21,21 +26,22 @@ const getCurrencies = cache(
 
 export default async function CurrencyPage({ params }: PageProps) {
   const { language } = await params;
-  const currencies = await getCurrencies();
+  const t = getTranslate(language);
 
+  const currencies = await getCurrencies();
   const Currencies = createDataTable(currencies, ({ id }) => id);
 
   return (
-    <HeroLayout hero={<Headline id="currencies">Currencies</Headline>}>
-      <FlexRow align="right">
-        <ColumnSelect table={Currencies}/>
-      </FlexRow>
-      <p/>
+    <HeroLayout hero={<Headline id="currencies"><Trans id="currencies"/></Headline>}>
+      <Description actions={<ColumnSelect table={Currencies}/>}>
+        <Trans id="currencies.description"/>
+      </Description>
 
       <Currencies.Table>
-        <Currencies.Column id="id" title="Id" align="right" small hidden>{({ id }) => id}</Currencies.Column>
-        <Currencies.Column id="currency" title="Currency" sort={compareLocalizedName(language)}>{(currency) => <CurrencyLink currency={currency}/>}</Currencies.Column>
-        <Currencies.Column id="order" title="Order" align="right" hidden>{({ order }) => order}</Currencies.Column>
+        <Currencies.Column id="id" title="Id" align="right" sortBy="id" small hidden>{({ id }) => id}</Currencies.Column>
+        <Currencies.Column id="currency" title={<Trans id="currency"/>} sort={compareLocalizedName(language)}>{(currency) => <CurrencyLink currency={currency}/>}</Currencies.Column>
+        <Currencies.Column id="category" title={<Trans id="currency.category"/>}>{({ id }) => categoryById[id]?.map((category) => t(`currency.category.${category}`)).join(', ')}</Currencies.Column>
+        <Currencies.Column id="order" title={<Trans id="currency.order"/>} sortBy="order" align="right" hidden>{({ order }) => order}</Currencies.Column>
         <Currencies.DynamicColumns headers={<AccountHeader/>}>
           {({ id }) => <AccountWalletRow currencyId={id}/>}
         </Currencies.DynamicColumns>
@@ -44,6 +50,15 @@ export default async function CurrencyPage({ params }: PageProps) {
   );
 }
 
-export const metadata = {
-  title: 'Currencies'
-};
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const { language } = await params;
+  const t = getTranslate(language);
+
+  return {
+    title: t('currencies'),
+    description: t('currencies.description'),
+    alternates: getAlternateUrls('/homestead/materials', language),
+    keywords: ['currency', 'wallet', 'key', 'map currency', 'black lion', 'gems', 'coins', 'gold', 'silver', 'copper', 'karma'],
+    // TODO(og): Add og image
+  };
+}

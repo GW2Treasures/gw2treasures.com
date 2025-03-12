679f432be8577f3e8e0f406a950cf66382f30742
darthmaim <git@darthmaim.de>
2025-03-12 15:22:36 +0100

    Add filter by type to legendary armor

--- apps/web/app/[language]/legendary/armor/page.tsx
+++ apps/web/app/[language]/legendary/armor/page.tsx
@@ -9,6 +9,8 @@ import { getTranslate } from '@/lib/translate';
 import { Notice } from '@gw2treasures/ui/components/Notice/Notice';
 import type { Metadata } from 'next';
 import { createItemTable, LegendaryItemDataTable } from '../table';
+import { TableFilterButton, TableFilterProvider, type TableFilterDefinition } from '@gw2treasures/ui/components/Table/TableFilter';
+import type { SubType } from '@/components/Item/ItemType.types';
 
 const legendaryRuneId = 91536;
 
@@ -27,17 +29,29 @@ const loadItems = cache(async () => {
 
 export default async function LegendaryRelicsPage({ params }: PageProps) {
   const { language } = await params;
+  const t = getTranslate(language);
+
   const items = await loadItems();
   const Items = createItemTable(items);
 
+  // TODO: add filtering by weight
+  const types = Array.from(new Set(items.map(({ type, subtype }) => type === 'Back' ? 'Back' : `${type}.${subtype}`))) as (`Armor.${SubType<'Armor'>}` | 'Back' | `UpgradeComponent.${SubType<'UpgradeComponent'>}`)[];
+  const armorFilter: TableFilterDefinition[] = types.map((type) => ({
+    id: type,
+    name: type === 'Back' ? t('item.type.Back') : t(`item.type.short.${type}`),
+    rowIndexes: items.map(({ type, subtype }, index) => [type === 'Back' ? 'Back' : `${type}.${subtype}`, index] as const)
+      .filter(([ itemType ]) => type === itemType)
+      .map(([, index]) => index)
+  })).sort((a, b) => a.name.localeCompare(b.name));
+
   return (
-    <>
+    <TableFilterProvider filter={armorFilter}>
       <Notice icon="eye" index={false}>This is a preview page and more features will be added in the future.</Notice>
-      <Description actions={<ColumnSelect table={Items}/>}>
+      <Description actions={[<TableFilterButton key="filter" totalCount={items.length}/>, <ColumnSelect key="columns" table={Items}/>]}>
         <Trans id="legendary-armory.armor.description"/>
       </Description>
-      <LegendaryItemDataTable language={language} table={Items}/>
-    </>
+      <LegendaryItemDataTable language={language} table={Items} filtered/>
+    </TableFilterProvider>
   );
 }
 

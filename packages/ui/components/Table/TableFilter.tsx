6be7a973992ecd8f410240ce051b1f65a8c144b3
darthmaim <git@darthmaim.de>
2025-04-02 09:17:00 +0200

    Add generic datatable search

--- packages/ui/components/Table/TableFilter.tsx
+++ packages/ui/components/Table/TableFilter.tsx
@@ -8,16 +8,30 @@ import { FlexRow } from '../Layout/FlexRow';
 import { Button } from '../Form/Button';
 import { Separator } from '../Layout/Separator';
 import type { DataTableRowFilterComponent, DataTableRowFilterComponentProps } from './DataTable';
+import type { TableFilterSearchIndex } from './TableFilter.utils';
+import { TextInput } from '../Form/TextInput';
+import type { Language } from '@gw2treasures/database';
 
 interface TableFilterContext {
-  filteredRows?: number[] | undefined;
-  filterMap: Map<number | string, { name: string, rowIndexes: number[] }>
+  filteredRows?: number[] | undefined,
+  filterMap: Map<number | string, { name: string, rowIndexes: number[] }>,
 
-  filterIds: (number | string)[];
-  setFilterIds: (filterIds: (number | string)[]) => void;
+  filterIds: (number | string)[],
+  setFilterIds: (filterIds: (number | string)[]) => void,
+
+  searchIndex?: TableFilterSearchIndex,
+  searchQuery: string,
+  setSearchQuery: (query: string) => void,
 }
 
-const context = createContext<TableFilterContext>({ filteredRows: undefined, filterMap: new Map(), filterIds: [], setFilterIds: () => {} });
+const context = createContext<TableFilterContext>({
+  filteredRows: undefined,
+  filterMap: new Map(),
+  filterIds: [],
+  setFilterIds: () => {},
+  searchQuery: '',
+  setSearchQuery: () => {}
+});
 
 export interface TableFilterDefinition {
   id: number | string,
@@ -26,22 +40,34 @@ export interface TableFilterDefinition {
 }
 
 export interface TableFilterProviderProps {
-  filter: TableFilterDefinition[];
-  children: ReactNode;
+  filter: TableFilterDefinition[],
+  searchIndex?: TableFilterSearchIndex,
+  children: ReactNode,
+  language: Language,
 }
 
-export const TableFilterProvider: FC<TableFilterProviderProps> = ({ children, filter }) => {
+export const TableFilterProvider: FC<TableFilterProviderProps> = ({ children, filter, searchIndex, language }) => {
   const filterMap = new Map(filter.map(({ id, ...filter }) => [id, filter]));
   const allFilterIds = filter.map(({ id }) => id);
 
   const [filterIds, setFilterIds] = useState(allFilterIds);
 
-  const filteredRows = filterIds.length !== allFilterIds.length
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const filteredRowsBySearch = (searchIndex !== undefined && searchQuery !== '')
+    ? Object.entries(searchIndex).map(([string, indexes]) => string.toLocaleLowerCase(language).includes(searchQuery.toLocaleLowerCase(language)) ? indexes : []).flat(1)
+    : undefined;
+
+  const filteredRowsByFilter = filterIds.length !== allFilterIds.length
     ? filterIds.flatMap((id) => filterMap.get(id)?.rowIndexes ?? [])
     : undefined;
 
+  const filteredRows = filteredRowsBySearch === undefined && filteredRowsByFilter === undefined
+    ? undefined
+    : filteredRowsBySearch === undefined ? filteredRowsByFilter : filteredRowsByFilter === undefined ? filteredRowsBySearch : filteredRowsBySearch.filter((index) => filteredRowsByFilter.includes(index));
+
   return (
-    <context.Provider value={{ filteredRows, filterIds, setFilterIds, filterMap }}>
+    <context.Provider value={{ filteredRows, filterIds, setFilterIds, filterMap, searchIndex, searchQuery, setSearchQuery }}>
       {children}
     </context.Provider>
   );
@@ -131,3 +157,9 @@ function toggleArray<T>(array: T[], value: T): T[] {
   const withoutValue = array.filter((v) => v !== value);
   return withoutValue.length === array.length ? [...array, value] : withoutValue;
 }
+
+export const TableSearchInput: FC = () => {
+  const { searchQuery, setSearchQuery } = useContext(context);
+
+  return <TextInput value={searchQuery} onChange={setSearchQuery} type="search" placeholder="Search…"/>;
+};
---
+++ packages/ui/components/Table/TableFilter.utils.ts
@@ -0,0 +1,23 @@
+export type TableFilterSearchIndex = Record<string, number[]>
+
+export function createSearchIndex<T>(values: T[], mapToSearchTerms: (value: T) => string | string[]): TableFilterSearchIndex {
+  const searchIndex: Record<string, number[]> = {};
+
+  for(let i = 0; i < values.length; i++) {
+    const terms = toArray(mapToSearchTerms(values[i]));
+
+    for(const term of terms) {
+      if(!searchIndex[term]) {
+        searchIndex[term] = []
+      }
+
+      searchIndex[term].push(i);
+    }
+  }
+
+  return searchIndex;
+}
+
+function toArray<T>(value: T | T[]): T[] {
+  return Array.isArray(value) ? value : [value];
+}

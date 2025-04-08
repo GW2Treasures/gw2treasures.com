338729c3eeaaa86a9ae9b53365462b6221309b67
darthmaim <git@darthmaim.de>
2025-04-08 09:10:31 +0200

    Add daily account dungeon clears

--- apps/web/app/[language]/(instances)/dungeons/page.tsx
+++ apps/web/app/[language]/(instances)/dungeons/page.tsx
@@ -1,16 +1,27 @@
-import { HeroLayout } from '@/components/Layout/HeroLayout';
-import { Headline } from '@gw2treasures/ui/components/Headline/Headline';
-import { createDataTable } from '@gw2treasures/ui/components/Table/DataTable';
-import type { Metadata } from 'next';
-import { dungeons } from '@gw2treasures/static-data/dungeons/index';
+import { AchievementLink } from '@/components/Achievement/AchievementLink';
 import { EntityIcon } from '@/components/Entity/EntityIcon';
-import { FlexRow } from '@gw2treasures/ui/components/Layout/FlexRow';
-import { parseIcon } from '@/lib/parseIcon';
-import type { PageProps } from '@/lib/next';
-import { getTranslate } from '@/lib/translate';
+import { Gw2AccountBodyCells, Gw2AccountHeaderCells } from '@/components/Gw2Api/Gw2AccountTableCells';
 import { Trans } from '@/components/I18n/Trans';
 import { Description } from '@/components/Layout/Description';
+import { HeroLayout } from '@/components/Layout/HeroLayout';
 import { ColumnSelect } from '@/components/Table/ColumnSelect';
+import { cache } from '@/lib/cache';
+import { linkPropertiesWithoutRarity } from '@/lib/linkProperties';
+import type { PageProps } from '@/lib/next';
+import { parseIcon } from '@/lib/parseIcon';
+import { db } from '@/lib/prisma';
+import { getTranslate } from '@/lib/translate';
+import { dungeons } from '@gw2treasures/static-data/dungeons/index';
+import { Headline } from '@gw2treasures/ui/components/Headline/Headline';
+import { FlexRow } from '@gw2treasures/ui/components/Layout/FlexRow';
+import { createDataTable } from '@gw2treasures/ui/components/Table/DataTable';
+import type { Metadata } from 'next';
+import { DungeonDailyCell, requiredScopes } from './page.client';
+
+const getDungeonFrequenter = cache(
+  () => db.achievement.findUnique({ where: { id: 2963 }, select: linkPropertiesWithoutRarity }),
+  ['dungeons-frequenter'], { revalidate: 60 * 60 }
+);
 
 export default async function DungeonsPage({ params }: PageProps) {
   const { language } = await params;
@@ -21,11 +32,17 @@ export default async function DungeonsPage({ params }: PageProps) {
 
   const dungeonIcons = new Map(dungeons.map(({ id, icon }) => [id, parseIcon(icon)]));
 
+  const dungeonFrequenter = await getDungeonFrequenter();
+
   return (
     <HeroLayout hero={<Headline id="dungeons">Dungeons</Headline>}>
       <Description actions={<ColumnSelect table={Paths}/>}>
         <Trans id="dungeons.description"/>
       </Description>
+
+      <p>
+        Additional rewards can be earned by completing the repeatable <AchievementLink achievement={dungeonFrequenter!} icon={24}/> achievement.
+      </p>
 
       <Paths.Table>
         <Paths.Column id="id" title="Id" hidden small>
@@ -43,6 +60,9 @@ export default async function DungeonsPage({ params }: PageProps) {
         <Paths.Column id="level" title={<Trans id="dungeons.level"/>} align="right">
           {({ level }) => level}
         </Paths.Column>
+        <Paths.DynamicColumns id="daily" title={<Trans id="dungeons.daily"/>} headers={<Gw2AccountHeaderCells requiredScopes={requiredScopes} small/>}>
+          {({ id }) => <Gw2AccountBodyCells requiredScopes={requiredScopes}><DungeonDailyCell path={id} accountId={undefined as never}/></Gw2AccountBodyCells> }
+        </Paths.DynamicColumns>
       </Paths.Table>
     </HeroLayout>
   );

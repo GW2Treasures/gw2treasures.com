cb99061517c83773073ef50ef85f788549e56561
darthmaim <git@darthmaim.de>
2025-06-02 00:20:25 +0200

    Move fractal data into static-data package

--- apps/web/app/[language]/(instances)/fractals/helper.ts
+++ apps/web/app/[language]/(instances)/fractals/helper.ts
@@ -1,30 +1,3 @@
-import instabilityData from './instabilities/data.json';
-
-export function getDayOfYearIndex(date: Date = new Date()) {
-  const dateUTC = Date.UTC(date.getFullYear(), date.getMonth(), date.getDate());
-  const startOfYearUTC = Date.UTC(date.getFullYear(), 0, 1);
-
-  const index = (dateUTC - startOfYearUTC) / 24 / 60 / 60 / 1000;
-
-  const isLeapYear = new Date(date.getUTCFullYear(), 1, 29).getDate() === 29;
-
-  return !isLeapYear && index >= 59
-    ? index + 1
-    : index;
-}
-
-export function getInstabilities(level: number, dayOfYearIndex: number) {
-  const tier = Math.floor((level - 1) / 25);
-
-  if(tier !== 3) {
-    return [];
-  }
-
-  // const t4Level = level + (3 - tier) * 25;
-
-  return instabilityData.instabilities[level.toString() as keyof typeof instabilityData.instabilities][dayOfYearIndex].slice(0, tier);
-}
-
 export type TierFilter = '4' | '3' | '2' | '1' | 'all';
 const validTiers: string[] = ['1', '2', '3', '4', 'all'] satisfies TierFilter[];
 

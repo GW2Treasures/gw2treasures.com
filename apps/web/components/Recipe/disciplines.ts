0b20f366e19179ae311932b780afacc648979191
darthmaim <git@darthmaim.de>
2025-06-13 15:23:03 +0200

    Translate crafting disciplines and improve icon colors

---
+++ apps/web/components/Recipe/disciplines.ts
@@ -0,0 +1,14 @@
+import type { CraftingDiscipline } from '@gw2api/types/data/recipe';
+
+export const allDisciplines: CraftingDiscipline[] = [
+  'Armorsmith',
+  'Artificer',
+  'Chef',
+  'Homesteader',
+  'Huntsman',
+  'Jeweler',
+  'Leatherworker',
+  'Scribe',
+  'Tailor',
+  'Weaponsmith'
+];

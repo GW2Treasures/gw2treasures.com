3682b971f1b2aa4406e6a1035c6caf512250165a
darthmaim <git@darthmaim.de>
2025-07-29 09:59:50 +0200

    Fix eslint config (Next.js types are currently broken)

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -2,14 +2,14 @@ import nextJsPlugin from '@gw2treasures/eslint-plugin-nextjs';
 import reactConfig from '@gw2treasures/eslint-config/react';
 import reactCompiler from 'eslint-plugin-react-compiler';
 import tseslint from 'typescript-eslint';
-import { flatConfig as nextConfig } from '@next/eslint-plugin-next';
+import nextConfig from '@next/eslint-plugin-next';
 
 export default tseslint.config(
   // ignore all files in .next
   { ignores: ['.next'] },
 
   // extends next/core-web-vitals
-  nextConfig.coreWebVitals,
+  nextConfig.flatConfig.coreWebVitals,
 
   // extend @gw2treasures/eslint-config/react
   ...reactConfig,
@@ -19,7 +19,7 @@ export default tseslint.config(
     plugins: { 'react-compiler': reactCompiler },
     rules: reactCompiler.configs.recommended.rules
   },
-  
+
   // enable @gw2treasures/nextjs plugin for page.tsx files (no flat preset yet)
   nextJsPlugin.configs.recommended,
 );

d762e290603b4c7ff85bdc0822b4959ef041ad18
darthmaim <git@darthmaim.de>
2025-10-30 21:15:26 +0100

    Fallback icon url to original RenderSrv if loading from cdn fails

--- apps/web/lib/getIconUrl.ts
+++ apps/web/lib/getIconUrl.ts
@@ -3,9 +3,11 @@ import type { Icon } from '@gw2treasures/database';
 export type FixedIconSize = 16 | 32 | 64;
 export type IconSize = FixedIconSize | (number & {});
 
-export function getIconUrl({ id, signature }: Pick<Icon, 'id'> & Partial<Pick<Icon, 'signature'>>, size: FixedIconSize) {
+export function getIconUrl({ id, signature }: Pick<Icon, 'id'> & Partial<Pick<Icon, 'signature'>>, size: FixedIconSize, useOriginalRenderSrv = false) {
   return signature
-    ? `https://icons-gw2.darthmaim-cdn.com/${signature}/${id}-${size}px.png`
+    ? !useOriginalRenderSrv
+      ? `https://icons-gw2.darthmaim-cdn.com/${signature}/${id}-${size}px.png`
+      : `https://render.guildwars2.com/file/${signature}/${id}.png`
     : `https://assets.gw2dat.com/${id}.png`;
 }
 

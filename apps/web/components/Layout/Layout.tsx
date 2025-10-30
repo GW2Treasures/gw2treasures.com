f35e89488bb1dadaf0e04494afad64a2ebaf30b6
darthmaim <git@darthmaim.de>
2025-10-30 17:47:40 +0100

    Remove API disabled banner

--- apps/web/components/Layout/Layout.tsx
+++ apps/web/components/Layout/Layout.tsx
@@ -15,7 +15,6 @@ import { currencyCategories, type CurrencyCategoryName } from '@gw2treasures/sta
 import { ReviewButton } from './Header/ReviewButton';
 import { PartnerButton } from './partner/button';
 import { PartnerLogo } from './partner/logo';
-import { FormatDate } from '../Format/FormatDate';
 import { getBaseUrl } from '@/lib/url';
 
 interface LayoutProps {
@@ -63,10 +62,7 @@ const Layout: FC<LayoutProps> = ({ children, language }) => {
         </Menu>
         <hr className={styles.headerShadow}/>
         <aside data-nosnippet="true" style={{ gridArea: 'notification', padding: 16, lineHeight: 1.5, background: '#03a9f422', marginTop: -1, borderBottom: '1px solid var(--color-border-transparent)', zIndex: 1 }}>
-          <b>The official Guild Wars 2 API will be disabled</b> from <FormatDate date={new Date('2025-10-24T17:00:00.000Z')}/> until <FormatDate date={new Date('2025-10-30T17:00:00.000Z')}/> to
-          avoid spoilers during the launch of <b style={{ whiteSpace: 'nowrap' }}>Guild Wars 2: Visions of Eternity.</b><br/>
-          <div style={{ fontSize: 15, opacity: .8 }}>New items, achievements, skills, and others will only be available after the API has been re-enabled. It will not be possible to access your account info (like inventories, achievements, ...) during this time.<br/></div>
-          The new expansion is now available, <ExternalLink href={new URL('/buy-gw2', getBaseUrl()).toString()}>buy it via our affiliate link to support us!</ExternalLink>
+          The new expansion <b>Guild Wars 2: Visions of Eternity</b> is now available! <ExternalLink href={new URL('/buy-gw2', getBaseUrl()).toString()}>Support gw2treasures.com by buying it using our affiliate link!</ExternalLink>
         </aside>
         {children}
         <footer className={styles.footer} data-nosnippet>

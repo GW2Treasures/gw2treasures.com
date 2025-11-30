91bcb8973be43e119be55b5c6c7f542af52f6146
darthmaim <git@darthmaim.de>
2025-11-30 11:45:12 +0100

    Add JW and VoE mastery regions

--- apps/web/components/Achievement/Mastery.tsx
+++ apps/web/components/Achievement/Mastery.tsx
@@ -4,12 +4,14 @@ import type * as CSS from 'csstype';
 import { Trans } from '../I18n/Trans';
 
 export const MasteryColors: Record<MasteryRegion, CSS.Property.Color> = {
-  Tyria: '#FB8C00', // core
+  Tyria: '#FF2000', // core
   Maguuma: '#43A047', // HoT
   Desert: '#D81B60', // PoF
   Tundra: '#00ACC1', // Icebrood
   Jade: '#1E88E5', // EoD
   Sky: '#F3D71F', // SotO
+  Wild: '#283593', // JW
+  Magic: '#E17D0D', // VoE
   Unknown: 'currentColor',
 };
 

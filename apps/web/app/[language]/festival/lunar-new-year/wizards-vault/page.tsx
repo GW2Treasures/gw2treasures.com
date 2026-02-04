e83a71f4c8693e6bc2240fd2c4feae8cbf2c1e1c
darthmaim <git@darthmaim.de>
2026-02-04 09:40:44 +0100

    Update content for LNY 2026

--- apps/web/app/[language]/festival/lunar-new-year/wizards-vault/page.tsx
+++ apps/web/app/[language]/festival/lunar-new-year/wizards-vault/page.tsx
@@ -1,16 +1,17 @@
 import { Gw2Accounts } from '@/components/Gw2Api/Gw2Accounts';
 import { Trans } from '@/components/I18n/Trans';
+import { Description } from '@/components/Layout/Description';
 import { PageLayout } from '@/components/Layout/PageLayout';
+import { WizardsVaultTable } from '@/components/WizardsVault/WizardsVaultTable';
 import { cache } from '@/lib/cache';
-import { db } from '@/lib/prisma';
-import { requiredScopes } from '../helper';
+import { createMetadata } from '@/lib/metadata';
 import { pageView } from '@/lib/pageView';
-import Link from 'next/link';
-import { WizardsVaultObjective } from '@/components/WizardsVault/WizardsVaultObjective';
+import { db } from '@/lib/prisma';
 import { getLanguage, getTranslate } from '@/lib/translate';
 import { Icon } from '@gw2treasures/ui';
 import { Notice } from '@gw2treasures/ui/components/Notice/Notice';
-import { createMetadata } from '@/lib/metadata';
+import Link from 'next/link';
+import { requiredScopes } from '../helper';
 
 const objectiveIds: number[] = [
   214, // Complete the Annual New Year Customs Achievement
@@ -31,7 +32,6 @@ const loadData = cache(async function loadData() {
 
 
 export default async function LunarNewYearAchievementsPage() {
-  const language = await getLanguage();
   const { objectives } = await loadData();
   await pageView('festival/lunar-new-year/wizards-vault');
 
@@ -39,13 +39,21 @@ export default async function LunarNewYearAchievementsPage() {
     <PageLayout>
       <Gw2Accounts requiredScopes={requiredScopes} loading={null} loginMessage={<Trans id="festival.wizards-vault.login"/>} authorizationMessage={<Trans id="festival.wizards-vault.authorize"/>}/>
 
-      <p><Trans id="festival.lunar-new-year.wizards-vault.description"/></p>
+      <Notice type="warning">
+        The official Guild Wars 2 API currently has a bug and is not providing progress for festival special objectives.
+      </Notice>
 
-      {objectives.length > 0 ? objectives.map((objective) => (
-        <WizardsVaultObjective key={objective.id} objective={objective} language={language} disabledLoginNotification/>
-      )) : (
-        <Notice>No Wizard&apos;s Vault objectives for Lunar New Year are available in the Guild Wars 2 API yet.</Notice>
-      )}
+      <WizardsVaultTable objectives={objectives}>
+        {(table, columnSelect) => (
+          <>
+            <Description actions={columnSelect}>
+              <Trans id="festival.lunar-new-year.wizards-vault.description"/>
+            </Description>
+
+            {table}
+          </>
+        )}
+      </WizardsVaultTable>
 
       <p style={{ border: '1px solid var(--color-border)', marginTop: 48, padding: 16 }}>
         <Icon icon="wizards-vault"/>{' '}

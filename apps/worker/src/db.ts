447412355b5af4f817504c0051e8c94a1e13e355
darthmaim <git@darthmaim.de>
2026-07-19 20:30:32 +0200

    Increase worker transaction timeout

--- apps/worker/src/db.ts
+++ apps/worker/src/db.ts
@@ -5,7 +5,10 @@ const datasourceUrl = new URL(process.env.DATABASE_URL!);
 datasourceUrl.searchParams.set('application_name', 'worker');
 
 export const db = createPrismaClient(datasourceUrl.toString(), {
-  log: ['info', { level: 'query', emit: 'event' }, 'error', 'warn']
+  log: ['info', { level: 'query', emit: 'event' }, 'error', 'warn'],
+  transactionOptions: {
+    timeout: 20_000,
+  }
 });
 
 export type PrismaTransaction = Omit<PrismaClient, '$connect' | '$disconnect' | '$on' | '$transaction' | '$extends'>;
